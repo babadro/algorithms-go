629cfe955f9ddeec382771b3789e39d5b3168a29
petrovich.andrey <petrovich.andrey@wildberries.ru>
2019-06-10 09:02:45 +0300

    DFS in progress 2

--- 04_TreesAndGraphs/DepthFirstSearchForGraph/main.go
+++ 04_TreesAndGraphs/DepthFirstSearchForGraph/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type AdjListNode struct {
 	Vertex int
 	Next   *AdjListNode
@@ -30,4 +35,59 @@ func (g *Graph) AddEdge(src, dest int) {
 	newNode = NewAdjListNode(src)
 	newNode.Next = g.Array[dest].Head
 	g.Array[dest].Head = newNode
+}
+
+func (g *Graph) String() string {
+	var str strings.Builder
+	var v int
+	for v = 0; v < g.V; v++ {
+		pCrawl := g.Array[v].Head
+		str.WriteString(fmt.Sprintf("\n Ajacency list of vertex %d\n head ", v))
+		for pCrawl != nil {
+			str.WriteString(fmt.Sprintf("-> %d", pCrawl.Vertex))
+			pCrawl = pCrawl.Next
+		}
+		str.WriteString("\n")
+	}
+	return str.String()
+}
+
+func (g *Graph) dFSUtil(vertex int, visited []bool) {
+	visited[vertex] = true
+	fmt.Println(vertex)
+
+	node := g.Array[vertex]
+
+	for node.Head.Next != nil {
+		if !visited[node.Head.Vertex] {
+			g.dFSUtil(node.Head.Vertex, visited)
+		}
+	}
+}
+
+func (g *Graph) DFS() {
+	visited := make([]bool, g.V)
+
+	for i := 0; i < g.V; i++ {
+		if !visited[i] {
+			g.dFSUtil(i, visited)
+		}
+	}
+}
+
+func main() {
+	g := CreateGraph(4)
+
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 2)
+	g.AddEdge(2, 0)
+	g.AddEdge(2, 3)
+	g.AddEdge(3, 3)
+
+	fmt.Println(g)
+
+	//fmt.Println("Following is Depth First Traversal")
+
+	///g.DFS()
 }
