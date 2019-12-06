9a0b9142bd3f8228dfa6a76b516222c9866fe686
petrovich.andrey <petrovich.andrey@wildberries.ru>
2019-12-06 09:01:19 +0300

    165 complete

--- leet/0165_compare_version_numbers/compareVersion.go
+++ leet/0165_compare_version_numbers/compareVersion.go
@@ -12,7 +12,6 @@ package _165_compare_version_numbers
 // "0" = 48, "." = 46
 func CompareVersion(version1 string, version2 string) int {
 	i, j := 0, 0
-	segment1, segment2 := 0, 0
 	for {
 		for i < len(version1) && version1[i] == 48 {
 			i++
@@ -25,40 +24,64 @@ func CompareVersion(version1 string, version2 string) int {
 		}
 
 		val1, val2 := uint8(0), uint8(0)
-		flag1, flag2 := false, false
+
 		for {
 			if i < len(version1) && version1[i] != 46 {
-				val1 = version1[i]
+				val1 *= 10
+				val1 += version1[i] - 48
 				i++
-
-			} else if version1[i-1] > 48 {
-				flag1 = true
 			}
 			if j < len(version2) && version2[j] != 46 {
-				val2 = version2[j]
+				val2 *= 10
+				val2 += version2[j] - 48
 				j++
 
-			} else if version2[j-1] > 48 {
-				flag2 = true
 			}
 
-			if (!flag1 && flag2) || (val1 > val2) {
-				return 1
-			} else if (flag1 && !flag2) || (val1 < val2) {
-				return -1
-			}
 			if (i == len(version1) || version1[i] == 46) && (j == len(version2) || version2[j] == 46) {
+				if val1 > val2 {
+					return 1
+				}
+				if val1 < val2 {
+					return -1
+				}
 				if i < len(version1) {
 					i++
-					segment1++
 				}
 				if j < len(version2) {
 					j++
-					segment2++
 				}
 				break
 			}
 		}
-
 	}
+}
+
+func CompareVersion2(version1 string, version2 string) int {
+	i, j := 0, 0
+	for i < len(version1) || j < len(version2) {
+		val1, val2 := uint8(0), uint8(0)
+		for i < len(version1) && version1[i] != 46 {
+			val1 *= 10
+			val1 += version1[i] - 48
+			i++
+		}
+		for j < len(version2) && version2[j] != 46 {
+			val2 *= 10
+			val2 += version2[j] - 48
+			j++
+
+		}
+
+		if val1 > val2 {
+			return 1
+		}
+		if val1 < val2 {
+			return -1
+		}
+		i++
+		j++
+	}
+	return 0
+
 }
