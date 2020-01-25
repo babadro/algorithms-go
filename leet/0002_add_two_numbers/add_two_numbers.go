9792a81c2a7fc0da25ea6534c1e32524fa40e12c
baba_dro <babadro@gmail.com>
2020-01-25 10:14:20 +0300

    add two numbers done

--- leet/0002_add_two_numbers/add_two_numbers.go
+++ leet/0002_add_two_numbers/add_two_numbers.go
@@ -1,46 +1,47 @@
 package _002_add_two_numbers
 
-import (
-	"strconv"
-)
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-//Output: 7 -> 0 -> 8
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var str1, str2 []byte
-	node := l1
-	for node != nil {
-		str1 = append(str1, strconv.Itoa(node.Val)...)
-		node = node.Next
+	mem := 0
+	res := &ListNode{}
+	var prevNode *ListNode
+	val1, val2 := 0, 0
+	val1 = l1.Val
+	val2 = l2.Val
+	sum := val1 + val2
+	if sum < 10 {
+		res.Val = sum
+	} else {
+		res.Val = sum % 10
+		mem = 1
 	}
-	for i := 0; i < len(str1)/2; i++ {
-		str1[i], str1[len(str1)-1-i] = str1[len(str1)-1-i], str1[i]
+	prevNode = res
+	l1, l2 = l1.Next, l2.Next
+	for l1 != nil || l2 != nil || mem != 0 {
+		newNode := &ListNode{}
+		prevNode.Next = newNode
+		val1, val2 := 0, 0
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := val1 + val2 + mem
+		if sum < 10 {
+			newNode.Val = sum
+			mem = 0
+		} else {
+			newNode.Val = sum % 10
+			mem = 1
+		}
+		prevNode = newNode
 	}
-	node = l2
-	for node != nil {
-		str2 = append(str2, strconv.Itoa(node.Val)...)
-		node = node.Next
-	}
-	for i := 0; i < len(str2)/2; i++ {
-		str2[i], str2[len(str2)-1-i] = str2[len(str2)-1-i], str2[i]
-	}
-	var int1, int2 int
-
-	int1, _ = strconv.Atoi(string(str1))
-	int2, _ = strconv.Atoi(string(str2))
-	sum := int1 + int2
-	sumStr := []byte(strconv.Itoa(sum))
-	firstDigit, _ := strconv.Atoi(string(sumStr[0]))
-	node = &ListNode{Val: firstDigit}
-	for i := 1; i < len(sumStr); i++ {
-		digit, _ := strconv.Atoi(string(sumStr[i]))
-		newNode := &ListNode{Val: digit, Next: node}
-		node = newNode
-	}
-	return node
+	return res
 }
--- leet/0002_add_two_numbers/add_two_numbers_test.go
+++ leet/0002_add_two_numbers/add_two_numbers_test.go
@@ -1,60 +1,60 @@
 package _002_add_two_numbers
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestAddTwoNumbers(t *testing.T) {
-	// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-	//Output: 7 -> 0 -> 8
-	l1 := &ListNode{Val: 3}
-	l2 := &ListNode{Val: 0}
-	l3 := &ListNode{Val: 9}
+	cases := []struct {
+		term1    string
+		term2    string
+		expected string
+	}{
+		{term1: "0", term2: "0", expected: "0"},
+		{"0", "2", "2"},
+		{"3", "0", "3"},
+		{"99", "0", "99"},
+		{"0", "83", "83"},
+		{"903", "217", "1120"},
+		{"99", "85", "184"},
+		{"100000000000000000000000000000000000000000000000000000000000001", "10", "100000000000000000000000000000000000000000000000000000000000011"},
+	}
+	for i, c := range cases {
+		l1, l2 := StringToLinkedList(c.term1), StringToLinkedList(c.term2)
+		if fact := LinkedListToString(addTwoNumbers(l1, l2)); fact != c.expected {
+			t.Errorf("case:#%d, expected %s, got %s", i+1, c.expected, fact)
+		}
+	}
+}
 
-	l1.Next = l2
-	l2.Next = l3
+func TestLinkedListFromString(t *testing.T) {
+	cases := []string{"0", "1", "2", "100", "992"}
+	for _, c := range cases {
+		node := StringToLinkedList(c)
+		t.Log(LinkedListToString(node))
+	}
+}
 
-	node := l1
+func StringToLinkedList(input string) *ListNode {
+	res := &ListNode{Val: int(input[len(input)-1]) - 48}
+	prevNode := res
+	for i := len(input) - 2; i >= 0; i-- {
+		newNode := &ListNode{Val: int(input[i] - 48)}
+		prevNode.Next = newNode
+		prevNode = newNode
+	}
+	return res
+}
+
+func LinkedListToString(node *ListNode) string {
+	res := make([]byte, 0)
 	for node != nil {
-		t.Log(node.Val)
+		res = append(res, strconv.Itoa(node.Val)...)
 		node = node.Next
 	}
-
-	l4 := &ListNode{Val: 7}
-	l5 := &ListNode{Val: 1}
-	l6 := &ListNode{Val: 2}
-	l4.Next = l5
-	l5.Next = l6
-
-	node = l4
-	for node != nil {
-		t.Log(node.Val)
-		node = node.Next
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-1-i] = res[len(res)-1-i], res[i]
 	}
-	node = addTwoNumbers(l4, l1)
-	for node != nil {
-		t.Log(node.Val)
-		node = node.Next
-	}
-
-	l1 = &ListNode{Val: 0}
-
-	l2 = &ListNode{Val: 7}
-	l3 = &ListNode{Val: 3}
-
-	l2.Next = l3
-	node = addTwoNumbers(l1, l2)
-	for node != nil {
-		t.Log(node.Val)
-		node = node.Next
-	}
-
-	l1 = &ListNode{Val: 1}
-	l2 = &ListNode{Val: 8}
-	l1.Next = l2
-
-	l3 = &ListNode{Val: 0}
-	node = addTwoNumbers(l1, l3)
-	for node != nil {
-		t.Log(node.Val)
-		node = node.Next
-	}
+	return string(res)
 }
