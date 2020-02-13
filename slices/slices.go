fa9ee9fbc9282defe51590bc47a9a3cade0a656c
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-02-13 08:44:27 +0300

    slice equal util

---
+++ slices/slices.go
@@ -0,0 +1,14 @@
+package slices
+
+func IntSlicesAreEqual(slice1, slice2 []int) bool {
+	length := len(slice1)
+	if length != len(slice2) {
+		return false
+	}
+	for i := 0; i < length; i++ {
+		if slice1[i] != slice2[i] {
+			return false
+		}
+	}
+	return true
+}
