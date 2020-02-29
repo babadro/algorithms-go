34aa4746f853b0374f20611689d4ed1a5dd2ae01
baba_dro <babadro@gmail.com>
2020-02-29 10:26:35 +0300

    heaters final

--- leet/0475_heaters/heaters.go
+++ leet/0475_heaters/heaters.go
@@ -4,7 +4,63 @@ import (
 	"sort"
 )
 
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+// My solution ver 2.0
 func findRadius(houses []int, heaters []int) int {
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	radius := 0
+	for _, housePos := range houses {
+		nearestHeaterPosIdx := sort.Search(len(heaters), func(i int) bool {
+			return heaters[i] >= housePos
+		})
+		if nearestHeaterPosIdx == len(heaters) {
+			nearestHeaterPosIdx--
+		}
+		minDistance := abs(housePos, heaters[nearestHeaterPosIdx])
+		if nearestHeaterPosIdx > 0 {
+			if distance := abs(heaters[nearestHeaterPosIdx-1], housePos); distance < minDistance {
+				minDistance = distance
+			}
+		}
+		if minDistance > radius {
+			radius = minDistance
+		}
+	}
+	return radius
+}
+
+// 40 ms solution
+func findRadius40(houses []int, heaters []int) int {
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	w := 0
+	m := 0
+	for _, h := range houses {
+		v := abs(h, heaters[w])
+		for ; w < len(heaters)-1; w++ {
+			vv := abs(h, heaters[w+1])
+			if vv > v {
+				break
+			} else {
+				v = vv
+			}
+		}
+		if v > m {
+			m = v
+		}
+	}
+	return m
+}
+
+// My solution ver 1.0
+func findRadius2(houses []int, heaters []int) int {
 	sort.Ints(houses)
 	sort.Ints(heaters)
 	max, maxHeater := houses[len(houses)-1], heaters[len(heaters)-1]
