5626376c8106616f266d11b2233c24f735b4065e
baba_dro <babadro@gmail.com>
2020-02-29 09:25:21 +0300

    heaters complete

--- leet/0475_heaters/heaters.go
+++ leet/0475_heaters/heaters.go
@@ -1,17 +1,30 @@
 package _475_heaters
 
-import "sort"
+import (
+	"sort"
+)
 
 func findRadius(houses []int, heaters []int) int {
-	n := 1_000_000_000
+	sort.Ints(houses)
+	sort.Ints(heaters)
+	max, maxHeater := houses[len(houses)-1], heaters[len(heaters)-1]
+	min, minHeater := houses[0], heaters[0]
+	if maxHeater > max {
+		max = maxHeater
+	}
+	if minHeater < min {
+		min = minHeater
+	}
+	n := max - min + 1
 	return sort.Search(n, func(i int) bool {
 		return checkRadius(houses, heaters, i)
-	}) - 1
+	})
 }
 
 func checkRadius(houses []int, heaters []int, radius int) bool {
 	heatersIdx := 0
-	for _, housePos := range houses {
+	for i := 0; i < len(houses); {
+		housePos := houses[i]
 		if heatersIdx == len(heaters) {
 			return false
 		}
@@ -23,6 +36,7 @@ func checkRadius(houses []int, heaters []int, radius int) bool {
 		if heatersPos-radius > housePos {
 			return false
 		}
+		i++
 	}
 	return true
 }
