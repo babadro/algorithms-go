41d5e0d566d0453056d8c9190b2780cc9833cf90
baba_dro <babadro@gmail.com>
2020-03-07 12:12:01 +0300

    islands complete

--- leet/0200_number_of_islands/number_of_islands.go
+++ leet/0200_number_of_islands/number_of_islands.go
@@ -1,51 +1,30 @@
 package _200_number_of_islands
 
-import "sort"
-
-type point struct {
-	x, y int
+func numIslands(grid [][]byte) int {
+	lenY := len(grid)
+	if lenY == 0 {
+		return 0
+	}
+	lenX := len(grid[0])
+	counter := 0
+	for y, arr := range grid {
+		for x := range arr {
+			if grid[y][x] == '1' {
+				counter++
+				walk(x, y, lenY, lenX, grid)
+			}
+		}
+	}
+	return counter
 }
 
-func numIslands(grid [][]byte) int {
-	pointToIsland := make(map[point]int)
-
-	islandId := 0
-	for x, arr := range grid {
-		for y, val := range arr {
-			if val == '0' {
-				continue
-			}
-			currPoint := point{x, y}
-			if y > 0 && arr[y-1] != '0' {
-				adjacentPoint := point{x, y - 1}
-				pointToIsland[currPoint] = pointToIsland[adjacentPoint]
-				continue
-			}
-			islandId++
-			pointToIsland[currPoint] = islandId
-			if x > 0 && grid[x-1][y] != '0' {
-				adjacentPoint := point{x - 1, y}
-				if pointToIsland[adjacentPoint] > pointToIsland[currPoint] {
-					pointToIsland[adjacentPoint] = pointToIsland[currPoint]
-				} else if pointToIsland[adjacentPoint] < pointToIsland[currPoint] {
-					pointToIsland[currPoint] = pointToIsland[adjacentPoint]
-				}
-			}
-		}
+func walk(x, y, lenY, lenX int, grid [][]byte) {
+	if x < 0 || x == lenX || y < 0 || y == lenY || grid[y][x] == '0' {
+		return
 	}
-
-	islandIds := make([]int, 0)
-	for _, id := range pointToIsland {
-		islandIds = append(islandIds, id)
-	}
-
-	sort.Ints(islandIds)
-	counter := 0
-	for i, id := range islandIds {
-		if i == 0 || islandIds[i-1] != id {
-			counter++
-		}
-	}
-
-	return counter
+	grid[y][x] = '0'
+	walk(x, y-1, lenY, lenX, grid)
+	walk(x, y+1, lenY, lenX, grid)
+	walk(x-1, y, lenY, lenX, grid)
+	walk(x+1, y, lenY, lenX, grid)
 }
