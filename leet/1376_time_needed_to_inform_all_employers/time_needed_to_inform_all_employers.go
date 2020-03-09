27936c19da925f24bcb7675cb511705bdced51b7
baba_dro <babadro@gmail.com>
2020-03-09 11:20:53 +0300

    1376 in progress

--- leet/1376_time_needed_to_inform_all_employers/time_needed_to_inform_all_employers.go
+++ leet/1376_time_needed_to_inform_all_employers/time_needed_to_inform_all_employers.go
@@ -1,23 +1,36 @@
 package _1376_time_needed_to_inform_all_employers
 
+import "fmt"
+
 func numOfMinutes(n int, headID int, manager []int, informTime []int) int {
-	employeeMap := make(map[int][]int)
+	managerToEmployees := make(map[int][]int)
 	for employeeId, managerId := range manager {
-		employeeMap[managerId] = append(employeeMap[managerId], employeeId)
+		managerToEmployees[managerId] = append(managerToEmployees[managerId], employeeId)
 	}
+	level := []int{headID}
+	var nextLevel []int
 	totalTime := informTime[headID]
-	managerId := headID
-	counter := 1
-	for counter < n {
-		employees := employeeMap[managerId]
+	for {
+		nextLevelExists := false
 		maxTime := 0
-		for _, employeeId := range employees {
-			if timeNeeded := informTime[employeeId]; timeNeeded > maxTime {
-				maxTime = timeNeeded
+		for _, managerId := range level {
+			if employees, ok := managerToEmployees[managerId]; ok {
+				nextLevelExists = true
+				for _, employeeId := range employees {
+					if timeNeeded := informTime[employeeId]; timeNeeded > maxTime {
+						maxTime = timeNeeded
+					}
+				}
+				nextLevel = append(nextLevel, employees...)
 			}
-			counter++
 		}
+		if !nextLevelExists {
+			break
+		}
+		fmt.Println(maxTime)
 		totalTime += maxTime
+		level = nextLevel
+		nextLevel = nextLevel[:0]
 	}
 	return totalTime
 }
