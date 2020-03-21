0c5f3c0491f55b322779411616b6b239e324bf5f
baba_dro <babadro@gmail.com>
2020-03-21 10:44:10 +0300

    i've tried

--- leet/0053_Maximum_Subbarray/maximum_subarray.go
+++ leet/0053_Maximum_Subbarray/maximum_subarray.go
@@ -1,46 +1,16 @@
 package _053_Maximum_Subbarray
 
 func maxSubArray(nums []int) int {
-	minSum, maxSum, totalSum := 0, 0, 0
-	minIdx, maxIdx := 0, 0
-	signedOnly, unsignedOnly := true, true
-	maxSigned := 0
-	minSumFlag, maxSumFlag := true, true
-	for i, n := range nums {
-		if n > 0 {
-			signedOnly = false
-		} else if n < 0 {
-			unsignedOnly = false
+	currSum, maxSum := nums[0], nums[0]
+	for _, v := range nums[1:] {
+		if currSum < 0 {
+			currSum = v
+		} else {
+			currSum += v
 		}
-		if signedOnly && n > maxSigned || maxSigned == 0 {
-			maxSigned = n
-		}
-		totalSum += n
-		if totalSum < minSum || minSumFlag {
-			minSum = totalSum
-			minIdx = i
-			minSumFlag = false
-		}
-		if totalSum >= maxSum || maxSumFlag {
-			maxSum = totalSum
-			maxIdx = i
-			maxSumFlag = false
+		if currSum > maxSum {
+			maxSum = currSum
 		}
 	}
-
-	if signedOnly {
-		return maxSigned
-	} else if unsignedOnly {
-		return totalSum
-	}
-
-	start := minIdx
-	if nums[minIdx] < 0 {
-		start++
-	}
-	res := 0
-	for i := start; i <= maxIdx; i++ {
-		res += nums[i]
-	}
-	return res
+	return maxSum
 }
