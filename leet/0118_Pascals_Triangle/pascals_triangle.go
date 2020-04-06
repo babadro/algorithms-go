9e989925a1c743f8bfd55707131eee4c4fce56e7
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-06 08:13:32 +0300

    pascals triangle comlete

--- leet/0118_Pascals_Triangle/pascals_triangle.go
+++ leet/0118_Pascals_Triangle/pascals_triangle.go
@@ -1,5 +1,14 @@
 package _118_Pascals_Triangle
 
 func generate(numRows int) [][]int {
-	// TODO
+	res := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		length := i + 1
+		res[i] = make([]int, length)
+		res[i][0], res[i][length-1] = 1, 1
+		for j := 1; j < length-1; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
 }
