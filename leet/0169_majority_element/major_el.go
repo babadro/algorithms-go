4f2a11f38654cae2ca6b37512014a7ea36188dc6
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-15 08:47:11 +0300

    majority element

---
+++ leet/0169_majority_element/major_el.go
@@ -0,0 +1,8 @@
+package _169_majority_element
+
+import "sort"
+
+func majorityElement(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)/2]
+}
---
+++ leet/0169_majority_element/major_el_test.go
@@ -0,0 +1,7 @@
+package _169_majority_element
+
+import "testing"
+
+func TestMajorityElement(t *testing.T) {
+	t.Log(majorityElement([]int{2, 2, 1, 1, 1, 2, 2}))
+}
