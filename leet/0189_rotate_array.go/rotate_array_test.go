c3695e72dfd68bba5bbf38713018471d8a50202a
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-19 19:26:21 +0300

    rotate in progress 2

--- leet/0189_rotate_array.go/rotate_array_test.go
+++ leet/0189_rotate_array.go/rotate_array_test.go
@@ -1,4 +1,4 @@
-package _190_rotate_array_go
+package _189_rotate_array_go
 
 import (
 	"github.com/babadro/algorithms-go/slices"
@@ -11,8 +11,15 @@ func TestRotate(t *testing.T) {
 		k        int
 		expected []int
 	}{
-		//{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 3, []int{10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9},},
+		{[]int{1, 2, 3, 4, 5}, 2, []int{4, 5, 1, 2, 3}},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 2, []int{11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
 		{[]int{1, 2, 3, 4}, 3, []int{2, 3, 4, 1}},
+		{[]int{1, 2, 3, 4}, 4, []int{1, 2, 3, 4}},
+		{[]int{1}, 1, []int{1}},
+		{[]int{-1}, 2, []int{-1}},
+		{[]int{}, 2, []int{}},
+		{[]int{1, 2}, 3, []int{2, 1}},
+		{[]int{1, 2, 3}, 4, []int{3, 1, 2}},
 	}
 
 	for i, c := range cases {
