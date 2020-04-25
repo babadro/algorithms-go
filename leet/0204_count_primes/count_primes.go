3cc3a993c74442fb7600cdf8c2d68d73aad22480
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-25 09:10:10 +0300

    countPrimes naive

--- leet/0204_count_primes/count_primes.go
+++ leet/0204_count_primes/count_primes.go
@@ -1,6 +1,39 @@
 package _204_count_primes
 
+import "math"
+
 // TODO
 func countPrimes(n int) int {
 
+	arr := make([]bool, n)
+	for {
+		break
+	}
+	counter := 0
+	for _, notPrime := range arr {
+		if !notPrime {
+			counter++
+		}
+	}
+	return counter
+}
+
+func countPrimesNaive(n int) int {
+	counter := 0
+	for i := 2; i < n; i++ {
+		if isPrime(i) {
+			counter++
+		}
+	}
+	return counter
+}
+
+func isPrime(n int) bool {
+	to := int(math.Sqrt(float64(n)))
+	for i := 2; i <= to; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
---
+++ leet/0204_count_primes/count_primes_test.go
@@ -0,0 +1,7 @@
+package _204_count_primes
+
+import "testing"
+
+func TestCountPrimes(t *testing.T) {
+	t.Log(countPrimes(3572))
+}
