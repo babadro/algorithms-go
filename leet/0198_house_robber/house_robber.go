3b65459e748483377e55a4b740ef307f9dd81499
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-26 07:49:34 +0300

    house robber

--- leet/0198_house_robber/house_robber.go
+++ leet/0198_house_robber/house_robber.go
@@ -1,6 +1,23 @@
 package _198_house_robber
 
-// TODO I don't know how I can solve it
 func rob(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	if l == 1 {
+		return nums[0]
+	}
+	nums[1] = max(nums[0], nums[1])
+	for i := 2; i < l; i++ {
+		nums[i] = max(nums[i-1], nums[i-2]+nums[i])
+	}
+	return nums[l-1]
+}
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
