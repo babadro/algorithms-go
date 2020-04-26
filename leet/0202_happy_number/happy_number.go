0776f9542999fcb2d157ec4571c87ee117a2d21e
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-04-26 09:23:56 +0300

    happy number completed

--- leet/0202_happy_number/happy_number.go
+++ leet/0202_happy_number/happy_number.go
@@ -1,6 +1,41 @@
 package _202_happy_number
 
-// TODO
-func isHappy(n int) bool {
+import "fmt"
 
+func isHappy(n int) bool {
+	slow, fast := n, n
+	for {
+		slow = calcSquareSum(slow)
+		fast = calcSquareSum(calcSquareSum(fast))
+		fmt.Println(slow, " ", fast)
+		if slow == fast {
+			break
+		}
+	}
+	return slow == 1
+}
+
+func isHappyNaive(n int) bool {
+	set := make(map[int]bool)
+	for {
+		n = calcSquareSum(n)
+		if n == 1 {
+			return true
+		}
+		if set[n] {
+			return false
+		}
+		set[n] = true
+	}
+	return false
+}
+
+func calcSquareSum(n int) int {
+	res, digit := 0, 0
+	for n > 0 {
+		digit = n % 10
+		res += digit * digit
+		n = n / 10
+	}
+	return res
 }
---
+++ leet/0202_happy_number/happy_number_test.go
@@ -0,0 +1,11 @@
+package _202_happy_number
+
+import "testing"
+
+func TestCalcSquareSum(t *testing.T) {
+	t.Log(calcSquareSum(123467))
+}
+
+func TestIsHappy(t *testing.T) {
+	t.Log(isHappy(12))
+}
