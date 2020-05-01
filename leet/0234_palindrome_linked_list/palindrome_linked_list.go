78ee4fd7004df554dc872668e1922998782cbf2e
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-01 09:48:16 +0300

    palindrome linked list completed

--- leet/0234_palindrome_linked_list/palindrome_linked_list.go
+++ leet/0234_palindrome_linked_list/palindrome_linked_list.go
@@ -2,7 +2,59 @@ package _234_palindrome_linked_list
 
 import "github.com/babadro/algorithms-go/03_StacksAndQueues/04_LinkedList/single"
 
-// TODO
 func isPalindrome(head *single.ListNode) bool {
+	totalCount := 0
+	node := head
+	for node != nil {
+		totalCount++
+		node = node.Next
+	}
+	length := totalCount / 2
+	var nodeToReverse *single.ListNode
+	counter := 0
+	nodeToReverse = head
+	for {
+		if counter == totalCount-length {
+			break
+		}
+		nodeToReverse = nodeToReverse.Next
+		counter++
 
+	}
+	node1, node2 := head, reverse(nodeToReverse)
+	for i := 0; i < length; i++ {
+		if node1.Val != node2.Val {
+			return false
+		}
+		node1 = node1.Next
+		node2 = node2.Next
+	}
+	return true
+}
+
+func reverse(head *single.ListNode) *single.ListNode {
+	node := head
+	var prev *single.ListNode
+	var next *single.ListNode
+	for node != nil {
+		next = node.Next
+		node.Next = prev
+		prev = node
+		node = next
+	}
+	return prev
+}
+
+// TODO need to understand
+func isPalindromeTheBest(head *single.ListNode) bool {
+	head, slow, fast := nil, head, head
+	for fast != nil && fast.Next != nil {
+		head, slow.Next, slow, fast = slow, head, slow.Next, fast.Next.Next
+	}
+	if fast != nil {
+		slow = slow.Next
+	}
+	for ; head != nil && head.Val == slow.Val; head, slow = head.Next, slow.Next {
+	}
+	return head == nil
 }
---
+++ leet/0234_palindrome_linked_list/palindrome_linked_list_test.go
@@ -0,0 +1,28 @@
+package _234_palindrome_linked_list
+
+import (
+	"github.com/babadro/algorithms-go/03_StacksAndQueues/04_LinkedList/single"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	cases := []struct {
+		input    []int
+		expected bool
+	}{
+		{[]int{}, true},
+		{[]int{1}, true},
+		{[]int{1, 1}, true},
+		{[]int{1, 2}, false},
+		{[]int{1, 2, 3, 4}, false},
+		{[]int{1, 2, 3, 4, 3, 2, 1}, true},
+		{[]int{1, 2, 3, 3, 2, 1}, true},
+	}
+
+	for i, c := range cases {
+		head := single.ArrToLinkedList(c.input)
+		if fact := isPalindrome(head); fact != c.expected {
+			t.Errorf("case#%d, want %t, got %t", i+1, c.expected, fact)
+		}
+	}
+}
