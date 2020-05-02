06cc00ce18103cd45bcd5ccfc2cf63d5719e9945
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-02 07:29:52 +0300

    power of three

--- leet/0326_power_of_three/power_of_three.go
+++ leet/0326_power_of_three/power_of_three.go
@@ -1,20 +1,15 @@
 package _326_power_of_three
 
-// TODO 1
+func isPowerOfThree(n int) bool {
+	return n > 0 && 4052555153018976267%n == 0
+}
+
 func isPowerOfThreeNaive(n int) bool {
 	if n < 1 {
 		return false
 	}
-	if n == 1 {
-		return true
-	}
-	for {
-		if n%3 != 0 {
-			return false
-		}
+	for n%3 == 0 {
 		n = n / 3
-		if n == 1 {
-			return true
-		}
 	}
+	return n == 1
 }
