0895b47881dc355e53e6f6d4311ee9393a1a1ef9
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-02 06:37:26 +0300

    missing number

--- leet/0268_missing_number/missing_number.go
+++ leet/0268_missing_number/missing_number.go
@@ -1,12 +1,10 @@
 package _268_missing_number
 
-// TODO
 func missingNumber(nums []int) int {
 	sum := 0
 	for _, num := range nums {
 		sum += num
 	}
-	length := len(nums) - 1
-	expectedSum := (length * (length + 1)) / 2
+	expectedSum := (len(nums) * (len(nums) + 1)) / 2
 	return expectedSum - sum
 }
