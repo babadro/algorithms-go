387a604c155076cd57abda28d09685d8fe649e2e
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-08 08:43:15 +0300

    firstUniqChar completed

--- leet/0387_first_unique_character_in_string/first_unique_character_in_string.go
+++ leet/0387_first_unique_character_in_string/first_unique_character_in_string.go
@@ -1,6 +1,14 @@
 package _387_first_unique_character_in_string
 
-// TODO 1
 func firstUniqChar(s string) int {
-
+	arr := [26]int{}
+	for _, r := range s {
+		arr[r-'a']++
+	}
+	for i, r := range s {
+		if arr[r-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
 }
---
+++ leet/0387_first_unique_character_in_string/first_unique_character_in_string_test.go
@@ -0,0 +1,7 @@
+package _387_first_unique_character_in_string
+
+import "testing"
+
+func TestFirstUniqCharacter(t *testing.T) {
+	t.Log(firstUniqChar(""))
+}
