735a3338988c87175a07e599f19730cd7919e69d
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-11 10:09:11 +0300

    3sum in progress

--- leet/0015_3Sum/3Sum.go
+++ leet/0015_3Sum/3Sum.go
@@ -1,6 +1,41 @@
 package _015_3Sum
 
+import "sort"
+
 //TODO 1
 func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	length := removeDuplicates(nums)
+	nums = nums[:length]
+	next := tripletGenerator(nums)
+	res := make([][]int, 0)
+	for {
+		a, b, c, ok := next()
+		if !ok {
+			break
+		}
+		if a+b+c == 0 {
+			res = append(res, []int{a, b, c})
+		}
+	}
+	return res
+}
 
+func tripletGenerator(nums []int) func() (a, b, c int, ok bool) {
+	idxA, idxB, idxC, ok := 0, 0, 0, true
+	if len(nums) < 3 {
+		return 0, 0, 0, false
+	}
+	return nil
+}
+
+func removeDuplicates(nums []int) int {
+	lastPos := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[lastPos] {
+			nums[lastPos+1] = nums[i]
+			lastPos++
+		}
+	}
+	return lastPos + 1
 }
--- leet/0026_remove_duplicates/remove_duplicates_test.go
+++ leet/0026_remove_duplicates/remove_duplicates_test.go
@@ -3,7 +3,7 @@ package _026_remove_duplicates
 import "testing"
 
 func TestRemoveDuplicates(t *testing.T) {
-	arr := []int{1, 2, 3}
+	arr := []int{1, 2, 2, 2, 3}
 	length := removeDuplicates(arr)
 	t.Log(length, arr)
 }
