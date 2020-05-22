f095b4de5558fc8a7b0614e04726a4504c2c1efd
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-22 08:28:18 +0300

    search in rotated array completed

--- leet/0033_search_in_rotated_sorted_array/search_in_rotated_sorted_array.go
+++ leet/0033_search_in_rotated_sorted_array/search_in_rotated_sorted_array.go
@@ -1,1 +1,49 @@
 package _033_search_in_rotated_sorted_array
+
+import (
+	"sort"
+)
+
+// TODO 2 look for mor short solution. And, maybe, without sort.Search
+// like https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/307436/Faster-than-100-Go
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Search in Rotated Sorted Array.
+// Memory Usage: 2.6 MB, less than 50.00% of Go online submissions for Search in Rotated Sorted Array.
+func search(nums []int, target int) int {
+	l := len(nums)
+	if l == 0 {
+		return -1
+	}
+	if l == 1 {
+		if nums[0] == target {
+			return 0
+		} else {
+			return -1
+		}
+	}
+	leftIdx, rightIdx := 0, l-1
+	for rightIdx-leftIdx > 1 {
+		idx := ((rightIdx - leftIdx) / 2) + leftIdx
+		if nums[idx] < nums[leftIdx] {
+			rightIdx = idx
+		} else {
+			leftIdx = idx
+		}
+	}
+	if sliceIsSorted := nums[rightIdx] > nums[leftIdx]; sliceIsSorted {
+		return binarySearch(nums, target, 0)
+	}
+	if target <= nums[l-1] {
+		return binarySearch(nums[rightIdx:], target, rightIdx)
+	}
+	return binarySearch(nums[:rightIdx], target, 0)
+}
+
+func binarySearch(sliceOfNums []int, target int, offset int) int {
+	idx := sort.Search(len(sliceOfNums), func(i int) bool {
+		return sliceOfNums[i] >= target
+	})
+	if idx < len(sliceOfNums) && sliceOfNums[idx] == target {
+		return idx + offset
+	}
+	return -1
+}
--- leet/0033_search_in_rotated_sorted_array/search_in_rotated_sorted_array_test.go
+++ leet/0033_search_in_rotated_sorted_array/search_in_rotated_sorted_array_test.go
@@ -1,6 +1,29 @@
 package _033_search_in_rotated_sorted_array
 
-// TODO 1 binary search?
-func search(nums []int, target int) int {
+import "testing"
 
+func TestSearch(t *testing.T) {
+	cases := []struct {
+		nums     []int
+		target   int
+		expected int
+	}{
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 0, 4},
+		{[]int{}, 0, -1},
+		{[]int{1}, 0, -1},
+		{[]int{1}, 1, 0},
+		{[]int{2, 1}, 1, 1},
+		{[]int{3, 1, 2}, 2, 2},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 3, -1},
+		{[]int{1, 3}, 1, 0},
+		{[]int{1, 2, 3, 4}, 2, 1},
+		{[]int{3, 1}, 3, 0},
+		{[]int{3, 1, 2}, 3, 0},
+	}
+
+	for i, c := range cases {
+		if fact := search(c.nums, c.target); fact != c.expected {
+			t.Errorf("case#%d, want %d, got %d", i+1, c.expected, fact)
+		}
+	}
 }
