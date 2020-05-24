66dc2cd1c02e57d38ff1b9dd368f7144fd7d20fd
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-24 17:09:24 +0300

    rotate image completed

--- leet/0048_rotate_image/rotate_image.go
+++ leet/0048_rotate_image/rotate_image.go
@@ -1,18 +1,46 @@
 package _048_rotate_image
 
-// TODO 1
+// 100% 100%
+// TODO 2 need to understand rotate2 - it's faster than mine solution 6x times.
 func rotate(matrix [][]int) {
 	matrixSize := len(matrix)
-	for y := 0; y < matrixSize/2; y++ {
-		rowLen := matrixSize - 2*y
-		for x := 0; x < rowLen-1; x++ {
-			move(matrix, x, y)
+	newY, newX, x, y := 0, 0, 0, 0
+	for i := 0; i < matrixSize/2; i++ {
+		step := matrixSize - 2*i - 1
+		for j := 0; j < step; j++ {
+			y, x = i, i+j
+			var tmp int
+
+			// edge 1 => edge 2
+			newY, newX = y, i+step
+			if j > 0 {
+				newY = i + (j+step)%step
+			}
+			tmp, matrix[newY][newX] = matrix[newY][newX], matrix[y][x]
+			y, x = newY, newX
+
+			// edge 2 => edge 3
+			newY, newX = i+step, x
+			if j > 0 {
+				newX = i + step - (j+step)%step
+			}
+			tmp, matrix[newY][newX] = matrix[newY][newX], tmp
+			y, x = newY, newX
+
+			// edge 3 => edge 4
+			newY, newX = y, i
+			if j > 0 {
+				newY = i + step - (j+step)%step
+			}
+			tmp, matrix[newY][newX] = matrix[newY][newX], tmp
+			y, x = newY, newX
+
+			// edge 4 => edge 1
+			newY, newX = i, x
+			if j > 0 {
+				newX = i + (j+step)%step
+			}
+			matrix[newY][newX] = tmp
 		}
 	}
-}
-
-func move(matrix [][]int, x, y int) {
-	step, matrixSize := 3, 4
-	tmp, newX, newY := 0, 0, 0
-	matrix[y]
 }
--- leet/0048_rotate_image/rotate_image_test.go
+++ leet/0048_rotate_image/rotate_image_test.go
@@ -1,6 +1,48 @@
 package _048_rotate_image
 
-// TODO 1
-func rotate(matrix [][]int) {
+import "testing"
 
+// TODO 1
+func TestRotate(t *testing.T) {
+	_ = [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	input2 := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	input3 := [][]int{
+		{1},
+	}
+	_ = input2
+	rotate(input3)
+	t.Log(input3)
+}
+
+func BenchmarkMyRotate(b *testing.B) {
+	input2 := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	for i := 0; i < b.N; i++ {
+		rotate(input2)
+	}
+}
+
+func BenchmarkRotate2(b *testing.B) {
+	input2 := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16},
+	}
+	for i := 0; i < b.N; i++ {
+		rotate2(input2)
+	}
 }
