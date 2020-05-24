35173387d705ebb8405e3d814b981a782175a736
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-24 09:29:53 +0300

    rotate image in progress

--- leet/0048_rotate_image/rotate_image.go
+++ leet/0048_rotate_image/rotate_image.go
@@ -2,5 +2,17 @@ package _048_rotate_image
 
 // TODO 1
 func rotate(matrix [][]int) {
+	matrixSize := len(matrix)
+	for y := 0; y < matrixSize/2; y++ {
+		rowLen := matrixSize - 2*y
+		for x := 0; x < rowLen-1; x++ {
+			move(matrix, x, y)
+		}
+	}
+}
 
+func move(matrix [][]int, x, y int) {
+	step, matrixSize := 3, 4
+	tmp, newX, newY := 0, 0, 0
+	matrix[y]
 }
