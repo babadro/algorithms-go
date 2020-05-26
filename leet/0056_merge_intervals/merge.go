28076fa8d4e9477d4bcfc15c48df378d99376a3c
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-26 07:06:31 +0300

    merge completed

--- leet/0056_merge_intervals/merge.go
+++ leet/0056_merge_intervals/merge.go
@@ -1,6 +1,25 @@
 package _056_merge_intervals
 
-// TODO 1
-func merge(intervals [][]int) [][]int {
+import "sort"
 
+// 8 ms 96%, 4.7MB 100 %
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return nil
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	res := [][]int{intervals[0]}
+	for i := 1; i < len(intervals); i++ {
+		last := len(res) - 1
+		if res[last][1] >= intervals[i][0] {
+			if res[last][1] < intervals[i][1] {
+				res[last][1] = intervals[i][1]
+			}
+		} else {
+			res = append(res, intervals[i])
+		}
+	}
+	return res
 }
--- leet/0056_merge_intervals/merge_test.go
+++ leet/0056_merge_intervals/merge_test.go
@@ -1,1 +1,10 @@
 package _056_merge_intervals
+
+import "testing"
+
+func TestMerge(t *testing.T) {
+	t.Log(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
+	t.Log(merge([][]int{{1, 4}, {4, 5}}))
+	t.Log(merge([][]int{{1, 4}, {5, 6}}))
+	t.Log(merge([][]int{{1, 4}, {2, 3}}))
+}
