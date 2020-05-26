61804e5ecd206ae6f9f4482db6f8f2cb7ee7020e
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-26 06:32:54 +0300

    myPow completed

--- leet/0050_pow/pow.go
+++ leet/0050_pow/pow.go
@@ -1,7 +1,59 @@
 package _050_pow
 
-// TODO 1
-// Brutforce - cyclic multiplying?
-func myPow(x float64, n int) float64 {
+// Brutforce
+// Time Limit Exceeded 1.00000 -2147483648
+func myPowNaive(x float64, n int) float64 {
+	res := x
+	if n == 0 {
+		return 1
+	}
+	negative := n < 0
+	if negative {
+		n = -n
+	}
+	for n > 1 {
+		res *= x
+		n--
+	}
+	if negative {
+		return 1 / res
+	}
+	return res
+}
 
+// Best solution
+// TODO 2 need to understand
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		x, n = 1/x, -n
+	}
+	result := 1.0
+	for n > 0 {
+		if n&1 == 1 {
+			result *= x
+		}
+		x, n = x*x, n>>1
+	}
+	return result
+}
+
+func myPowRecursive(x float64, n int) float64 {
+
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+
+	if n < 0 {
+		return 1 / myPowRecursive(x, -n)
+	}
+
+	r := myPowRecursive(x, n/2)
+	if n%2 == 0 {
+		return r * r
+	} else {
+		return r * r * x
+	}
 }
--- leet/0050_pow/pow_test.go
+++ leet/0050_pow/pow_test.go
@@ -1,1 +1,30 @@
 package _050_pow
+
+import (
+	"math"
+	"testing"
+)
+
+func TestMyPow(t *testing.T) {
+	cases := []struct {
+		x float64
+		n int
+	}{
+		{2, 0},
+		{-2, 0},
+		{1, 1243123423},
+		{-2, 3},
+		{-1, 1243123423},
+		{-1, 1243123423},
+		{2.1, 3},
+		{2, -2},
+		{1.00000, -2147483648},
+	}
+
+	for i, c := range cases {
+		expected := math.Pow(c.x, float64(c.n))
+		if fact := myPow(c.x, c.n); fact != expected {
+			t.Errorf("case#%d, want %f, got %f", i+1, expected, fact)
+		}
+	}
+}
