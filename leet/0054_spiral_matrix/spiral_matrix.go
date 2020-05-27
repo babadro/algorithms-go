c087a428c0a23c04cb8c3ea3c0e79e202129d061
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-27 08:45:34 +0300

    spiral matrix in progress

--- leet/0054_spiral_matrix/spiral_matrix.go
+++ leet/0054_spiral_matrix/spiral_matrix.go
@@ -2,5 +2,37 @@ package _054_spiral_matrix
 
 // TODO 1
 func spiralOrder(matrix [][]int) []int {
-
+	m := len(matrix)
+	if m == 0 {
+		return nil
+	}
+	n := len(matrix[1])
+	minSize := n
+	if m < n {
+		minSize = m
+	}
+	res := make([]int, 0, m*n)
+	spiralCount := minSize/2 + minSize%2
+	l, h := m, n
+	x, y := 0, 0
+	for i := 0; i < spiralCount; i++ {
+		l, h = m-2*i, n-2*i
+		for j := 0; j < l-1; j++ {
+			res = append(res, matrix[y][x])
+			x++
+		}
+		for j := 0; j < h-1; j++ {
+			res = append(res, matrix[y][x])
+			y++
+		}
+		for j := 0; j < l-1; j++ {
+			res = append(res, matrix[y][x])
+			x--
+		}
+		for j := 0; j < h-1; j++ {
+			res = append(res, matrix[y][x])
+			y--
+		}
+	}
+	return res
 }
--- leet/0054_spiral_matrix/spiral_matrix_test.go
+++ leet/0054_spiral_matrix/spiral_matrix_test.go
@@ -1,1 +1,19 @@
 package _054_spiral_matrix
+
+import "testing"
+
+var input1 = [][]int{
+	{1, 2, 3},
+	{4, 5, 6},
+	{7, 8, 9},
+}
+var input2 = [][]int{
+	{5, 1, 9, 11},
+	{2, 4, 8, 10},
+	{13, 3, 6, 7},
+	{15, 14, 12, 16},
+}
+
+func TestSpiralOrder(t *testing.T) {
+	t.Log(spiralOrder(input1))
+}
