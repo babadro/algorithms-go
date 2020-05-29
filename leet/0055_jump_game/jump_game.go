8c8b1dab07e62d452a1e9b4492d1bdb8cd51bb43
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-29 09:35:31 +0300

    spiral order matrix completed

--- leet/0055_jump_game/jump_game.go
+++ leet/0055_jump_game/jump_game.go
@@ -1,6 +1,24 @@
 package _055_jump_game
 
-// TODO 1
+// TODO 1 - doesn't work. infinitive loop
 func canJump(nums []int) bool {
-
+	i := 0
+	last := len(nums) - 1
+	for i < last {
+		j := i + nums[i]
+		if j >= last {
+			return true
+		}
+		if nums[j] == 0 {
+			for j = j - 1; j > i; j-- {
+				if idx := j + nums[j]; idx >= last || nums[idx] != 0 {
+					i = idx
+					break
+				}
+			}
+		} else {
+			i = j
+		}
+	}
+	return true
 }
--- leet/0055_jump_game/jump_game_test.go
+++ leet/0055_jump_game/jump_game_test.go
@@ -1,1 +1,26 @@
 package _055_jump_game
+
+import "testing"
+
+func TestCanJump(t *testing.T) {
+	cases := []struct {
+		nums     []int
+		expected bool
+	}{
+		{[]int{1, 0, 2}, false},
+		{[]int{1}, true},
+		{[]int{0}, true},
+		{[]int{1, 0}, true},
+		{[]int{3, 0, 0, 0}, true},
+		{[]int{0, 0}, false},
+		{[]int{2, 3, 1, 1, 4}, true},
+		{[]int{3, 2, 1, 0, 4}, false},
+		{[]int{2, 5, 0, 0}, true},
+	}
+
+	for i, c := range cases {
+		if fact := canJump(c.nums); fact != c.expected {
+			t.Errorf("case#%d, want %t, got %t", i+1, c.expected, fact)
+		}
+	}
+}
