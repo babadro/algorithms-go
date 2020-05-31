a84d8119c30afde2c684dd53d65c8cf9ded3748f
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-05-31 07:10:14 +0300

    jump game works

--- leet/0055_jump_game/jump_game.go
+++ leet/0055_jump_game/jump_game.go
@@ -1,9 +1,78 @@
 package _055_jump_game
 
+const (
+	Unknown = iota
+	Good
+	Bad
+)
+
+var memo []int
+
+// Approach 3: Dynamic Programming Bottom-up
+// 5.17%, 14.29%
+func canJumpMemo(nums []int) bool {
+	memo = make([]int, len(nums))
+	memo[len(nums)-1] = Good
+	return canJumpFromPositionMemo(0, nums)
+}
+
+func canJumpFromPositionMemo(position int, nums []int) bool {
+	if v := memo[position]; v == Good {
+		return true
+	} else if v == Bad {
+		return false
+	}
+
+	furthestJump := position + nums[position]
+	if furthestJump >= len(nums)-1 {
+		return true
+	}
+	for nextPosition := position + 1; nextPosition <= furthestJump; nextPosition++ {
+		if canJumpFromPositionMemo(nextPosition, nums) {
+			memo[position] = Good
+			return true
+		}
+	}
+
+	memo[position] = Bad
+	return false
+}
+
+// Approach 1: Backtracking. Time limit exceeded
+func canJumpBacktracing(nums []int) bool {
+	return canJumpFromPositionBacktracing(0, nums)
+}
+
+func canJumpFromPositionBacktracing(position int, nums []int) bool {
+	if position == len(nums)-1 {
+		return true
+	}
+
+	furthestJump := position + nums[position]
+	if furthestJump >= len(nums)-1 {
+		return true
+	}
+	for nextPosition := furthestJump; nextPosition > position; nextPosition-- {
+		if canJumpFromPositionBacktracing(nextPosition, nums) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 // TODO 1 - doesn't work. infinitive loop
-func canJump(nums []int) bool {
+func canJumpOld(nums []int) bool {
 	i := 0
 	last := len(nums) - 1
+Loop:
 	for i < last {
 		j := i + nums[i]
 		if j >= last {
@@ -13,9 +82,10 @@ func canJump(nums []int) bool {
 			for j = j - 1; j > i; j-- {
 				if idx := j + nums[j]; idx >= last || nums[idx] != 0 {
 					i = idx
-					break
+					continue Loop
 				}
 			}
+			return false
 		} else {
 			i = j
 		}
--- leet/0055_jump_game/jump_game_test.go
+++ leet/0055_jump_game/jump_game_test.go
@@ -16,11 +16,21 @@ func TestCanJump(t *testing.T) {
 		{[]int{2, 3, 1, 1, 4}, true},
 		{[]int{3, 2, 1, 0, 4}, false},
 		{[]int{2, 5, 0, 0}, true},
+		{[]int{0, 0, 0}, false},
+		{[]int{2, 0, 0}, true},
+		{[]int{3, 0, 8, 2, 0, 0, 1}, true},
+		{bigInput, false},
 	}
 
 	for i, c := range cases {
 		if fact := canJump(c.nums); fact != c.expected {
 			t.Errorf("case#%d, want %t, got %t", i+1, c.expected, fact)
 		}
+	}
+}
+
+func BenchmarkCanJump(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = canJump(bigInput)
 	}
 }
