0f2d86768cb154082b7971e474972f2814a48a75
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-06-01 07:19:48 +0300

    unique paths in progress

--- leet/0062_unique_paths/unique_paths.go
+++ leet/0062_unique_paths/unique_paths.go
@@ -2,5 +2,19 @@ package _062_unique_paths
 
 // TODO 1
 func uniquePaths(m int, n int) int {
+	sequenceLen := m - 1 + n - 1
+	arr := make([]byte, sequenceLen)
 
+}
+
+func perm(nums []byte, res *int, l, r int) {
+	if l == r {
+		*res++
+	} else {
+		for i := l; i <= r; i++ {
+			nums[i], nums[l] = nums[l], nums[i]
+			perm(nums, res, l+1, r)
+			nums[i], nums[l] = nums[l], nums[i]
+		}
+	}
 }
