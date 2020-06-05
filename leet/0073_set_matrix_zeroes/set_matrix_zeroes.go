2f176f6d16f65f6a214d1308ab6d9833f0e38a67
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-06-05 16:43:55 +0300

    set_matrix_zeroes completed

--- leet/0073_set_matrix_zeroes/set_matrix_zeroes.go
+++ leet/0073_set_matrix_zeroes/set_matrix_zeroes.go
@@ -1,6 +1,39 @@
 package _073_set_matrix_zeroes
 
-// TODO 1
 func setZeros(matrix [][]int) {
+	row, col, zeroFound := 0, 0, false
+	for y := 0; y < len(matrix); y++ {
+		for x := 0; x < len(matrix[y]); x++ {
+			if matrix[y][x] == 0 {
+				if !zeroFound {
+					zeroFound = true
+					row, col = y, x
+				}
+				matrix[row][x] = 0
+				matrix[y][col] = 0
+			}
+		}
+	}
 
+	if !zeroFound {
+		return
+	}
+
+	for x := range matrix[row] {
+		if matrix[row][x] == 0 && x != col {
+			for y := 0; y < len(matrix); y++ {
+				matrix[y][x] = 0
+			}
+		}
+		matrix[row][x] = 0
+	}
+
+	for y := 0; y < len(matrix); y++ {
+		if matrix[y][col] == 0 {
+			for x := 0; x < len(matrix[y]); x++ {
+				matrix[y][x] = 0
+			}
+		}
+		matrix[y][col] = 0
+	}
 }
--- leet/0073_set_matrix_zeroes/set_matrix_zeroes_test.go
+++ leet/0073_set_matrix_zeroes/set_matrix_zeroes_test.go
@@ -1,1 +1,21 @@
 package _073_set_matrix_zeroes
+
+import "testing"
+
+func TestSetZeroes(t *testing.T) {
+	matrix := [][]int{
+		{0, 0, 0, 5},
+		{4, 3, 1, 4},
+		{0, 1, 1, 4},
+		{1, 2, 1, 3},
+		{0, 0, 1, 1},
+	}
+	_ = matrix
+	matrix2 := [][]int{
+		{1, 2, 3},
+		{1, 0, 3},
+		{1, 2, 3},
+	}
+	setZeros(matrix2)
+	t.Log(matrix2)
+}
