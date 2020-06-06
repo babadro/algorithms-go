6b5ff70117b0fac10140a83b1d70fccada0703c2
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-06-06 16:01:27 +0300

    sort_colors in progress

--- leet/0075_sort_colors/sort_colors.go
+++ leet/0075_sort_colors/sort_colors.go
@@ -2,5 +2,32 @@ package _075_sort_colors
 
 // TODO 1
 func sortColors(nums []int) {
-
+	if len(nums) == 0 {
+		return
+	}
+	j, k := 0, 0
+	for i, v := range nums {
+		if v != 0 {
+			j = i
+			break
+		}
+	}
+	for i := len(nums) - 1; i > j; i-- {
+		if nums[i] != 2 {
+			k = i
+			break
+		}
+	}
+	i := j
+	for i <= k {
+		if nums[i] == 1 {
+			i++
+		} else if nums[i] == 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		} else if nums[i] == 2 {
+			nums[i], nums[k] = nums[k], nums[i]
+			k--
+		}
+	}
 }
--- leet/0075_sort_colors/sort_colors_test.go
+++ leet/0075_sort_colors/sort_colors_test.go
@@ -1,1 +1,12 @@
 package _075_sort_colors
+
+import "testing"
+
+func TestSetColors(t *testing.T) {
+	//input := []int{0,1,0,1,0,1,0,2,2,0,0,0}
+	//sortColors(input)
+	//t.Log(input)
+	input2 := []int{2, 2, 1, 1, 0, 0}
+	sortColors(input2)
+	t.Log(input2)
+}
