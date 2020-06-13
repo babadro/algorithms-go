1102620a22460ebba6d937158b154354f7d9579d
babadro <babadro@gmail.com>
2020-06-13 16:27:46 +0300

    btree title completed

--- leet/0563_binary_tree_tilt/binary_tree_tilt.go
+++ leet/0563_binary_tree_tilt/binary_tree_tilt.go
@@ -1,3 +1,29 @@
 package _563_binary_tree_tilt
 
-func postOrder(node *Tree.Node)
+import (
+	"github.com/babadro/algorithms-go/04_TreesAndGraphs/btree"
+)
+
+func findTilt(root *btree.Node) int {
+	tiltSum := 0
+	postOrder(root, &tiltSum)
+	return tiltSum
+}
+
+func postOrder(node *btree.Node, tiltSum *int) (sumValues int) {
+	if node != nil {
+		leftSumValues := postOrder(node.Left, tiltSum)
+		rightSumValues := postOrder(node.Right, tiltSum)
+		*tiltSum += mod(leftSumValues, rightSumValues)
+		return leftSumValues + rightSumValues + node.Val
+	}
+	return 0
+}
+
+func mod(a, b int) int {
+	res := a - b
+	if res < 0 {
+		return -res
+	}
+	return res
+}
--- leet/0563_binary_tree_tilt/binary_tree_tilt_test.go
+++ leet/0563_binary_tree_tilt/binary_tree_tilt_test.go
@@ -1,1 +1,18 @@
 package _563_binary_tree_tilt
+
+import (
+	"github.com/babadro/algorithms-go/04_TreesAndGraphs/btree"
+	"testing"
+)
+
+func TestFindTilt(t *testing.T) {
+	root := btree.ArrayToBinaryTree([]int{1, 2, 3, 4, 5, 6, 7})
+	t.Log(findTilt(root))
+}
+
+func TestPostOrder(t *testing.T) {
+	root := btree.ArrayToBinaryTree([]int{1, 2, 3, 4, 5, 6, 7})
+	tiltSum := 0
+	valuesSum := postOrder(root, &tiltSum)
+	t.Log("tiltSum: ", tiltSum, " valuesSum: ", valuesSum)
+}
