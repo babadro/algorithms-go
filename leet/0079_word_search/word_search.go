b01389956a309ddd4597e7e3da4fde92ca424231
babadro <babadro@gmail.com>
2020-06-20 07:46:54 +0300

    exists good solution

--- leet/0079_word_search/word_search.go
+++ leet/0079_word_search/word_search.go
@@ -1,17 +1,15 @@
 package _079_word_search
 
-// 26%, 54%
-// TODO 2 look discuss for best solution
+// 96% 84%
 func exist(board [][]byte, word string) bool {
 	m, n := len(board), len(board[0])
 	totalCount := m * n
 	if len(word) > totalCount {
 		return false
 	}
-	visited := make(map[key]bool)
 	for y := range board {
 		for x := range board[y] {
-			if success := search(y, x, m, n, 0, word, board, visited); success {
+			if success := search(y, x, m, n, 0, word, board); success {
 				return true
 			}
 		}
@@ -19,33 +17,26 @@ func exist(board [][]byte, word string) bool {
 	return false
 }
 
-func search(y, x, m, n, idx int, word string, board [][]byte, visited map[key]bool) bool {
+func search(y, x, m, n, idx int, word string, board [][]byte) bool {
 	if y < 0 || y >= m || x < 0 || x >= n {
 		return false
 	}
-	if board[y][x] != word[idx] {
-		return false
-	}
-	k := key{byte(x), byte(y)}
-	if visited[k] {
+	char := word[idx]
+	if board[y][x] != char {
 		return false
 	}
 	idx++
 	if idx == len(word) {
 		return true
 	}
-	visited[k] = true
-	result := search(y-1, x, m, n, idx, word, board, visited) ||
-		search(y+1, x, m, n, idx, word, board, visited) ||
-		search(y, x-1, m, n, idx, word, board, visited) ||
-		search(y, x+1, m, n, idx, word, board, visited)
+	board[y][x] = 0
+	result := search(y-1, x, m, n, idx, word, board) ||
+		search(y+1, x, m, n, idx, word, board) ||
+		search(y, x-1, m, n, idx, word, board) ||
+		search(y, x+1, m, n, idx, word, board)
 
 	if !result {
-		visited[k] = false
+		board[y][x] = char
 	}
 	return result
-}
-
-type key struct {
-	x, y byte
 }
