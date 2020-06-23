75feff76febaeab7d12b547a4db528aae92fa0f8
babadro <babadro@gmail.com>
2020-06-23 08:27:29 +0300

    decode ways in progress

--- leet/0091_decode_ways/decode_ways.go
+++ leet/0091_decode_ways/decode_ways.go
@@ -2,5 +2,17 @@ package _091_decode_ways
 
 // TODO 1
 func numDecodings(s string) int {
+	return decode(s, len(s), 0, 1)
+}
 
+func decode(s string, n, idx, counter int) int {
+	if idx >= n-1 {
+		return counter
+	}
+	counter += decode(s, n, idx+1, counter)
+	curr, next := s[idx], s[idx+1]
+	if curr == '1' || (curr == '2' && next >= '0' && next <= '6') {
+		counter += decode(s, n, idx+2, counter+1)
+	}
+	return counter
 }
--- leet/0091_decode_ways/decode_ways_test.go
+++ leet/0091_decode_ways/decode_ways_test.go
@@ -1,1 +1,19 @@
 package _091_decode_ways
+
+import "testing"
+
+func TestNumDecodings(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected int
+	}{
+		{"12", 2},
+		{"226", 3},
+	}
+
+	for i, c := range cases {
+		if fact := numDecodings(c.s); fact != c.expected {
+			t.Errorf("case#%d, want %d, got %d", i+1, c.expected, fact)
+		}
+	}
+}
