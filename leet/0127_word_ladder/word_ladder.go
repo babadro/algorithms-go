527efb0dad1485b9271323db7dbf2994500133d4
babadro <babadro@gmail.com>
2020-06-23 07:05:51 +0300

    ladder fails

--- leet/0127_word_ladder/word_ladder.go
+++ leet/0127_word_ladder/word_ladder.go
@@ -1,24 +1,23 @@
 package _127_word_ladder
 
-// TODO 1
+import "log"
+
+var iteration int
+
+// TODO 1 look solution
 func ladderLength(beginWord string, endWord string, wordList []string) int {
-	//found := false
-	//for _, str := range wordList {
-	//	if endWord == str {}
-	//	found = true
-	//}
-	//if !found {
-	//	return 0
-	//}
-	//if haveOneDifferentChar(beginWord, endWord) {
-	//	return 1
-	//}
 	res := 0
 	search(wordList, beginWord, endWord, &res, 0)
-	return res
+	if res == 0 {
+		return 0
+	}
+	return res + 1
 }
 
 func haveOneDifferentChar(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
 	diffFound := false
 	for i := 0; i < len(str1); i++ {
 		if str1[i] != str2[i] {
@@ -35,6 +34,11 @@ func search(wordlist []string, word, endWord string, res *int, counter int) {
 	if word == "" {
 		return
 	}
+	iteration++
+	if iteration >= 10000000 {
+		log.Fatal("Time limit exceed")
+	}
+	counter++
 	for i, w := range wordlist {
 		if haveOneDifferentChar(word, w) {
 			if w == endWord {
@@ -43,8 +47,11 @@ func search(wordlist []string, word, endWord string, res *int, counter int) {
 				}
 				return
 			}
+			if *res != 0 && counter >= *res {
+				return
+			}
 			wordlist[i] = ""
-			search(wordlist, w, endWord, res, counter+1)
+			search(wordlist, w, endWord, res, counter)
 			wordlist[i] = w
 		}
 	}
--- leet/0127_word_ladder/word_ladder_test.go
+++ leet/0127_word_ladder/word_ladder_test.go
@@ -8,7 +8,15 @@ func TestLadderLength(t *testing.T) {
 		wordList           []string
 		expected           int
 	}{
-		{"hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}, 5},
+		//{"hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}, 5},
+		//{"hit", "cog", []string{"hot","dot","dog","lot","log"}, 0},
+		//{"hita", "higa", []string{"higa"}, 2},
+		//{"hivo", "higa", []string{"higa"}, 0},
+		//{"a", "b", []string{"c"}, 0},
+		//{"a", "b", []string{"b", "c"}, 2},
+		//{"a", "b", []string{"b", "c"}, 2},
+		//{"alloc", "aldrb", []string{"bllac", "allac", "aldoc", "aldob", "aldrb"}, 4},
+		{"qa", "sq", []string{"si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"}, 2},
 	}
 
 	for i, c := range cases {
@@ -17,3 +25,9 @@ func TestLadderLength(t *testing.T) {
 		}
 	}
 }
+
+/* time limit exceed
+"qa"
+"sq"
+["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]
+*/
