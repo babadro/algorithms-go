67396b964b103b715d8fa168e63555f2f34f65a5
babadro <babadro@gmail.com>
2020-06-24 16:20:50 +0300

    decode ways completed

--- leet/0091_decode_ways/decode_ways.go
+++ leet/0091_decode_ways/decode_ways.go
@@ -1,7 +1,41 @@
 package _091_decode_ways
 
-// TODO 1
+// 100%, 93%
 func numDecodings(s string) int {
+	n := len(s)
+	if s[0] == '0' {
+		return 0
+	}
+	arr := make([]int, n)
+	if last := n - 1; s[last] != '0' {
+		arr[last] = 1
+	}
+	for i := n - 2; i >= 0; i-- {
+		if nextNum := arr[i+1]; nextNum == 0 {
+			arr[i] = 1
+		} else {
+			arr[i] = nextNum
+		}
+		currChar, nextChar := s[i], s[i+1]
+		if nextChar == '0' {
+			if currChar == '0' || currChar > '2' {
+				return 0
+			}
+			continue
+		}
+		idx := i + 2
+		if (currChar == '1' && nextChar > '0') || (currChar == '2' && nextChar > '0' && nextChar <= '6') {
+			if idx >= n {
+				arr[i] = 2
+			} else {
+				arr[i] += arr[idx]
+			}
+		}
+	}
+	return arr[0]
+}
+
+func numDecodingsRecursive(s string) int {
 	counter := 1
 	decode(s, len(s), 0, &counter)
 	return counter
@@ -13,7 +47,7 @@ func decode(s string, n, idx int, counter *int) {
 	}
 	decode(s, n, idx+1, counter)
 	curr, next := s[idx], s[idx+1]
-	if curr == '1' || (curr == '2' && next >= '0' && next <= '6') {
+	if curr == '1' || (curr == '2' && next >= '0' && next <= '6') { // incorrect conditional.
 		*counter++
 		decode(s, n, idx+2, counter)
 	}
