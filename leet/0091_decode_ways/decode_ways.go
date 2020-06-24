7f45f5e2c5db74fe3ee82bd93bbf7dd5276e2629
babadro <babadro@gmail.com>
2020-06-24 14:46:10 +0300

    decode ways works, but small input only

--- leet/0091_decode_ways/decode_ways.go
+++ leet/0091_decode_ways/decode_ways.go
@@ -2,17 +2,19 @@ package _091_decode_ways
 
 // TODO 1
 func numDecodings(s string) int {
-	return decode(s, len(s), 0, 1)
+	counter := 1
+	decode(s, len(s), 0, &counter)
+	return counter
 }
 
-func decode(s string, n, idx, counter int) int {
+func decode(s string, n, idx int, counter *int) {
 	if idx >= n-1 {
-		return counter
+		return
 	}
-	counter += decode(s, n, idx+1, counter)
+	decode(s, n, idx+1, counter)
 	curr, next := s[idx], s[idx+1]
 	if curr == '1' || (curr == '2' && next >= '0' && next <= '6') {
-		counter += decode(s, n, idx+2, counter+1)
+		*counter++
+		decode(s, n, idx+2, counter)
 	}
-	return counter
 }
