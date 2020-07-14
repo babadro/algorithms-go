16387f54ac3ba909475b38b81e160c4904c105eb
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-07-14 08:05:11 +0300

    course schedule 2 doesn't work

--- leet/0210_course_schedule_2/course_schedule_2.go
+++ leet/0210_course_schedule_2/course_schedule_2.go
@@ -1,6 +1,73 @@
 package _210_course_schedule_2
 
-// TODO 1
+// TODO 1 doesn't work
+// input 3, [[0,1],[0,2],[1,2]]
+// output [2, 0, 1]
+// expected [2, 1, 0]
 func findOrder(numCourses int, prerequisites [][]int) []int {
+	set := make(map[int]int, numCourses)
+	for _, arr := range prerequisites {
+		set[arr[0]] = arr[1]
+	}
+	var res []int
+	done := make(map[int]bool, numCourses)
+	for i := 0; i < numCourses; i++ {
+		if _, ok := set[i]; !ok {
+			res = append(res, i)
+			done[i] = true
+		}
+	}
+	if len(res) == 0 {
+		return nil
+	}
+
+	for len(set) > 0 {
+		k, deleteKey := 0, false
+		for course, prerequisite := range set {
+			if done[prerequisite] {
+				done[course] = true
+				res = append(res, course)
+				k, deleteKey = course, true
+				break
+			}
+		}
+		if !deleteKey {
+			return nil
+		}
+		delete(set, k)
+	}
+
+	if len(res) < numCourses {
+		return nil
+	}
+	return res
+}
+
+/*
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	graph := make([][]int, numCourses)
+	for _, arr := range prerequisites {
+		graph[arr[0]] = append(graph[arr[0]], arr[1])
+	}
+
+	counter := 0
+	courses := make(map[int]bool)
+
+	for i, arr := range graph {
+		for _, j := range arr {
+			visited := make(map[int]bool)
+			if visited[j] {
+				return nil
+			}
+			visited[j] = true
+
+		}
+	}
 
 }
+
+func add(v int, res *[]int, graph [][]int, visited, courses map[int]bool) {
+	if len(graph[v]) == 0 {
+
+	}
+}*/
--- leet/0210_course_schedule_2/course_schedule_2_test.go
+++ leet/0210_course_schedule_2/course_schedule_2_test.go
@@ -1,1 +1,21 @@
 package _210_course_schedule_2
+
+import "testing"
+
+func TestFindOrder(t *testing.T) {
+	cases := []struct {
+		numCourses    int
+		prerequisites [][]int
+	}{
+		{4, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}}},
+	}
+	_ = cases
+	//for i, c := range cases {
+	//	res := findOrder(c.numCourses, c.prerequisites)
+	//	if
+	//}
+}
+
+func TestFindOrder2(t *testing.T) {
+	t.Log(findOrder(4, [][]int{{1, 0}, {2, 0}, {3, 1}, {3, 2}}))
+}
