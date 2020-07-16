372cc0008bc4d48f9e2b34e239d41ad738bf52f7
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-07-16 08:04:56 +0300

    lru cache doesn't work

--- leet/0146_lru_cache/lru_cache.go
+++ leet/0146_lru_cache/lru_cache.go
@@ -1,1 +1,75 @@
 package _146_lru_cache
+
+import "fmt"
+
+// TODO 1
+
+type item struct {
+	payload, lastUsage int
+}
+
+type LRUCache struct {
+	newest       int
+	counterToKey map[int]int
+	storage      map[int]item
+	oldest       int
+	size         int
+	capacity     int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{counterToKey: make(map[int]int, capacity), storage: make(map[int]item, capacity)}
+}
+
+func (this *LRUCache) Get(key int) int {
+	if it, ok := this.storage[key]; ok {
+		delete(this.counterToKey, it.lastUsage)
+		this.newest++
+		this.counterToKey[this.newest] = key
+		if this.oldest == it.lastUsage {
+			this.findNextOldest()
+		}
+		this.storage[key] = item{payload: it.payload, lastUsage: this.newest}
+		return it.payload
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.storage[key]; ok {
+		return
+	}
+
+	if this.size < this.capacity {
+		if this.size == 0 {
+			this.oldest = 1
+			this.counterToKey[1] = key
+		}
+		this.size++
+	} else {
+		keyToDelete := this.counterToKey[this.oldest]
+		delete(this.storage, keyToDelete)
+		delete(this.counterToKey, this.oldest)
+		this.findNextOldest()
+	}
+	this.newest++
+	this.storage[key] = item{payload: value, lastUsage: this.newest}
+	this.counterToKey[this.newest] = key
+}
+
+func (this *LRUCache) findNextOldest() {
+	for {
+		this.oldest++
+		fmt.Println(this.oldest)
+		if _, ok := this.counterToKey[this.oldest]; ok {
+			break
+		}
+	}
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,item);
+ */
--- leet/0146_lru_cache/lru_cache_test.go
+++ leet/0146_lru_cache/lru_cache_test.go
@@ -1,25 +1,16 @@
 package _146_lru_cache
 
-// TODO 1
+import "testing"
 
-type LRUCache struct {
+func TestLruCache(t *testing.T) {
+	cache := Constructor(2)
+
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	t.Log(cache.Get(1)) // returns 1
+	cache.Put(3, 3)     // evicts key 2
+	t.Log(cache.Get(2)) // returns -1 (not found)
+	cache.Put(4, 4)     // evicts key 1
+	t.Log(cache.Get(1)) // returns -1 (not found)
+	t.Log(cache.Get(3)) // returns 3
 }
-
-func Constructor(capacity int) LRUCache {
-
-}
-
-func (this *LRUCache) Get(key int) int {
-
-}
-
-func (this *LRUCache) Put(key int, value int) {
-
-}
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * obj := Constructor(capacity);
- * param_1 := obj.Get(key);
- * obj.Put(key,value);
- */
