dd65fa349763e02a61d318f9ce2d92b5b351129c
petrovich.andrey <petrovich.andrey@wildberries.ru>
2020-07-19 08:43:43 +0300

    graph refactoring

--- 04_TreesAndGraphs/adjgraph/adjgraph.go
+++ 04_TreesAndGraphs/adjgraph/adjgraph.go
@@ -6,51 +6,55 @@ import (
 	"strings"
 )
 
-type AdjListNode struct {
-	Vertex int
-	Next   *AdjListNode
-}
-
-func NewAdjListNode(dest int) *AdjListNode {
-	return &AdjListNode{dest, nil}
-}
-
 type Graph struct {
-	V     int
-	Array []*AdjListNode
+	V   int
+	adj [][]int
 }
 
 func New(V int) *Graph {
-	return &Graph{V, make([]*AdjListNode, V)}
+	return &Graph{V, make([][]int, V)}
 }
 
 // Direction graph
-func (g *Graph) AddEdge(src, dest int) {
-	newNode := NewAdjListNode(dest)
-	newNode.Next = g.Array[src]
-	g.Array[src] = newNode
+func (g *Graph) AddEdge(src, dst int) {
+	g.adj[src] = append(g.adj[src], dst)
+	// g.adj[dst] = append(g.adj[dst], src) non direction graph
 }
 
-func (g *Graph) BFS(s int) {
-	visited := make(map[int]bool, g.V)
+func (g *Graph) BFS(v int) {
+	visited := make([]bool, g.V)
 
 	q := queue.New(g.V)
 
-	visited[s] = true
-	q.Enqueue(s)
+	visited[v] = true
+	q.Enqueue(v)
 
 	for q.Len() != 0 {
-		s, _ = q.Dequeue()
-		fmt.Print(s, " ")
+		v, _ = q.Dequeue()
+		fmt.Print(v, " ")
 
-		node := g.Array[s]
-		for node != nil {
-			n := node.Vertex
+		for _, n := range g.adj[v] {
 			if !visited[n] {
 				visited[n] = true
 				q.Enqueue(n)
 			}
-			node = node.Next
+		}
+	}
+}
+
+func (g *Graph) DFS(i int) {
+	visited := make([]bool, g.V)
+	g.dfsUtil(i, visited)
+}
+
+func (g *Graph) dfsUtil(v int, visited []bool) {
+	visited[v] = true
+	fmt.Print(v, " ")
+
+	vList := g.adj[v]
+	for _, n := range vList {
+		if !visited[n] {
+			g.dfsUtil(n, visited)
 		}
 	}
 }
@@ -59,11 +63,10 @@ func (g *Graph) String() string {
 	var str strings.Builder
 	var v int
 	for v = 0; v < g.V; v++ {
-		pCrawl := g.Array[v]
-		str.WriteString(fmt.Sprintf("\n Ajacency list of vertex %d\n head ", v))
-		for pCrawl != nil {
-			str.WriteString(fmt.Sprintf("-> %d", pCrawl.Vertex))
-			pCrawl = pCrawl.Next
+		pCrawl := g.adj[v]
+		str.WriteString(fmt.Sprintf("\n Ajacency list of n %d\n head ", v))
+		for _, n := range pCrawl {
+			str.WriteString(fmt.Sprintf("-> %d", n))
 		}
 		str.WriteString("\n")
 	}
--- 04_TreesAndGraphs/adjgraph/adjgraph_test.go
+++ 04_TreesAndGraphs/adjgraph/adjgraph_test.go
@@ -30,3 +30,15 @@ func TestBFS(t *testing.T) {
 
 	g.BFS(2)
 }
+
+func TestDFS(t *testing.T) {
+	g := New(4)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 2)
+	g.AddEdge(2, 0)
+	g.AddEdge(2, 3)
+	g.AddEdge(3, 3)
+
+	g.DFS(2)
+}
