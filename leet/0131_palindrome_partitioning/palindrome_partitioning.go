a105497915b262a15096437e3086fbded0d5a221
petrovich.andrey <babadro@gmail.com>
2020-07-28 09:01:52 +0300

    palindrome partitioning in progress

--- leet/0131_palindrome_partitioning/palindrome_partitioning.go
+++ leet/0131_palindrome_partitioning/palindrome_partitioning.go
@@ -1,6 +1,34 @@
 package _131_palindrome_partitioning
 
+import "strings"
+
 // TODO 1
 func partition(s string) [][]string {
+	if len(s) == 0 {
+		return nil
+	}
 
+	runes := []rune(s)
+	n := len(runes)
+	for i := 0; i < n; i++ {
+
+	}
+
+}
+
+func recursive(r []rune) []rune {
+	if len(r) == 0 {
+		return r
+	}
+	return append([]rune{r[0]}, recursive(r[1:])...)
+}
+
+func isPalindrome(r []rune) bool {
+	n := len(r)
+	for i := 0; i < n/2; i++ {
+		if r[i] != r[n-1-i] {
+			return false
+		}
+	}
+	return true
 }
