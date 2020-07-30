cdbd991118ae04ec0a7c5825239a328487cc085f
petrovich.andrey <babadro@gmail.com>
2020-07-30 08:37:06 +0300

    merge sort completed

---
+++ sorting/mergeSort/mergeSort.go
@@ -0,0 +1,47 @@
+package mergeSort
+
+func merge(arr []int, l, m, r int) {
+	n1 := m - l + 1
+	n2 := r - m
+
+	L, R := make([]int, n1), make([]int, n2)
+
+	copy(L, arr[l:m+1])
+	copy(R, arr[m+1:r+1])
+
+	i, j := 0, 0
+	k := l
+	for i < n1 && j < n2 {
+		if L[i] <= R[j] {
+			arr[k] = L[i]
+			i++
+		} else {
+			arr[k] = R[j]
+			j++
+		}
+		k++
+	}
+
+	for i < n1 {
+		arr[k] = L[i]
+		i++
+		k++
+	}
+
+	for j < n2 {
+		arr[k] = R[j]
+		j++
+		k++
+	}
+}
+
+func mergeSort(arr []int, l, r int) {
+	if l < r {
+		m := l + (r-l)/2
+
+		mergeSort(arr, l, m)
+		mergeSort(arr, m+1, r)
+
+		merge(arr, l, m, r)
+	}
+}
