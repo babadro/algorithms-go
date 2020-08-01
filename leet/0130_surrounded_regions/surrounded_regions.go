4868e19129871e423d0a70422c5ebfd68edf80bc
petrovich.andrey <babadro@gmail.com>
2020-08-01 08:14:07 +0300

    surrounded regions completed

--- leet/0130_surrounded_regions/surrounded_regions.go
+++ leet/0130_surrounded_regions/surrounded_regions.go
@@ -1,7 +1,44 @@
 package _130_surrounded_regions
 
-// TODO 1
+// 96% 83%
 func solve(board [][]byte) {
+	height := len(board)
+	if height == 0 {
+		return
+	}
+	width := len(board[0])
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if (y == 0 || y == height-1 || x == 0 || x == width-1) && board[y][x] == 'O' {
+				crawl(board, x, y)
+			}
+		}
+	}
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if board[y][x] == 'O' {
+				board[y][x] = 'X'
+			} else if board[y][x] == 'N' {
+				board[y][x] = 'O'
+			}
+		}
+	}
+}
+
+func crawl(board [][]byte, x, y int) {
+	if x >= len(board[0]) || x < 0 || y >= len(board) || y < 0 || board[y][x] != 'O' {
+		return
+	}
+	board[y][x] = 'N'
+	crawl(board, x-1, y)
+	crawl(board, x+1, y)
+	crawl(board, x, y-1)
+	crawl(board, x, y+1)
+}
+
+// Doesn't work
+func solve1(board [][]byte) {
 	height := len(board)
 	if height == 0 {
 		return
