89297682181df89d520b6546bd4469e1c035b8f6
petrovich.andrey <babadro@gmail.com>
2020-08-01 09:33:07 +0300

    string partition

--- leet/0131_palindrome_partitioning/palindrome_partitioning.go
+++ leet/0131_palindrome_partitioning/palindrome_partitioning.go
@@ -1,27 +1,27 @@
 package _131_palindrome_partitioning
 
 // 18 % 20%
+// TODO 2 look for fast solution in discuss
 func partition(s string) [][]string {
 	if len(s) == 0 {
 		return nil
 	}
 
-	runes := []rune(s)
-	res := make([][][]string, len(runes))
+	res := make([][][]string, len(s))
 
-	last := len(runes) - 1
+	last := len(s) - 1
 
-	res[last] = [][]string{{string(runes[last])}}
+	res[last] = [][]string{{string(s[last])}}
 
-	for i := len(runes) - 2; i >= 0; i-- {
-		for end := len(runes); end > i; end-- {
-			candidate := runes[i:end]
+	for i := len(s) - 2; i >= 0; i-- {
+		for end := len(s); end > i; end-- {
+			candidate := s[i:end]
 			if isPalindrome(candidate) {
-				if end == len(runes) {
-					res[i] = append(res[i], [][]string{{string(candidate)}}...)
+				if end == len(s) {
+					res[i] = append(res[i], [][]string{{candidate}}...)
 				} else {
 					for _, arr := range res[end] {
-						newArr := append([]string{string(candidate)}, arr...)
+						newArr := append([]string{candidate}, arr...)
 						res[i] = append(res[i], newArr)
 					}
 				}
@@ -33,10 +33,10 @@ func partition(s string) [][]string {
 	return res[0]
 }
 
-func isPalindrome(r []rune) bool {
-	n := len(r)
+func isPalindrome(s string) bool {
+	n := len(s)
 	for i := 0; i < n/2; i++ {
-		if r[i] != r[n-1-i] {
+		if s[i] != s[n-1-i] {
 			return false
 		}
 	}
