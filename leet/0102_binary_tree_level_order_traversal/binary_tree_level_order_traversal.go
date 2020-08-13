f0171241877d5443f64877dc29950bb9e628b88b
petrovich.andrey <babadro@gmail.com>
2020-08-13 07:32:02 +0300

    order traversal completed

--- leet/0102_binary_tree_level_order_traversal/binary_tree_level_order_traversal.go
+++ leet/0102_binary_tree_level_order_traversal/binary_tree_level_order_traversal.go
@@ -2,7 +2,37 @@ package _102_binary_tree_level_order_traversal
 
 import "github.com/babadro/algorithms-go/04_TreesAndGraphs/btree"
 
-// TODO 1
+// 100% 66%
+// TODO 2 find better solution in discuss
 func levelOrder(root *btree.Node) [][]int {
+	if root == nil {
+		return nil
+	}
+	nodeArrays := [][]*btree.Node{{root}}
+	for {
+		last := len(nodeArrays) - 1
+		var arr []*btree.Node
+		for _, node := range nodeArrays[last] {
+			if node.Left != nil {
+				arr = append(arr, node.Left)
+			}
+			if node.Right != nil {
+				arr = append(arr, node.Right)
+			}
+		}
+		if arr == nil {
+			break
+		}
+		nodeArrays = append(nodeArrays, arr)
+	}
 
+	res := make([][]int, len(nodeArrays))
+	for i := range nodeArrays {
+		res[i] = make([]int, len(nodeArrays[i]))
+		for j := range nodeArrays[i] {
+			res[i][j] = nodeArrays[i][j].Val
+		}
+	}
+
+	return res
 }
--- leet/0102_binary_tree_level_order_traversal/binary_tree_level_order_traversal_test.go
+++ leet/0102_binary_tree_level_order_traversal/binary_tree_level_order_traversal_test.go
@@ -1,1 +1,19 @@
 package _102_binary_tree_level_order_traversal
+
+import (
+	"github.com/babadro/algorithms-go/04_TreesAndGraphs/btree"
+	"testing"
+)
+
+func TestLevelOrder(t *testing.T) {
+	root := btree.ArrayToBinaryTree([]int{3, 9, 20, btree.Null, btree.Null, 15, 7})
+	t.Log(levelOrder(root))
+	root2 := &btree.Node{Val: 1}
+	n2 := &btree.Node{Val: 2}
+	n3 := &btree.Node{Val: 3}
+	n4 := &btree.Node{Val: 4}
+	root2.Right = n2
+	n2.Right = n3
+	n3.Right = n4
+	t.Log(levelOrder(root2))
+}
