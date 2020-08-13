fef57d30a9a187e5fc639be7acb47ba0d4d21070
petrovich.andrey <babadro@gmail.com>
2020-08-13 08:09:15 +0300

    gas stations completed

--- leet/0134_gas_station/gas_station.go
+++ leet/0134_gas_station/gas_station.go
@@ -1,7 +1,21 @@
 package _134_gas_station
 
-// TODO 1 попробовать брутфорс для начала
-// пробуем делать круг с каждого элемента
+// 20% 100%. This is a brutforce
+// TODO 2 find quickier solution in discuss
 func canCompleteCircuit(gas []int, cost []int) int {
-
+	n := len(gas)
+Loop:
+	for start := 0; start < n; start++ {
+		tank := 0
+		for j := 0; j < n; j++ {
+			idx := (start + j) % n
+			tank += gas[idx]
+			tank -= cost[idx]
+			if tank < 0 {
+				continue Loop
+			}
+		}
+		return start
+	}
+	return -1
 }
--- leet/0134_gas_station/gas_station_test.go
+++ leet/0134_gas_station/gas_station_test.go
@@ -1,1 +1,36 @@
 package _134_gas_station
+
+import "testing"
+
+func TestCanCompleterCircuit(t *testing.T) {
+	var cases = []struct {
+		gas, cost []int
+		expected  int
+	}{
+		{
+			gas:      []int{1, 2, 3, 4, 5},
+			cost:     []int{3, 4, 5, 1, 2},
+			expected: 3,
+		},
+		{
+			gas:      []int{1},
+			cost:     []int{1},
+			expected: 0,
+		},
+		{
+			gas:      []int{1},
+			cost:     []int{2},
+			expected: -1,
+		},
+		{
+			gas:      []int{1, 2},
+			cost:     []int{2, 1},
+			expected: 1,
+		},
+	}
+	for i, c := range cases {
+		if fact := canCompleteCircuit(c.gas, c.cost); fact != c.expected {
+			t.Errorf("case#%d, want %d, got %d", i+1, c.expected, fact)
+		}
+	}
+}
