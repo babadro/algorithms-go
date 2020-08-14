861ad7f9801c7fd103a9a6a77c85286995371e2b
petrovich.andrey <babadro@gmail.com>
2020-08-14 07:53:35 +0300

    copy list random in progress

--- leet/0138_copy_list_with_random_pointer/copy_list_with_random_pointer.go
+++ leet/0138_copy_list_with_random_pointer/copy_list_with_random_pointer.go
@@ -1,8 +1,32 @@
 package _138_copy_list_with_random_pointer
 
-import "github.com/babadro/algorithms-go/03_StacksAndQueues/04_LinkedList/single"
+type arrElem struct {
+	node *Node
+	idx int
+	randIdx int
+}
+
+type Node struct {
+	    Val int
+	    Next *Node
+	    Random *Node
+	}
 
 // TODO 1
-func copyRandomList(head *single.ListNode) *single.ListNode {
+// maybe use node.val as randIdx...
+func copyRandomList(head *Node) *Node {
+	var arr []arrElem
 
+	node := head
+	idx := 0
+	for node != nil {
+		arr = append(arr, arrElem{node: node, idx: idx})
+		idx++
+	}
+	for i := range arr {
+		random := arr[i].node.Random
+		if random != nil {
+			arr[i].randIdx =
+		}
+	}
 }
