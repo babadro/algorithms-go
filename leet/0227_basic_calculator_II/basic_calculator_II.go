f88109248babe865795cf3dffa1e36b61d3860a6
petrovich.andrey <babadro@gmail.com>
2020-08-18 07:52:05 +0300

    calculator in progress

--- leet/0227_basic_calculator_II/basic_calculator_II.go
+++ leet/0227_basic_calculator_II/basic_calculator_II.go
@@ -1,45 +1,44 @@
 package _227_basic_calculator_II
 
-const add = 0
-const sub = 1
-const mult = 2
-const div = 3
-
 // TODO 1
 func calculate(s string) int {
-
-}
-
-func calc(s string, multAndDivOnly bool) int {
 	n := len(s)
-	left := 0
-	var operator int
-	var right int
+	left, right, add := 0, 0, false
 	for i := 0; i < n; i++ {
-		if i > 0 {
-			i, left = findOperand(i, s)
-			if i == n {
-				return left
-			}
-
-			i, operator = findOperator(i, s)
-			if i == n {
-				return left
-			}
+		i, add = parsePlusOrMinus(i, s)
+		if i == n {
+			return left
 		}
-
-		i, right = findOperand(i, s)
-		left = eval(left, right, operator)
+		i, right = calcOperand(i, s)
+		left = evalAddOrSub(left, right, add)
 	}
 
 	return left
 }
 
-func findOperand(i int, s string, multAndDivOnly bool) (int, int) {
+func calcOperand(i int, s string) (int, int) {
+	n := len(s)
+	left, right, mult := 0, 0, false
+	for ; i < len(s) && !(s[i] == '+' || s[i] == '-'); i++ {
+		i, left = parseNum(i, s)
+		if i == n || s[i] == '+' || s[i] == '-' {
+			return i, left
+		}
+		i, mult = parseDivOrMult(i, s)
+		if i == n {
+			return i, left
+		}
+		i, right = parseNum(i, s)
+		left = evalMultOrDiv(left, right, mult)
+	}
+
+	return i, left
+}
+
+func parseNum(i int, s string) (int, int) {
 	for s[i] == ' ' {
 		i++
 	}
-	start := i
 	res := 0
 	for ; i < len(s); i++ {
 		if s[i] < '0' || s[i] > '9' {
@@ -50,19 +49,45 @@ func findOperand(i int, s string, multAndDivOnly bool) (int, int) {
 		res += int(num)
 	}
 
-	if i < len(s) && !multAndDivOnly && (s[i] == '/' || s[i] == '*') {
-		for ; i < len(s) && !(s[i] == ' ' || s[i] == '+' || s[i] == '-'); i++ {
-		}
-		return i, calc(s[start:i], true)
-	}
-
 	return i, res
 }
 
-func findOperator(i int, s string) (int, int) {
-	return 0, 0
+func parsePlusOrMinus(i int, s string) (int, bool) {
+	for s[i] == ' ' {
+		i++
+	}
+	if i == len(s) {
+		return i, false
+	}
+	if s[i] == '-' {
+		return i, false
+	}
+	return i, true
 }
 
-func eval(left, right, operator int) int {
-	return 0
+func parseDivOrMult(i int, s string) (int, bool) {
+	for s[i] == ' ' {
+		i++
+	}
+	if i == len(s) {
+		return i, false
+	}
+	if s[i] == '/' {
+		return i, false
+	}
+	return i, true
+}
+
+func evalAddOrSub(left, right int, add bool) int {
+	if add {
+		return left + right
+	}
+	return left - right
+}
+
+func evalMultOrDiv(left, right int, mult bool) int {
+	if mult {
+		return left * right
+	}
+	return left / right
 }
