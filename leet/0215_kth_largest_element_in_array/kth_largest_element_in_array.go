f91ab302810889e577985b178c4888e4e66e9749
petrovich.andrey <babadro@gmail.com>
2020-08-24 08:08:53 +0300

    kth largest element in progress

--- leet/0215_kth_largest_element_in_array/kth_largest_element_in_array.go
+++ leet/0215_kth_largest_element_in_array/kth_largest_element_in_array.go
@@ -4,7 +4,20 @@ import "sort"
 
 // TODO 1
 func findKthLargest(nums []int, k int) int {
-
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	pivot := nums[0]
+	p1, p2 := 1, 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] >= pivot {
+			p2++
+		} else {
+			nums[i], nums[p1] = nums[p1], nums[i]
+			p1++
+			p2++
+		}
+	}
 }
 
 // Brutforce. it works.
