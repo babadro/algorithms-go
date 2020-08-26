9088fed071d74b801d176a5568137f23e31d2890
petrovich.andrey <babadro@gmail.com>
2020-08-26 07:50:46 +0300

    word break has learnt

---
+++ leet/0139_word_break/solution.go
@@ -0,0 +1,21 @@
+package _139_word_break
+
+// https://leetcode.com/problems/word-break/discuss/448646/Go-golang-0ms-solution
+// TODO 1 learn by heart
+func wordBreak(s string, wordDict []string) bool {
+	dict := make(map[string]bool)
+	for _, v := range wordDict {
+		dict[v] = true
+	}
+	mem := make([]bool, len(s)+1)
+	mem[0] = true
+	for i := 1; i < len(mem); i++ {
+		for j := i - 1; j >= 0; j-- {
+			if mem[j] && dict[s[j:i]] {
+				mem[i] = true
+				break
+			}
+		}
+	}
+	return mem[len(mem)-1]
+}
--- leet/0139_word_break/word_break.go
+++ leet/0139_word_break/word_break.go
@@ -1,6 +1,20 @@
 package _139_word_break
 
-// TODO 1
-func wordBreak(s string, wordDict []string) bool {
+import "strings"
+
+// draft
+func wordBreakDraft(s string, wordDict []string) bool {
+	dict := make(map[string]bool)
+	for _, word := range wordDict {
+		if strings.Index(s, word) != -1 {
+			dict[word] = true
+		}
+	}
+	if len(dict) == 0 {
+		return false
+	}
+
+	// some code
+	return false
 
 }
