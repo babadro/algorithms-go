18d302b2e7d07e0996dd8f36215b4902d3080344
petrovich.andrey <babadro@gmail.com>
2020-08-27 07:01:18 +0300

    evaluate reverse polish notation completed

--- leet/0150_evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.go
+++ leet/0150_evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.go
@@ -1,6 +1,33 @@
 package _150_evaluate_reverse_polish_notation
 
-// TODO 1
-func evalRPN(tokens []string) int {
+import "strconv"
 
+// 100% 79%
+func evalRPN(tokens []string) int {
+	var stack []int
+	for i := 0; i < len(tokens); i++ {
+		switch tokens[i] {
+		case "+":
+			res := stack[len(stack)-2] + stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, res)
+		case "-":
+			res := stack[len(stack)-2] - stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, res)
+		case "/":
+			res := stack[len(stack)-2] / stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, res)
+		case "*":
+			res := stack[len(stack)-2] * stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			stack = append(stack, res)
+		default:
+			num, _ := strconv.Atoi(tokens[i])
+			stack = append(stack, num)
+		}
+	}
+
+	return stack[0]
 }
