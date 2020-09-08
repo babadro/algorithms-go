a5379519c52b7c9adac32fec0f4f45bd8f14e045
petrovich.andrey <babadro@gmail.com>
2020-09-08 17:04:18 +0300

    maxProduct in progress

--- leet/0152_maximum_product_subarray/maxProduct.go
+++ leet/0152_maximum_product_subarray/maxProduct.go
@@ -6,10 +6,25 @@ func maxProduct(nums []int) int {
 	i, j, maxProduct, currProduct := 0, 1, nums[0], nums[0]
 	for i < len(nums) || j < len(nums) {
 		for j < len(nums) && nums[j] != 0 {
-			if currProduct *= nums[j]; currProduct > maxProduct {
-				0
+			if currProduct == 0 {
+				currProduct = 1
 			}
+			if currProduct *= nums[j]; currProduct > maxProduct {
+				maxProduct = currProduct
+			}
+			j++
 		}
+
+		for currProduct < 0 && i < len(nums) && nums[i] != 0 {
+			if currProduct /= nums[i]; currProduct > maxProduct {
+				maxProduct = currProduct
+			}
+			i++
+		}
+
+		currProduct = 0
+		i++
+		j++
 	}
 
 	return maxProduct
---
+++ leet/0152_maximum_product_subarray/maxProduct_test.go
@@ -0,0 +1,29 @@
+package _152_maximum_product_subarray
+
+import "testing"
+
+func Test_maxProduct(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "1", args: args{nums: []int{2, 3, -2, 4}}, want: 6},
+		{name: "2", args: args{nums: []int{1}}, want: 1},
+		{name: "3", args: args{nums: []int{0}}, want: 0},
+		{name: "4", args: args{nums: []int{1, 1, 0}}, want: 1},
+		{name: "5", args: args{nums: []int{-1, -2, -3, -4}}, want: 24},
+		{name: "6", args: args{nums: []int{-2, 0, 0, 0, -2, -4, 2}}, want: 16},
+		{name: "7", args: args{nums: []int{-2, 0, 1}}, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProduct(tt.args.nums); got != tt.want {
+				t.Errorf("maxProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
