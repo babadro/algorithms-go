2f444a1580023a77b51c2c62d8e2ba7200b93c07
andrey.petrovich <babadro@gmail.com>
2020-09-08 16:27:26 +0300

    maxproduct in progress

--- leet/0152_maximum_product_subarray/maxProduct.go
+++ leet/0152_maximum_product_subarray/maxProduct.go
@@ -3,5 +3,14 @@ package _152_maximum_product_subarray
 // TODO 1
 // caterpillar?
 func maxProduct(nums []int) int {
+	i, j, maxProduct, currProduct := 0, 1, nums[0], nums[0]
+	for i < len(nums) || j < len(nums) {
+		for j < len(nums) && nums[j] != 0 {
+			if currProduct *= nums[j]; currProduct > maxProduct {
+				0
+			}
+		}
+	}
 
+	return maxProduct
 }
