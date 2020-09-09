7268939b95783572a3e8cb0a014311190cc8738a
petrovich.andrey <babadro@gmail.com>
2020-09-09 08:11:04 +0300

    maxProduct completed

--- leet/0152_maximum_product_subarray/maxProduct.go
+++ leet/0152_maximum_product_subarray/maxProduct.go
@@ -1,31 +1,58 @@
 package _152_maximum_product_subarray
 
-// TODO 1
-// caterpillar?
+// caterpillar 100% 77%
 func maxProduct(nums []int) int {
-	i, j, maxProduct, currProduct := 0, 1, nums[0], nums[0]
-	for i < len(nums) || j < len(nums) {
-		for j < len(nums) && nums[j] != 0 {
-			if currProduct == 0 {
-				currProduct = 1
+	front, maxProduct := 0, nums[0]
+	for front < len(nums) {
+		if nums[front] == 0 {
+			if maxProduct < 0 {
+				maxProduct = 0
 			}
-			if currProduct *= nums[j]; currProduct > maxProduct {
-				maxProduct = currProduct
-			}
-			j++
+			front++
+			continue
 		}
 
-		for currProduct < 0 && i < len(nums) && nums[i] != 0 {
-			if currProduct /= nums[i]; currProduct > maxProduct {
+		end := front
+		currProduct := 1
+		for ; front < len(nums) && nums[front] != 0; front++ {
+			if currProduct *= nums[front]; currProduct > maxProduct {
 				maxProduct = currProduct
 			}
-			i++
 		}
 
-		currProduct = 0
-		i++
-		j++
+		for ; currProduct < 0 && end < front-1; end++ {
+			if currProduct /= nums[end]; currProduct > maxProduct {
+				maxProduct = currProduct
+			}
+		}
 	}
 
 	return maxProduct
+}
+
+// TODO 2 look for java solution in https://leetcode.com/problems/maximum-product-subarray/discuss/183483/JavaC%2B%2BPython-it-can-be-more-simple
+
+// TODO 2 understand
+// https://leetcode.com/problems/maximum-product-subarray/discuss/302805/Simple-code-in-Go-0ms
+func maxProduct2(nums []int) int {
+	tmp := nums[0]
+	max := nums[0]
+	min := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < 0 {
+			max, min = min, max
+		}
+		max *= nums[i]
+		if max < nums[i] {
+			max = nums[i]
+		}
+		min *= nums[i]
+		if min > nums[i] {
+			min = nums[i]
+		}
+		if max > tmp {
+			tmp = max
+		}
+	}
+	return tmp
 }
--- leet/0152_maximum_product_subarray/maxProduct_test.go
+++ leet/0152_maximum_product_subarray/maxProduct_test.go
@@ -17,7 +17,9 @@ func Test_maxProduct(t *testing.T) {
 		{name: "4", args: args{nums: []int{1, 1, 0}}, want: 1},
 		{name: "5", args: args{nums: []int{-1, -2, -3, -4}}, want: 24},
 		{name: "6", args: args{nums: []int{-2, 0, 0, 0, -2, -4, 2}}, want: 16},
-		{name: "7", args: args{nums: []int{-2, 0, 1}}, want: 0},
+		{name: "7", args: args{nums: []int{-2, 0, -1}}, want: 0},
+		{name: "8", args: args{nums: []int{-2}}, want: -2},
+		{name: "9", args: args{nums: []int{-2, 0}}, want: 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
