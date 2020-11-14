42533e4ea8e5a438a671a89882ef4dde9f81705e
babadro <babadro@gmail.com>
2020-11-14 08:19:22 +0300

    word pattern bruteforce

--- leet/0290_word_pattern/word_pattern.go
+++ leet/0290_word_pattern/word_pattern.go
@@ -1,6 +1,48 @@
 package _290_word_pattern
 
-// todo 1
-func wordPattern(pattern string, s string) bool {
+import "strings"
 
+// todo 1
+// bruteforce
+func wordPattern(pattern string, s string) bool {
+	pattern1 := make([]int, len(pattern))
+	m1 := make(map[int32]int)
+
+	var idx int
+	var ok bool
+	for i, letter := range pattern {
+
+		if idx, ok = m1[letter]; !ok {
+			idx = len(m1)
+			m1[letter] = idx
+		}
+
+		pattern1[i] = idx
+	}
+
+	words := strings.Split(s, " ")
+	pattern2 := make([]int, 0)
+	m2 := make(map[string]int)
+
+	for _, word := range words {
+
+		if idx, ok = m2[word]; !ok {
+			idx = len(m2)
+			m2[word] = idx
+		}
+
+		pattern2 = append(pattern2, idx)
+	}
+
+	if len(pattern1) != len(pattern2) {
+		return false
+	}
+
+	for i := range pattern1 {
+		if pattern1[i] != pattern2[i] {
+			return false
+		}
+	}
+
+	return true
 }
---
+++ leet/0290_word_pattern/word_pattern_test.go
@@ -0,0 +1,25 @@
+package _290_word_pattern
+
+import "testing"
+
+func Test_wordPattern(t *testing.T) {
+	tests := []struct {
+		name    string
+		pattern string
+		s       string
+		want    bool
+	}{
+		{"1", "abba", "dog cat cat dog", true},
+		{"1", "abba", "dog cat cat fish", false},
+		{"1", "aaaa", "dog cat cat dog", false},
+		{"1", "abba", "dog dog dog dog", false},
+		{"1", "abba", "dog word word dog", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := wordPattern(tt.pattern, tt.s); got != tt.want {
+				t.Errorf("wordPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
