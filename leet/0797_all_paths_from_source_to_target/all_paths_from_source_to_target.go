69ca6a183121b5b76476c60020cc87d9af9cee6e
babadro <babadro@gmail.com>
2020-11-23 06:15:33 +0300

    all paths solved

--- leet/0797_all_paths_from_source_to_target/all_paths_from_source_to_target.go
+++ leet/0797_all_paths_from_source_to_target/all_paths_from_source_to_target.go
@@ -1,6 +1,24 @@
 package _797_all_paths_from_source_to_target
 
-// todo 1
+// tptl. passed.
 func allPathsSourceTarget(graph [][]int) [][]int {
+	tmp, res := make([]int, 0), make([][]int, 0)
+	traverse(graph, 0, tmp, &res)
 
+	return res
+}
+
+func traverse(graph [][]int, V int, tmp []int, res *[][]int) {
+	tmp = append(tmp, V)
+
+	if V == len(graph)-1 {
+		arr := make([]int, len(tmp))
+		copy(arr, tmp)
+		*res = append(*res, arr)
+		return
+	}
+
+	for _, v := range graph[V] {
+		traverse(graph, v, tmp, res)
+	}
 }
