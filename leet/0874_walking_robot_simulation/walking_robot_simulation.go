53f654ecaaa886f9751f71990e5145f8912531e2
petrovich.andrey <APetrovich@one.kz>
2020-12-17 10:45:24 +0300

    robot v1

--- leet/0874_walking_robot_simulation/walking_robot_simulation.go
+++ leet/0874_walking_robot_simulation/walking_robot_simulation.go
@@ -1,13 +1,16 @@
 package _874_walking_robot_simulation
 
 const (
-	up    = 0
-	left  = 1
-	right = 2
-	down  = 3
+	north = 0
+	east  = 1
+	south = 2
+	west  = 3
+
+	left  = -2
+	right = -1
 )
 
-// todo 1
+// passed. todo 1: look for shorter solution
 func robotSim(commands []int, obstacles [][]int) int {
 	m := make(map[[2]int]bool)
 
@@ -15,8 +18,65 @@ func robotSim(commands []int, obstacles [][]int) int {
 		m[[2]int{point[0], point[1]}] = true
 	}
 
+	curDirection := north
+	x, y, max := 0, 0, 0
+	for _, command := range commands {
+		x, y, curDirection = nextState(command, curDirection, x, y, m)
+		if dist := x*x + y*y; dist > max {
+			max = dist
+		}
+	}
+
+	return max
 }
 
-func changeDirection(command, currDirection int) int {
+func turnLeft(currDirection int) int {
+	if currDirection == north {
+		return west
+	}
 
+	return currDirection - 1
+}
+
+func turnRight(currDirection int) int {
+	if currDirection == west {
+		return north
+	}
+
+	return currDirection + 1
+}
+
+func nextState(command, curDirect, curX, curY int, mObstacles map[[2]int]bool) (x, y, direct int) {
+	if command == left {
+		return curX, curY, turnLeft(curDirect)
+	}
+
+	if command == right {
+		return curX, curY, turnRight(curDirect)
+	}
+
+	switch curDirect {
+	case north:
+		for command != 0 && !mObstacles[[2]int{curX, curY + 1}] {
+			curY++
+			command--
+		}
+	case east:
+		for command != 0 && !mObstacles[[2]int{curX + 1, curY}] {
+			curX++
+			command--
+		}
+	case south:
+		for command != 0 && !mObstacles[[2]int{curX, curY - 1}] {
+			curY--
+			command--
+		}
+	case west:
+		for command != 0 && !mObstacles[[2]int{curX - 1, curY}] {
+			curX--
+			command--
+		}
+	}
+
+	return curX, curY, curDirect
 }
---
+++ leet/0874_walking_robot_simulation/walking_robot_simulation_test.go
@@ -0,0 +1,56 @@
+package _874_walking_robot_simulation
+
+import "testing"
+
+func Test_robotSim(t *testing.T) {
+	tests := []struct {
+		name      string
+		commands  []int
+		obstacles [][]int
+		want      int
+	}{
+		{
+			name:      "1",
+			commands:  []int{4, -1, 3},
+			obstacles: [][]int{},
+			want:      25,
+		},
+		{
+			name:      "2",
+			commands:  []int{4, -1, 4, -2, 4},
+			obstacles: [][]int{{2, 4}},
+			want:      65,
+		},
+		{
+			name:      "3",
+			commands:  []int{8},
+			obstacles: [][]int{},
+			want:      64,
+		},
+		{
+			name:      "4",
+			commands:  []int{-1, 8},
+			obstacles: [][]int{},
+			want:      64,
+		},
+		{
+			name:      "4",
+			commands:  []int{-1, 8},
+			obstacles: [][]int{{1, 0}},
+			want:      0,
+		},
+		{
+			name:      "5",
+			commands:  []int{-2, 8, 3, 7, -1},
+			obstacles: [][]int{{-4, -1}, {1, -1}, {1, 4}, {5, 0}, {4, 5}, {-2, -1}, {2, -5}, {5, 1}, {-3, -1}, {5, -3}},
+			want:      324,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := robotSim(tt.commands, tt.obstacles); got != tt.want {
+				t.Errorf("robotSim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
