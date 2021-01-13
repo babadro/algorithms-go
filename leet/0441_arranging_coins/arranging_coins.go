88195cf5ce584fc48d9c2b816158cfd038c584bf
petrovich.andrey <APetrovich@one.kz>
2021-01-13 07:20:02 +0300

    arrangeCoins in progress

---
+++ leet/0441_arranging_coins/arranging_coins.go
@@ -0,0 +1,14 @@
+package _441_arranging_coins
+
+// todo 1
+func arrangeCoins(n int) int {
+	total := n
+	i := 0
+	for ; i < n; i++ {
+		if total
+	}
+}
+
+func seriesSum(n int) int {
+	return n*(1+n)/2
+}
