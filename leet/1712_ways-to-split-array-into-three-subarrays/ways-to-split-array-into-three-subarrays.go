c28b188d292838cf1bc4798381f03733596939e1
petrovich.andrey <APetrovich@one.kz>
2021-02-20 08:50:52 +0300

    ways to split array in progress

--- leet/1712_ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.go
+++ leet/1712_ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.go
@@ -1,5 +1,46 @@
 package _1712_ways_to_split_array_into_three_subarrays
 
+import (
+	"fmt"
+	"sort"
+)
+
+// todo 1
+func waysToSplit2(nums []int) int {
+	n := len(nums)
+	for i := 1; i < n; i++ {
+		nums[i] += nums[i-1]
+	}
+
+	if nums[n-1] == 0 {
+		k := n - 2
+		return (1 + k) * k / 2
+	}
+
+	counter := 0
+	for i := 0; i < n-2; i++ {
+
+		start := sort.Search(n-3-i, func(j int) bool {
+			return nums[j+i+1]-nums[i] >= nums[i]
+		})
+		start += i + 1
+
+		finish := sort.Search(n-start, func(j int) bool {
+			return nums[j+start]-nums[i] > nums[n-1]-nums[j]
+		})
+		finish += start
+
+		count := finish - start
+		if count <= 0 || finish >= n || start <= i {
+			continue
+		}
+
+		counter += count
+	}
+
+	return counter % 1_000_000_007
+}
+
 func waysToSplit(nums []int) int {
 	n := len(nums)
 	for i := 1; i < n; i++ {
@@ -25,6 +66,11 @@ func waysToSplit(nums []int) int {
 		}
 
 		counter += finish - start
+
+		if counter == 227 {
+
+			fmt.Printf("i=%d, start=%d, finish=%d\n", i, start, finish)
+		}
 	}
 
 	return counter % 1_000_000_007
