26e7806e240dee99a1cd1ec71d707b2d95f9bdcd
petrovich.andrey <APetrovich@one.kz>
2021-03-04 06:36:33 +0300

    numTeams completed but slow

--- leet/1395_count_number_of_teams/count_number_of_teams.go
+++ leet/1395_count_number_of_teams/count_number_of_teams.go
@@ -1,6 +1,34 @@
-package _395_count_number_of_teams
+package _1395_count_number_of_teams
 
-// todo 1
+const (
+	first     = 0
+	second    = 1
+	thirdUp   = 2
+	thirdDown = 3
+)
+
+// passed, but slow
 func numTeams(rating []int) int {
+	counter := 0
 
+	helper(first, &counter, 0, rating)
+
+	return counter
+}
+
+func helper(mode int, counter *int, prev int, arr []int) {
+	for i, num := range arr {
+		if mode == first {
+			helper(second, counter, num, arr[i+1:])
+		} else if mode == second {
+			nextMode := thirdUp
+			if num < prev {
+				nextMode = thirdDown
+			}
+
+			helper(nextMode, counter, num, arr[i+1:])
+		} else if (mode == thirdUp && num > prev) || (mode == thirdDown && num < prev) {
+			*counter++
+		}
+	}
 }
---
+++ leet/1395_count_number_of_teams/count_number_of_teams_test.go
@@ -0,0 +1,25 @@
+package _1395_count_number_of_teams
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_numTeams(t *testing.T) {
+	tests := []struct {
+		rating []int
+		want   int
+	}{
+		{[]int{2, 5, 3, 4, 1}, 3},
+		{[]int{2, 1, 3}, 0},
+		{[]int{1, 2, 3, 4}, 4},
+		{[]int{1, 2, 3}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.rating), func(t *testing.T) {
+			if got := numTeams(tt.rating); got != tt.want {
+				t.Errorf("numTeams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
