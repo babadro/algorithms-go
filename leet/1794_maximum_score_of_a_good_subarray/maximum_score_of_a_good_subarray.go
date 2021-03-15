d28d36f8cf742507a1edc409447c2fc1e9485522
petrovich.andrey <APetrovich@one.kz>
2021-03-15 07:06:17 +0300

    maximumScore in progress

--- leet/1794_maximum_score_of_a_good_subarray/maximum_score_of_a_good_subarray.go
+++ leet/1794_maximum_score_of_a_good_subarray/maximum_score_of_a_good_subarray.go
@@ -1,5 +1,52 @@
 package _794_maximum_score_of_a_good_subarray
 
-func maximumScore(nums []int, k int) int {
+import "math"
 
+// todo 1 doesnt' work
+func maximumScore(nums []int, k int) int {
+	leftIdx, leftMin, leftMaxScore := k, math.MaxInt64, 0
+	for i := k; i >= 0; i-- {
+		if num := nums[i]; num < leftMin {
+			leftMin = num
+		}
+
+		length := k - i + 1
+
+		if score := leftMin * length; score > leftMaxScore {
+			leftIdx, leftMaxScore = i, score
+		}
+	}
+
+	rightIdx, rightMin, rightMaxScore := k, math.MaxInt64, 0
+	for i := k; i < len(nums); i++ {
+		if num := nums[i]; num < rightMin {
+			rightMin = num
+		}
+
+		length := i - k + 1
+
+		if score := rightMin * length; score > rightMaxScore {
+			rightIdx, rightMaxScore = i, score
+		}
+	}
+
+	min := math.MaxInt64
+	for i := leftIdx; i <= rightIdx; i++ {
+		if num := nums[i]; num < min {
+			min = num
+		}
+	}
+
+	totalScore := (rightIdx - leftIdx + 1) * min
+
+	max := leftMaxScore
+	if rightMaxScore > leftMaxScore {
+		max = rightMaxScore
+	}
+
+	if totalScore > max {
+		max = totalScore
+	}
+
+	return max
 }
---
+++ leet/1794_maximum_score_of_a_good_subarray/maximum_score_of_a_good_subarray_test.go
@@ -0,0 +1,35 @@
+package _794_maximum_score_of_a_good_subarray
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_maximumScore(t *testing.T) {
+	tests := []struct {
+		nums []int
+		k    int
+		want int
+	}{
+		{[]int{1, 4, 3, 7, 4, 5}, 3, 15},
+		{[]int{5, 5, 4, 5, 4, 1, 1, 1}, 0, 20},
+		{[]int{5}, 0, 5},
+		{[]int{1, 2, 5}, 2, 5},
+		{[]int{6569, 9667, 3148, 7698, 1622, 2194, 793, 9041, 1670, 1872}, 5, 9732},
+		{arr474, 474, 36808}, // todo 1 fails.
+	}
+	for _, tt := range tests {
+		var name string
+		if len(tt.nums) > 5 {
+			name = fmt.Sprintf("%v<...>", tt.nums[:5])
+		} else {
+			name = fmt.Sprintf("%v", tt.nums)
+		}
+
+		t.Run(name, func(t *testing.T) {
+			if got := maximumScore(tt.nums, tt.k); got != tt.want {
+				t.Errorf("maximumScore() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
