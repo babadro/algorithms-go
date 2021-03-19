c20c99e3c24bff1621a99ae1023d656088d8b863
petrovich.andrey <APetrovich@one.kz>
2021-03-19 07:11:56 +0300

    set mismatch completed

--- leet/0645_set_mismatch/set_mismatch.go
+++ leet/0645_set_mismatch/set_mismatch.go
@@ -1,27 +1,79 @@
 package _645_set_mismatch
 
-import "sort"
-
-// todo 1 doesn't work
-func findErrorNums(nums []int) []int {
-	sort.Ints(nums)
-
-	res := make([]int, 0, 2)
-	for i := 1; i < len(nums); i++ {
-		if nums[i] == nums[i-1] {
-			res = append(res, nums[i])
-
-			if i+1 != nums[i] {
-				res = append(res, i+1)
-			} else {
-				res = append(res, i)
-			}
+// passed. best solution
+func findErrorNums3(nums []int) []int {
+	for i := 0; i < len(nums); {
+		idx := nums[i] - 1
+		if nums[i] == nums[idx] {
+			i++
+			continue
 		}
 
-		if len(res) == 2 {
-			return res
+		nums[i], nums[idx] = nums[idx], nums[i]
+	}
+
+	for i, num := range nums {
+		if num != i+1 {
+			return []int{num, i + 1}
 		}
 	}
 
-	return res
+	return nil
+}
+
+// passed. tptl. todo 2 look for xor solution
+func findErrorNums(nums []int) []int {
+	n := len(nums)
+	sumArithmeticProgression := (1 + n) * n / 2
+
+	sum := 0
+	for i := range nums {
+		sum += nums[i]
+	}
+
+	diff := sumArithmeticProgression - sum
+
+	for i := 0; i < n; {
+		idx := nums[i] - 1
+
+		if idx == i {
+			i++
+			continue
+		}
+
+		if nums[i] == nums[idx] {
+			return []int{nums[i], nums[i] + diff}
+		}
+
+		nums[i], nums[idx] = nums[idx], nums[i]
+	}
+
+	return nil
+}
+
+// doesn't work
+func findErrorNums2(nums []int) []int {
+	i, k, j, duplicate := 0, 0, 0, len(nums)
+	for i < duplicate {
+		idx := nums[i] - 1
+		if idx == i {
+			i++
+			continue
+		}
+
+		if nums[i] == nums[idx] {
+			duplicate = nums[i]
+			k, j = i, idx
+			i++
+			continue
+		}
+
+		nums[i], nums[idx] = nums[idx], nums[i]
+	}
+
+	if j == nums[k] {
+		return []int{k + 1, j + 1}
+	}
+
+	return []int{j + 1, k + 1}
 }
--- leet/0645_set_mismatch/set_mismatch_test.go
+++ leet/0645_set_mismatch/set_mismatch_test.go
@@ -15,10 +15,12 @@ func Test_findErrorNums(t *testing.T) {
 		{[]int{1, 1}, []int{1, 2}},
 		{[]int{2, 2, 3, 4}, []int{2, 1}},
 		{[]int{3, 2, 3, 4, 6, 5}, []int{3, 1}},
+		{[]int{8, 7, 3, 5, 3, 6, 1, 4}, []int{3, 2}},
+		{[]int{1, 5, 3, 2, 2, 7, 6, 4, 8, 9}, []int{2, 10}},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
-			if got := findErrorNums(tt.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := findErrorNums3(tt.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("findErrorNums() = %v, want %v", got, tt.want)
 			}
 		})
