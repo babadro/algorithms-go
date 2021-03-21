83449e95f86ba2049ee9fd003296e122c5fbf0b4
petrovich.andrey <APetrovich@one.kz>
2021-03-21 08:17:15 +0300

    maxvalue doesn't work 2

--- leet/contests/233/task_3.go
+++ leet/contests/233/task_3.go
@@ -1,12 +1,14 @@
 package _33
 
 func maxValue(n int, index int, maxSum int) int {
-	l, r := 1, 1_000_000_000
+	l, r := 1, maxSum
 	for l < r {
 		mid := (l + r) / 2
 
-		if sum(n, index, mid) > maxSum {
-			r = mid
+		if curr := sum(n, index, mid); curr > maxSum {
+			r = mid - 1
+		} else if curr == maxSum {
+			return mid
 		} else {
 			l = mid + 1
 		}
@@ -16,9 +18,23 @@ func maxValue(n int, index int, maxSum int) int {
 }
 
 func sum(n, index, num int) int {
-	return sumArProg(num-index, num, index+1) +
-		sumArProg(num, num-(n-1-index), n-index) -
-		num
+	sumLeft, sumRight := 0, 0
+
+	startLeft := num - index
+	if startLeft < 1 {
+		sumLeft = sumArProg(1, num, num) + index - num + 1
+	} else {
+		sumLeft = sumArProg(num-index, num, index+1)
+	}
+
+	finishRight := num - (n - 1 - index)
+	if finishRight < 1 {
+		sumRight = sumArProg(num, 1, num) + (n - index - num)
+	} else {
+		sumRight = sumArProg(num, finishRight, n-index)
+	}
+
+	return sumLeft + sumRight - num
 }
 
 func sumArProg(a1, an, n int) int {
--- leet/contests/233/task_3_test.go
+++ leet/contests/233/task_3_test.go
@@ -2,14 +2,8 @@ package _33
 
 import (
 	"fmt"
-	"math"
 	"testing"
 )
-
-func Test_sum(t *testing.T) {
-	t.Log(sum(1_000_000_000, 500_000_000, 1_000_000_000))
-	t.Log(math.MaxInt64)
-}
 
 func Test_maxValue(t *testing.T) {
 	tests := []struct {
@@ -18,14 +12,40 @@ func Test_maxValue(t *testing.T) {
 		maxSum int
 		want   int
 	}{
-		{
-			n: 4, index: 2, maxSum: 6, want: 2,
-		},
+		{n: 4, index: 2, maxSum: 6, want: 2},
+		{6, 1, 10, 3},
+		{1, 0, 3, 3},
+		{3, 0, 6, 3},
+		{3, 2, 6, 3},
+		{3, 1, 7, 3},
+		{3, 0, 3, 1},
+		{3, 2, 3, 1},
+		{3, 1, 3, 1},
+		{3, 1, 4, 2},
+		{5, 0, 28, 7},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("n: %d, index: %d, maxSum: %d", tt.n, tt.index, tt.maxSum), func(t *testing.T) {
 			if got := maxValue(tt.n, tt.index, tt.maxSum); got != tt.want {
 				t.Errorf("maxValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sum1(t *testing.T) {
+	tests := []struct {
+		num   int
+		n     int
+		index int
+		want  int
+	}{
+		{num: 2, n: 4, index: 2, want: 5},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("num: %d, n: %d, index: %d", tt.num, tt.n, tt.index), func(t *testing.T) {
+			if got := sum(tt.n, tt.index, tt.num); got != tt.want {
+				t.Errorf("sum() = %v, want %v", got, tt.want)
 			}
 		})
 	}
