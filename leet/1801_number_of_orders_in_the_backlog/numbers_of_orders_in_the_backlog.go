d0a7822973adea53a5f571ef403adaf884dab69d
petrovich.andrey <APetrovich@one.kz>
2021-03-24 07:10:25 +0300

    orders backlog doesn't work

---
+++ leet/1801_number_of_orders_in_the_backlog/numbers_of_orders_in_the_backlog.go
@@ -0,0 +1,75 @@
+package _1801_number_of_orders_in_the_backlog
+
+import "container/heap"
+
+const (
+	buy  = 0
+	sell = 1
+	d    = 1_000_000_007
+)
+
+// todo 1
+func getNumberOfBacklogOrders(orders [][]int) int {
+	buyOr, sellOr := &minHeap{}, &minHeap{}
+
+	for _, batch := range orders {
+		price, amount, typ := batch[0], batch[1], batch[2]
+		h1, h2 := buyOr, sellOr
+		if typ == sell {
+			h1, h2 = sellOr, buyOr
+		}
+
+		if h2.Len() > 0 {
+			if (*h2)[0][0] <= price {
+				for h2.Len() > 0 && amount > 0 {
+					itemPrice := (*h2)[0][0]
+					if itemPrice > price {
+						break
+					}
+
+					itemAmount := (*h2)[0][1]
+					if itemAmount <= amount {
+						h2.Pop()
+						amount -= itemAmount
+
+						continue
+					}
+
+					(*h2)[0][1] -= amount
+					amount = 0
+				}
+			}
+		}
+
+		if amount > 0 {
+			heap.Push(h1, []int{price, amount})
+		}
+	}
+
+	sum := 0
+	for _, batch := range *buyOr {
+		sum += batch[1]
+		sum %= d
+	}
+
+	for _, batch := range *sellOr {
+		sum += batch[1]
+		sum %= d
+	}
+
+	return sum
+}
+
+type minHeap [][]int
+
+func (h minHeap) Len() int            { return len(h) }
+func (h minHeap) Less(i, j int) bool  { return h[i][0] < h[j][0] }
+func (h minHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *minHeap) Push(x interface{}) { *h = append(*h, x.([]int)) }
+func (h *minHeap) Pop() interface{} {
+	n := len(*h)
+	res := (*h)[n-1]
+	*h = (*h)[:n-1]
+
+	return res
+}
---
+++ leet/1801_number_of_orders_in_the_backlog/numbers_of_orders_in_the_backlog_test.go
@@ -0,0 +1,24 @@
+package _1801_number_of_orders_in_the_backlog
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_getNumberOfBacklogOrders(t *testing.T) {
+	tests := []struct {
+		orders [][]int
+		want   int
+	}{
+		{[][]int{{10, 5, 0}, {15, 2, 1}, {25, 1, 1}, {30, 4, 0}}, 6},
+		{[][]int{{7, 1000000000, 1}, {15, 3, 0}, {5, 999999995, 0}, {5, 1, 1}}, 999999984},
+		{[][]int{{19, 28, 0}, {9, 4, 1}, {25, 15, 1}}, 39},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.orders), func(t *testing.T) {
+			if got := getNumberOfBacklogOrders(tt.orders); got != tt.want {
+				t.Errorf("getNumberOfBacklogOrders() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
