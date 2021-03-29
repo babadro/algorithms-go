776e966088c72f2a157846c55ef37cc54a13d922
petrovich.andrey <APetrovich@one.kz>
2021-03-29 07:16:05 +0300

    todo for nice divisors

--- leet/1808_maximize-number-of-nice-divisors/maximize-number-of-nice-divisors.go
+++ leet/1808_maximize-number-of-nice-divisors/maximize-number-of-nice-divisors.go
@@ -1,7 +1,7 @@
 package _1808_maximize_number_of_nice_divisors
 
 // todo 1
-// 1808
+// https://leetcode.com/problems/maximize-number-of-nice-divisors/discuss/1130586/C%2B%2BJava-modpow
 func maxNiceDivisors(primeFactors int) int {
 	return 0
 }
