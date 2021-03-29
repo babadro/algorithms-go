4116ae6376e67b1de6486c97ebc67ebdc1479cb9
petrovich.andrey <APetrovich@one.kz>
2021-03-29 05:26:50 +0300

    integerBreak completed

--- leet/0343_integer_break/integer_break.go
+++ leet/0343_integer_break/integer_break.go
@@ -1,7 +1,24 @@
 package _343_integer_break
 
-// todo 1
+// passed. tptl. best solution
 // https://leetcode.com/problems/integer-break/discuss/80689/A-simple-explanation-of-the-math-part-and-a-O(n)-solution
+func integerBreak2(n int) int {
+	switch n {
+	case 2:
+		return 1
+	case 3:
+		return 2
+	}
+
+	res := 1
+	for n > 4 {
+		res *= 3
+		n -= 3
+	}
+
+	return res * n
+}
+
 // and realize math solution
 
 // passed. bruteforce
--- leet/0343_integer_break/integer_break_test.go
+++ leet/0343_integer_break/integer_break_test.go
@@ -13,11 +13,12 @@ func Test_integerBreak(t *testing.T) {
 		{2, 1},
 		{10, 36},
 		{3, 2},
+		{4, 4},
 		{6, 9},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%d", tt.n), func(t *testing.T) {
-			if got := integerBreak(tt.n); got != tt.want {
+			if got := integerBreak2(tt.n); got != tt.want {
 				t.Errorf("integerBreak() = %v, want %v", got, tt.want)
 			}
 		})
