498124910ba17551f5068baf6c0dda5b02a5d1b3
petrovich.andrey <APetrovich@one.kz>
2021-04-21 07:09:30 +0300

    jugjePoint24 int progress

--- leet/500-999/679_24_game/24_game.go
+++ leet/500-999/679_24_game/24_game.go
@@ -1,6 +1,59 @@
 package _679_24_game
 
+import "strconv"
+
+var operators = []byte{'*', '/', '+', '-'}
+var brackets = []string{"", "()", "()()", "(())"}
+
 // todo 1
 func judgePoint24(nums []int) bool {
+	stmt := make([]byte, 0, 11)
+	for _, br := range brackets {
+		if helper(nums, stmt, []byte(br)) {
+			return true
+		}
+	}
 
+	return false
+}
+
+func helper(nums []int, stmt []byte, brackets []byte) bool {
+	n, nb := len(nums), len(brackets)
+
+	if  n == 0 && nb == 0 {
+		return eval(stmt) == 24
+	}
+
+	// nums
+	if n == 4 {
+		for i := 0; i < 4; i++ {
+			nextStmt := append(stmt, strconv.Itoa(nums[i])...)
+			nums[i], nums[n-1] = nums[n-1], nums[i]
+			nextNums := nums[:n-1]
+
+			if helper(nextNums, nextStmt, brackets) {
+				return true
+			}
+		}
+	} else if ...{}
+
+
+	// brackets
+	if n == 4 {
+		if nb > 0 && brackets[0] == '(' {
+			nextStmt := append(stmt, '(')
+			nextBrackets := brackets[1:]
+
+			return helper(nums, nextStmt, nextBrackets)
+		}
+	}
+
+
+
+
+	return false
+}
+
+func eval(stmt []byte) int {
+	return 0
 }
