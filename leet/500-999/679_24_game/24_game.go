8558a6a895a903b92fc5799646d0ca6e83125d77
petrovich.andrey <APetrovich@one.kz>
2021-04-22 07:15:49 +0300

    jugjePoint24 int progress 2

--- leet/500-999/679_24_game/24_game.go
+++ leet/500-999/679_24_game/24_game.go
@@ -1,6 +1,9 @@
 package _679_24_game
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 var operators = []byte{'*', '/', '+', '-'}
 var brackets = []string{"", "()", "()()", "(())"}
@@ -18,42 +21,73 @@ func judgePoint24(nums []int) bool {
 }
 
 func helper(nums []int, stmt []byte, brackets []byte) bool {
-	n, nb := len(nums), len(brackets)
+	n, nb, ns := len(nums), len(brackets), len(stmt)
 
-	if  n == 0 && nb == 0 {
+	if n == 0 && nb == 0 {
 		return eval(stmt) == 24
 	}
 
+	last := ns - 1
 	// nums
-	if n == 4 {
-		for i := 0; i < 4; i++ {
+	if (n == 4 || stmt[last] == '(' || isOperator(stmt[last])) &&
+		!(n == 2 && nb == 2) && !(n == 3 && nb == 4) && !(n == 2 && nb == 3) {
+		for i := 0; i < n; i++ {
 			nextStmt := append(stmt, strconv.Itoa(nums[i])...)
-			nums[i], nums[n-1] = nums[n-1], nums[i]
+			nums[i], nums[n-1] = nums[n-1], nums[i] // todo i'm not sure
 			nextNums := nums[:n-1]
 
 			if helper(nextNums, nextStmt, brackets) {
 				return true
 			}
 		}
-	} else if ...{}
-
+	}
 
 	// brackets
-	if n == 4 {
-		if nb > 0 && brackets[0] == '(' {
-			nextStmt := append(stmt, '(')
-			nextBrackets := brackets[1:]
+	if !(nb == 0 ||
+		(ns > 1 && stmt[ns-2] == '(' && brackets[0] == ')') ||
+		(ns > 0 && isOperator(stmt[last])) ||
+		(ns > 0 && isNum(stmt[last]) && brackets[0] == '(') ||
+		(ns > 0 && stmt[last] == ')' && brackets[0] == '(') ||
+		(ns > 0 && stmt[last] == '(' && brackets[0] == ')')) {
+		nextStmt := append(stmt, brackets[0])
+		nextBrackets := brackets[1:]
 
-			return helper(nums, nextStmt, nextBrackets)
+		if helper(nums, nextStmt, nextBrackets) {
+			return true
 		}
 	}
 
+	// operators
+	if !(n == 0 || n == 4 || (ns > 0 && (isOperator(stmt[last]) || stmt[last] == '('))) {
+		for _, operator := range operators {
+			nextStmt := append(stmt, operator)
 
-
+			if helper(nums, nextStmt, brackets) {
+				return true
+			}
+		}
+	}
 
 	return false
 }
 
+func isOperator(b byte) bool {
+	for _, op := range operators {
+		if b == op {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isNum(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+// todo take into account division by zero
 func eval(stmt []byte) int {
+	fmt.Println(string(stmt))
+
 	return 0
 }
---
+++ leet/500-999/679_24_game/24_game_test.go
@@ -0,0 +1,23 @@
+package _679_24_game
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_judgePoint24(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want bool
+	}{
+		{[]int{4, 1, 8, 7}, true},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("%d%d%d%d", tt.nums[0], tt.nums[1], tt.nums[2], tt.nums[3])
+		t.Run(name, func(t *testing.T) {
+			if got := judgePoint24(tt.nums); got != tt.want {
+				t.Errorf("judgePoint24() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
