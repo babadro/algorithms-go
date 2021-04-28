6afc161d6c14253f726013b012e5f02d48a6cb3a
petrovich.andrey <APetrovich@one.kz>
2021-04-28 06:31:29 +0300

    most frequent element doesn't work

---
+++ leet/1800-1899/1838_frequency_of_the_most_frequent_element/frequency_of_the_most_frequent_element.go
@@ -0,0 +1,36 @@
+package _1838_frequency_of_the_most_frequent_element
+
+import "sort"
+
+const limit = 10_000
+
+// todo 1
+func maxFrequency(nums []int, k int) int {
+	y := sort.Search(limit, func(i int) bool {
+		return sum(i, nums) >= k
+	})
+
+	if y == limit {
+		return len(nums)
+	}
+
+	res := 0
+	for _, num := range nums {
+		if y >= num {
+			res++
+		}
+	}
+
+	return res
+}
+
+func sum(y int, nums []int) int {
+	res := 0
+	for _, num := range nums {
+		if y > num {
+			res += y - num
+		}
+	}
+
+	return res
+}
---
+++ leet/1800-1899/1838_frequency_of_the_most_frequent_element/frequency_of_the_most_frequent_element_test.go
@@ -0,0 +1,44 @@
+package _1838_frequency_of_the_most_frequent_element
+
+import (
+	"fmt"
+	"testing"
+)
+
+var longArr = []int{9930, 9923, 9983, 9997, 9934, 9952, 9945, 9914, 9985, 9982, 9970, 9932, 9985, 9902, 9975, 9990, 9922, 9990, 9994, 9937, 9996, 9964, 9943, 9963, 9911, 9925, 9935, 9945, 9933, 9916, 9930, 9938, 10000, 9916, 9911, 9959, 9957, 9907, 9913, 9916, 9993, 9930, 9975, 9924, 9988, 9923, 9910, 9925, 9977, 9981, 9927, 9930, 9927, 9925, 9923, 9904, 9928, 9928, 9986, 9903, 9985, 9954, 9938, 9911, 9952, 9974, 9926, 9920, 9972, 9983, 9973, 9917, 9995, 9973, 9977, 9947, 9936, 9975, 9954, 9932, 9964, 9972, 9935, 9946, 9966}
+
+func Test_maxFrequency(t *testing.T) {
+	tests := []struct {
+		nums []int
+		k    int
+		want int
+	}{
+		{[]int{1, 2, 4}, 5, 3},
+		{[]int{1, 4, 8, 13}, 5, 2},
+		{[]int{3, 9, 6}, 2, 1},
+		{[]int{2, 3, 2, 100}, 3, 3},
+		{[]int{2, 3, 2, 100}, 98 + 98 + 97, 4},
+		{[]int{1}, 1, 1},
+		{longArr, 3056, 73},
+		{[]int{1, 2, 10, 11, 11, 11}, 1, 4},
+		{[]int{1, 2, 10, 11, 12, 13}, 6, 4},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
+			if got := maxFrequency(tt.nums, tt.k); got != tt.want {
+				t.Errorf("maxFrequency() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSomething(t *testing.T) {
+	res := 0
+	for _, num := range longArr {
+		if 9983 >= num {
+			res++
+		}
+	}
+
+	t.Log(res)
+}
