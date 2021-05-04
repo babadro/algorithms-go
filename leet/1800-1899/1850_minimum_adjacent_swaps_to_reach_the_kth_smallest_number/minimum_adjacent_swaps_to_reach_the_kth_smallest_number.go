4f6e420ab10a1dd091cb5a2c75510c4a65b6ffc5
petrovich.andrey <APetrovich@one.kz>
2021-05-04 07:03:57 +0300

    getMinSwaps in progress

--- leet/1800-1899/1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number/minimum_adjacent_swaps_to_reach_the_kth_smallest_number.go
+++ leet/1800-1899/1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number/minimum_adjacent_swaps_to_reach_the_kth_smallest_number.go
@@ -1,7 +1,28 @@
 package _1850_minimum_adjacent_swaps_to_reach_the_kth_smallest_number
 
-// todo 1 solve 0031 next permutation task, then do that:
+import "github.com/babadro/algorithms-go/perm"
+
+// todo 1:
 // https://leetcode.com/problems/minimum-adjacent-swaps-to-reach-the-kth-smallest-number/discuss/1186921/C%2B%2B-next-permutation
 func getMinSwaps(num string, k int) int {
-	return 0
+	num1 := []byte(num)
+	for i := 0; i < k; i++ {
+		perm.NextPermutationBytes(num1)
+	}
+
+	res, n := 0, len(num)
+	for i := 0; i < n; i++ {
+		if num[i] != num1[i] {
+			for j := i + 1; j < n; j++ {
+				if num[i] == num1[j] {
+					res += j - i
+					num1 = append(num1[:i+1], num1[i:i+j-1]...)
+					num1 = append(num1, num1[j+1:]...)
+					break
+				}
+			}
+		}
+	}
+
+	return res
 }
