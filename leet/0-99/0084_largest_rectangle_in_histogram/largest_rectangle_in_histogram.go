35118b507530078a63aeb2dcc6409b373918b1f9
petrovich.andrey <APetrovich@one.kz>
2021-05-13 07:26:23 +0300

    waysToSplit passed

--- leet/0-99/0084_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
+++ leet/0-99/0084_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
@@ -2,5 +2,5 @@ package _084_largest_rectangle_in_histogram
 
 // todo 1
 func largestRectangleArea(heights []int) int {
-
+	return 0
 }
--- leet/1500-1799/1712_ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.go
+++ leet/1500-1799/1712_ways-to-split-array-into-three-subarrays/ways-to-split-array-into-three-subarrays.go
@@ -1,53 +1,11 @@
 package _1712_ways_to_split_array_into_three_subarrays
 
 import (
-	"fmt"
 	"sort"
 )
 
-// todo 1
-// https://leetcode.com/problems/ways-to-split-array-into-three-subarrays/discuss/1005689/Java-O(N)-sliding-window
-// https://leetcode.com/problems/ways-to-split-array-into-three-subarrays/discuss/999257/C%2B%2BJavaPython-O(n)-with-picture
-func waysToSplit2(nums []int) int {
-	n := len(nums)
-	for i := 1; i < n; i++ {
-		nums[i] += nums[i-1]
-	}
-
-	if nums[n-1] == 0 {
-		k := n - 2
-		return (1 + k) * k / 2
-	}
-
-	counter := 0
-	for i := 0; i < n-2; i++ {
-
-		tmp := nums[i+1 : n-1]
-		k := len(tmp)
-		start := sort.Search(k, func(j int) bool {
-			return tmp[j]-nums[i] >= nums[i]
-		})
-		start += i + 1
-
-		tmp2 := nums[start+1 : n]
-		finish := sort.Search(len(tmp2), func(j int) bool {
-			return nums[j]-nums[i] > nums[n-1]-nums[j]
-		})
-		if finish >= len(tmp2) {
-			continue
-		}
-
-		count := finish - start
-		if count <= 0 {
-			continue
-		}
-
-		counter += count
-	}
-
-	return counter % 1_000_000_007
-}
-
+// passed. tptl. medium (hard for me). mine!
+// todo 2 look for non binary search solution
 func waysToSplit(nums []int) int {
 	n := len(nums)
 	for i := 1; i < n; i++ {
@@ -55,44 +13,28 @@ func waysToSplit(nums []int) int {
 	}
 
 	counter := 0
-	for i := 0; i < n-2; i++ {
-		start, finish := -1, n-1
-		for j := i + 1; j < n-1; j++ {
-			if start == -1 {
-				if nums[j]-nums[i] >= nums[i] {
-					start = j
-				}
-			} else if nums[j]-nums[i] > nums[n-1]-nums[j] {
-				finish = j
-				break
-			}
-		}
+	for rightArrEnd := n - 1; rightArrEnd >= 2; rightArrEnd-- {
+		midArrEnd := rightArrEnd - 1
+		right := nums[n-1] - nums[midArrEnd]
 
-		if start == -1 || nums[n-1]-nums[finish-1] < nums[finish-1]-nums[i] {
-			continue
-		}
+		lowestLeftEnd := sort.Search(midArrEnd, func(leftArrEnd int) bool {
+			middle := nums[midArrEnd] - nums[leftArrEnd]
 
-		counter += finish - start
+			return right >= middle
+		})
 
-		if counter == 227 {
+		highestLeftEnd := sort.Search(midArrEnd, func(leftArrEnd int) bool {
+			middle, left := nums[midArrEnd]-nums[leftArrEnd], nums[leftArrEnd]
 
-			fmt.Printf("i=%d, start=%d, finish=%d\n", i, start, finish)
+			return middle < left
+		})
+
+		highestLeftEnd--
+
+		if highestLeftEnd >= lowestLeftEnd {
+			counter += highestLeftEnd - lowestLeftEnd + 1
 		}
 	}
 
 	return counter % 1_000_000_007
 }
-
-//if nums[i] >= nums[n-1]-nums[i] {
-//	break
-//}
-//tmp := nums[i+1:n-2]
-//nTmp := len(tmp)
-//
-//start := sort.Search(nTmp, func(j int) bool {
-//	return tmp[j]-nums[i] >= nums[i]
-//})
-//
-//finish := sort.Search(nTmp, func(j int) bool {
-//	return tmp[j]-nums[i] > nums[n-1]-tmp[j]
-//})
