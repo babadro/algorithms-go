ddbf63a80dde43a7ac7c0ab6a90f5eb4e82a942d
petrovich.andrey <APetrovich@one.kz>
2021-05-25 05:59:05 +0300

    stoneGame in progress

--- leet/contest/242/task_4.go
+++ leet/contest/242/task_4.go
@@ -1,7 +1,25 @@
 package _42
 
-// todo 1
+// todo 1 need to understand
+// https://leetcode.com/problems/stone-game-viii/discuss/1224872/Top-Down-and-Bottom-Up
 func stoneGameVIII(stones []int) int {
-	// backtracking? memorization? recursion?
-	return 0
+	n := len(stones)
+	for i := 1; i < n; i++ {
+		stones[i] += stones[i-1]
+	}
+
+	res := stones[n-1]
+	for i := n - 2; i > 0; i-- {
+		res = max(res, stones[i]-res)
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
 }
---
+++ leet/contest/242/task_4_test.go
@@ -0,0 +1,24 @@
+package _42
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_stoneGameVIII(t *testing.T) {
+	tests := []struct {
+		stones []int
+		want   int
+	}{
+		{[]int{-1, 2, -3, 4, -5}, 5},
+		{[]int{7, -6, 5, 10, 5, -2, -6}, 13},
+		{[]int{-10, -12}, -22},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.stones), func(t *testing.T) {
+			if got := stoneGameVIII(tt.stones); got != tt.want {
+				t.Errorf("stoneGameVIII() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
