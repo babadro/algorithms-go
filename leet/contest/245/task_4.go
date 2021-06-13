933fe950bfc61ed2c693045340a68acf924c9426
petrovich.andrey <APetrovich@one.kz>
2021-06-13 10:38:58 +0300

    comment to earliestAndLatest

--- leet/contest/245/task_4.go
+++ leet/contest/245/task_4.go
@@ -1,6 +1,7 @@
 package _45
 
 // todo 1
+// https://leetcode.com/problems/the-earliest-and-latest-rounds-where-players-compete/discuss/1268586/Java-DP-with-memory
 func earliestAndLatest(n int, firstPlayer int, secondPlayer int) []int {
 	return nil
 }
