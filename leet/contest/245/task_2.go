0054014136d3d5bc57a0ca86a7b6d892925c3447
petrovich.andrey <APetrovich@one.kz>
2021-06-14 05:48:58 +0300

    maxumumRemovals refac2

--- leet/contest/245/task_2.go
+++ leet/contest/245/task_2.go
@@ -1,13 +1,14 @@
 package _45
 
-// todo 1 refac and understand
+// tptl. passed. medium #string
 // https://leetcode.com/problems/maximum-number-of-removable-characters/discuss/1268656/C%2B%2B-Binary-search
 func maximumRemovals(s string, p string, removable []int) int {
 	left, right := 0, len(removable)+1
 	var res int
+	b := []byte(s)
 	for left < right {
 		mid := (left + right) / 2
-		if check(s, p, mid, removable) {
+		if check(b, s, p, mid, removable) {
 			res = mid
 			left = mid + 1
 		} else {
@@ -18,9 +19,7 @@ func maximumRemovals(s string, p string, removable []int) int {
 	return res
 }
 
-func check(s, p string, count int, removable []int) bool {
-	var b = []byte(s)
-
+func check(b []byte, s, p string, count int, removable []int) bool {
 	for i := 0; i < count; i++ {
 		b[removable[i]] = 0
 	}
@@ -30,6 +29,10 @@ func check(s, p string, count int, removable []int) bool {
 		if p[j] == b[i] {
 			j++
 		}
+	}
+
+	for i := 0; i < count; i++ {
+		b[removable[i]] = s[removable[i]]
 	}
 
 	return j == len(p)
