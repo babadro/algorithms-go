6b672241aa08046061d3d664c48489aadfa3827c
petrovich.andrey <APetrovich@one.kz>
2021-07-11 07:27:37 +0300

    task_4 in progress

---
+++ leet/contest/task_4.go
@@ -0,0 +1,91 @@
+package contest
+
+import (
+	"github.com/babadro/algorithms-go/base/binaryTree"
+	"math"
+)
+
+// todo 1
+func canMerge(trees []*binaryTree.Node) *binaryTree.Node {
+	roots := make(map[int]*binaryTree.Node)
+	leafToParent := make(map[int]*binaryTree.Node)
+
+	// fill maps
+	for _, tree := range trees {
+		roots[tree.Val] = tree
+
+		if tree.Left == nil && tree.Right == nil {
+			continue
+		}
+
+		if tree.Left != nil {
+			if _, ok := leafToParent[tree.Left.Val]; ok {
+				return nil
+			}
+
+			leafToParent[tree.Left.Val] = tree
+		}
+
+		if tree.Right != nil {
+			if _, ok := leafToParent[tree.Right.Val]; ok {
+				return nil
+			}
+
+			leafToParent[tree.Right.Val] = tree
+		}
+	}
+
+	var parent *binaryTree.Node
+	var ok bool
+	for len(roots) > 1 {
+		rootK, rootV := pop(roots)
+		if parent, ok = leafToParent[rootK]; !ok {
+			return nil
+		}
+
+		if parent.Left != nil && parent.Left.Val == rootK {
+			parent.Left = rootV
+		} else {
+			parent.Right = rootV
+		}
+
+		if rootV.Left != nil || rootV.Right != nil {
+			delete(leafToParent, rootK)
+		}
+	}
+
+	for _, root := range roots {
+		if !isValidBST(root) {
+			return nil
+		}
+
+		return root
+	}
+
+	return nil
+}
+
+func pop(nodes map[int]*binaryTree.Node) (k int, v *binaryTree.Node) {
+	for k, v = range nodes {
+		delete(nodes, k)
+
+		return k, v
+	}
+
+	return 0, nil
+}
+
+func isValidBST(root *binaryTree.Node) bool {
+	return recursive(root, math.MinInt64, math.MaxInt64)
+}
+
+func recursive(node *binaryTree.Node, min, max int) bool {
+	if node == nil {
+		return true
+	}
+	if node.Val <= min || node.Val >= max {
+		return false
+	}
+
+	return recursive(node.Left, min, node.Val) && recursive(node.Right, node.Val, max)
+}
---
+++ leet/contest/task_4_test.go
@@ -0,0 +1,65 @@
+package contest
+
+import (
+	"github.com/babadro/algorithms-go/base/binaryTree"
+	"reflect"
+	"testing"
+)
+
+func Test_canMerge(t *testing.T) {
+	tests := []struct {
+		name  string
+		trees func() []*binaryTree.Node
+		want  func() *binaryTree.Node
+	}{
+		{
+			trees: func() []*binaryTree.Node {
+				res := make([]*binaryTree.Node, 3)
+
+				n2, n1 := n(2), n(1)
+				n2.Left = n1
+				res[0] = n2
+
+				n2, n3, n5 := n(2), n(3), n(5)
+				n3.Left, n3.Right = n2, n5
+				res[1] = n3
+
+				n5, n4 := n(5), n(4)
+				n5.Left = n4
+				res[2] = n5
+
+				return res
+			},
+			want: func() *binaryTree.Node {
+				n := nodes(1, 2, 3, 4, 5)
+				n[3].Left, n[3].Right = n[2], n[5]
+				n[2].Left = n[1]
+				n[5].Left = n[4]
+
+				return n[3]
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			trees, want := tt.trees(), tt.want()
+			if got := canMerge(trees); !reflect.DeepEqual(got, want) {
+				t.Errorf("canMerge() = %v, want %v", got, want)
+			}
+		})
+	}
+}
+
+func n(v int) *binaryTree.Node {
+	return &binaryTree.Node{Val: v}
+}
+
+func nodes(values ...int) map[int]*binaryTree.Node {
+	res := make(map[int]*binaryTree.Node)
+
+	for _, v := range values {
+		res[v] = n(v)
+	}
+
+	return res
+}
