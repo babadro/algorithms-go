3def7faf7f382694d1a494fed785150639a337f5
petrovich.andrey <APetrovich@one.kz>
2021-07-30 07:03:07 +0300

    1865 in progress

--- leet/1800-1899/1865_finding_pairs_with_a_certain_sum/finding_pairs_with_a_certain_sum.go
+++ leet/1800-1899/1865_finding_pairs_with_a_certain_sum/finding_pairs_with_a_certain_sum.go
@@ -1,3 +1,43 @@
-package _865_finding_pairs_with_a_certain_sum
+package _1865_finding_pairs_with_a_certain_sum
+
 
 // todo 1
+type FindSumPairs struct {
+	sumToCount map[int]int
+	sums []map[int]int
+	nums1, nums2 []int
+}
+
+
+func Constructor(nums1 []int, nums2 []int) FindSumPairs {
+	res := FindSumPairs{
+		sumToCount: make(map[int]int),
+		pairs:      make([][]pair, len(nums2)),
+		nums1:      nums1,
+		nums2:      nums2,
+	}
+
+	for i, num2 := range nums2 {
+		for j, num1 := range nums1 {
+			sum := num1 + num2
+
+			res.
+		}
+	}
+
+
+}
+
+
+func (this *FindSumPairs) Add(index int, val int)  {
+
+}
+
+
+func (this *FindSumPairs) Count(tot int) int {
+
+}
+
+func (this *FindSumPairs) recalc() {
+
+}
