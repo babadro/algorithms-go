75f2819e89f4548646d461a020b2f64b7d19175c
petrovich.andrey <APetrovich@one.kz>
2021-08-18 07:50:40 +0300

    Equal SubSet sum partition completed

--- leet/100-499/0416_partition_equal_subset_sum/partition_equal_subset_sum_test.go
+++ leet/100-499/0416_partition_equal_subset_sum/partition_equal_subset_sum_test.go
@@ -10,15 +10,15 @@ func Test_canPartition(t *testing.T) {
 		nums []int
 		want bool
 	}{
-		//{[]int{1, 5, 11, 5}, true},
-		//{[]int{1, 2, 3, 5}, false},
-		//{[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, true},
-		//{[]int{1, 1, 2, 2, 3, 3, 4, 4}, true},
+		{[]int{1, 5, 11, 5}, true},
+		{[]int{1, 2, 3, 5}, false},
+		{[]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, true},
+		{[]int{1, 1, 2, 2, 3, 3, 4, 4}, true},
 		{tleInput1, false},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
-			if got := canPartition(tt.nums); got != tt.want {
+			if got := canPartitionBottomUp(tt.nums); got != tt.want {
 				t.Errorf("canPartition() = %v, want %v", got, tt.want)
 			}
 		})
--- leet/100-499/0416_partition_equal_subset_sum/solution.go
+++ leet/100-499/0416_partition_equal_subset_sum/solution.go
@@ -1,6 +1,7 @@
+// #medium tptl #dp
 package _416_partition_equal_subset_sum
 
-// passed #dp #topdown #medium
+// passed #topdown
 func canPartition(nums []int) bool {
 	sum := 0
 	for _, num := range nums {
@@ -11,12 +12,14 @@ func canPartition(nums []int) bool {
 		return false
 	}
 
+	sum /= 2
+
 	dp := make([][]int, len(nums))
 	for i := range dp {
 		dp[i] = make([]int, sum+1)
 	}
 
-	return req(nums, 0, sum/2, dp)
+	return req(nums, 0, sum, dp)
 }
 
 func req(nums []int, idx, amount int, dp [][]int) bool {
@@ -43,6 +46,72 @@ func req(nums []int, idx, amount int, dp [][]int) bool {
 	return res
 }
 
+// passed #bottomup
 func canPartitionBottomUp(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
 
+	if sum%2 == 1 {
+		return false
+	}
+
+	sum /= 2
+	n := len(nums)
+
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, sum+1)
+	}
+
+	for i := 0; i < n; i++ {
+		dp[i][0] = true
+	}
+
+	for s := 1; s <= sum; s++ {
+		if nums[0] == s {
+			dp[0][s] = true
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for s := 1; s <= sum; s++ {
+			if dp[i-1][s] {
+				dp[i][s] = dp[i-1][s]
+			} else if s >= nums[i] {
+				dp[i][s] = dp[i-1][s-nums[i]]
+			}
+		}
+	}
+
+	return dp[n-1][sum]
+}
+
+// best solution. not mine. passed
+func canPartitionBottomUpOptimized(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	if sum%2 == 1 {
+		return false
+	}
+
+	sum /= 2
+	dp := make([]bool, sum+1)
+	dp[0] = true
+	for _, num := range nums {
+		for i := sum; i >= num; i-- {
+			if j := i - num; j >= 0 && dp[j] {
+				dp[i] = true
+				if i == sum {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
