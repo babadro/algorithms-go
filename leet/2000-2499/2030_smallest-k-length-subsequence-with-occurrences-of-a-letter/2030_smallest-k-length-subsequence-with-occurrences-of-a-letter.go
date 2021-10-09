a38b5c3473e2a038751b755f0c823d039338eb35
petrovich.andrey <APetrovich@one.kz>
2021-10-09 09:08:48 +0200

    smallestSubSequence dp doesn't work

--- leet/2000-2499/2030_smallest-k-length-subsequence-with-occurrences-of-a-letter/2030_smallest-k-length-subsequence-with-occurrences-of-a-letter.go
+++ leet/2000-2499/2030_smallest-k-length-subsequence-with-occurrences-of-a-letter/2030_smallest-k-length-subsequence-with-occurrences-of-a-letter.go
@@ -10,7 +10,7 @@ func recBruteForce(s string, k, idx, repetition int, cur []byte, letter byte) st
 			return string(cur)
 		}
 
-		return ""
+		return "0"
 	}
 
 	res1 := recBruteForce(s, k, idx+1, repetition, append(cur, s[idx]), letter)
@@ -20,7 +20,7 @@ func recBruteForce(s string, k, idx, repetition int, cur []byte, letter byte) st
 }
 
 func valid(seq []byte, k int, letter byte, repetition int) bool {
-	if len(seq) < k {
+	if len(seq) != k {
 		return false
 	}
 
@@ -35,11 +35,11 @@ func valid(seq []byte, k int, letter byte, repetition int) bool {
 }
 
 func min(s1, s2 string) string {
-	if s1 == "" {
+	if s1 == "0" {
 		return s2
 	}
 
-	if s2 == "" {
+	if s2 == "0" {
 		return s1
 	}
 
@@ -48,4 +48,38 @@ func min(s1, s2 string) string {
 	}
 
 	return s2
+}
+
+// todo 1 dp solution doesn't work here. Use monostack:
+// https://leetcode.com/problems/smallest-k-length-subsequence-with-occurrences-of-a-letter/discuss/1502211/Monostack-sort-of
+func smallestSubsequenceTopDown(s string, k int, letter byte, repetition int) string {
+	dp := make([]map[string]string, len(s))
+	for i := range dp {
+		dp[i] = make(map[string]string)
+	}
+
+	return recTopDown(s, dp, k, 0, repetition, nil, letter)
+}
+
+func recTopDown(s string, dp []map[string]string, k, idx, repetition int, cur []byte, letter byte) string {
+	if k-len(cur) > len(s)-idx {
+		return "0"
+	}
+
+	if idx == len(s) || len(cur) == k {
+		if valid(cur, k, letter, repetition) {
+			return string(cur)
+		}
+
+		return "0"
+	}
+
+	if _, ok := dp[idx][string(cur)]; !ok {
+		s1 := recTopDown(s, dp, k, idx+1, repetition, append(cur, s[idx]), letter)
+		s2 := recTopDown(s, dp, k, idx+1, repetition, cur, letter)
+
+		dp[idx][string(cur)] = min(s1, s2)
+	}
+
+	return dp[idx][string(cur)]
 }
