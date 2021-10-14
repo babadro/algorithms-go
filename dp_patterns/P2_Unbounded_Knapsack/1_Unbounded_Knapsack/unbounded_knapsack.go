2bc6826d56bfa95dc3601afa9a30804c3474d2a9
petrovich.andrey <APetrovich@one.kz>
2021-10-14 07:02:47 +0200

    find selected items doesn't work almost done

--- dp_patterns/P2_Unbounded_Knapsack/1_Unbounded_Knapsack/unbounded_knapsack.go
+++ dp_patterns/P2_Unbounded_Knapsack/1_Unbounded_Knapsack/unbounded_knapsack.go
@@ -89,3 +89,25 @@ func max(a, b int) int {
 
 	return b
 }
+
+// todo 2 doesn't work
+func FindSelectedElements(dp [][]int, weights, profits []int, capacity int) []int {
+	var res []int
+	n := len(profits)
+	totalProfit := dp[n-1][capacity]
+	for i := n - 1; i >= 0; i-- {
+		for capacity != 0 {
+			if totalProfit != dp[i-1][capacity] {
+				res = append(res, i)
+				capacity -= weights[i]
+				totalProfit -= profits[i]
+			}
+		}
+	}
+
+	if totalProfit != 0 {
+		res = append(res, 0)
+	}
+
+	return res
+}
--- dp_patterns/P2_Unbounded_Knapsack/1_Unbounded_Knapsack/unbounded_knapsack_test.go
+++ dp_patterns/P2_Unbounded_Knapsack/1_Unbounded_Knapsack/unbounded_knapsack_test.go
@@ -1,6 +1,9 @@
 package __Unbounded_Knapsack
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_knapsackBruteForce(t *testing.T) {
 	tests := []struct {
@@ -9,13 +12,37 @@ func Test_knapsackBruteForce(t *testing.T) {
 		capacity int
 		want     int
 	}{
-		{[]int{15, 20, 50}, []int{1, 2, 3}, 5, 80},
-		{[]int{15, 20, 50, 4, 5, 6, 7, 8}, []int{1, 2, 3, 4, 5, 6, 7, 8}, 130, 2165},
+		//{[]int{15, 20, 50}, []int{1, 2, 3}, 5, 80},
+		{[]int{10, 9, 8}, []int{1, 1, 1}, 3, 30},
+		//{[]int{15, 20, 50, 4, 5, 6, 7, 8}, []int{1, 2, 3, 4, 5, 6, 7, 8}, 130, 2165},
 	}
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
 			if got := knapsackBottomUp(tt.profits, tt.weights, tt.capacity); got != tt.want {
 				t.Errorf("knapsackBruteForce() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFindSelectedElements(t *testing.T) {
+	tests := []struct {
+		dp       [][]int
+		weights  []int
+		profits  []int
+		capacity int
+		want     []int
+	}{
+		{[][]int{
+			{0, 10, 20, 30},
+			{0, 10, 20, 30},
+			{0, 10, 20, 30},
+		}, []int{1, 1, 1}, []int{10, 9, 8}, 3, []int{0, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := FindSelectedElements(tt.dp, tt.weights, tt.profits, tt.capacity); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindSelectedElements() = %v, want %v", got, tt.want)
 			}
 		})
 	}
