8ae86a2721e745a69ba2f6bddde726ae2a317823
petrovich.andrey <APetrovich@one.kz>
2021-10-15 07:30:26 +0200

    stone game again

---
+++ leet/2000-2499/2029_stone_game_ix/2029_stone_game_ix.go
@@ -0,0 +1,39 @@
+package _2029_stone_game_ix
+
+// todo 1 need to understand. too complicated.
+// read another simulation solution and math solution with explanation.
+func stoneGameIX(stones []int) bool {
+	r := [3]int{}
+	for _, s := range stones {
+		r[s%3]++
+	}
+
+	min12 := min(r[1], r[2])
+	if min12 != 0 {
+		min12--
+	}
+
+	return dfs(r[0]%2, r[1]-min12, r[2]-min12, 3, true)
+}
+
+func dfs(r0, r1, r2, r int, isAlice bool) bool {
+	if r == 0 || r0 < 0 || r1 < 0 || r2 < 0 {
+		return true
+	}
+
+	if r0 == 0 && r1 == 0 && r2 == 0 {
+		return !isAlice
+	}
+
+	return !dfs(r0-1, r1, r2, r, !isAlice) ||
+		!dfs(r0, r1-1, r2, (r+1)%3, !isAlice) ||
+		!dfs(r0, r1, r2-1, (r+2)%3, !isAlice)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
