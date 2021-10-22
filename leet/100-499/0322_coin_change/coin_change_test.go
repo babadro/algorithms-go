7d1609412efee0311928b3f161d3fea88f610096
petrovich.andrey <APetrovich@one.kz>
2021-10-22 07:35:20 +0200

    still thinking2

--- leet/100-499/0322_coin_change/coin_change_test.go
+++ leet/100-499/0322_coin_change/coin_change_test.go
@@ -10,15 +10,16 @@ func Test_coinChange(t *testing.T) {
 		want   int
 	}{
 		{"1", []int{1, 2, 5}, 11, 3},
-		//{"2", []int{2}, 3, -1},
-		//{"3", []int{1}, 0, 0},
-		//{"4", []int{1}, 1, 1},
-		//{"5", []int{1}, 2, 2},
-		//{"6", []int{186, 419, 83, 408}, 6249, 20},
+		{"2", []int{2}, 3, -1},
+		{"3", []int{1}, 0, 0},
+		{"4", []int{1}, 1, 1},
+		{"5", []int{1}, 2, 2},
+		{"6", []int{186, 419, 83, 408}, 6249, 20},
+		{"7", []int{411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422}, 9864, 24},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := coinChangeTopDown(tt.coins, tt.amount); got != tt.want {
+			if got := coinChangeTopDown2(tt.coins, tt.amount); got != tt.want {
 				t.Errorf("coinChange() = %v, want %v", got, tt.want)
 			}
 		})
--- leet/100-499/0322_coin_change/solution1.go
+++ leet/100-499/0322_coin_change/solution1.go
@@ -38,19 +38,59 @@ func coinChangeTopDown(coins []int, amount int) int {
 		}
 	}
 
-	if res := recTopDown(dp, coins, 0, 0, amount, nil); res != math.MaxInt64 {
+	if res := recTopDown(dp, coins, 0, 0, 0, amount); res != math.MaxInt64 {
 		return res
 	}
 
 	return -1
 }
 
-func recTopDown(dp [][]int, coins []int, idx, cur, amount int, arr []int) int {
-	if amount == 0 {
-		return cur
+func recTopDown(dp [][]int, coins []int, idx, curCount, curAmount, target int) int {
+	if curAmount == target {
+		return curCount
 	}
 
-	if idx == len(coins) || amount < 0 {
+	if idx == len(coins) || curAmount > target {
+		return math.MaxInt64
+	}
+
+	if dp[idx][curAmount] == -1 {
+		sum1 := recTopDown(dp, coins, idx, curCount+1, curAmount+coins[idx], target)
+		sum2 := recTopDown(dp, coins, idx+1, curCount, curAmount, target)
+
+		dp[idx][target] = min(sum1, sum2)
+	}
+
+	return dp[idx][target]
+}
+
+type item struct {
+	count int
+	arr   []int
+}
+
+func coinChangeTopDown2(coins []int, amount int) int {
+	dp := make([][]item, len(coins))
+	for i := range dp {
+		dp[i] = make([]item, amount+1)
+		for j := range dp[i] {
+			dp[i][j].count = -1
+		}
+	}
+
+	if res := recTopDown2(dp, coins, 0, amount, nil); res != math.MaxInt64 {
+		return res
+	}
+
+	return -1
+}
+
+func recTopDown2(dp [][]item, coins []int, idx, amount int, arr []int) int {
+	if amount == 0 {
+		return 0
+	}
+
+	if idx == len(coins) {
 		return math.MaxInt64
 	}
 
@@ -58,18 +98,32 @@ func recTopDown(dp [][]int, coins []int, idx, cur, amount int, arr []int) int {
 		fmt.Println(arr)
 	}
 
-	if dp[idx][amount] == -1 {
-		sum1 := recTopDown(dp, coins, idx, cur+1, amount-coins[idx], append(arr, coins[idx]))
-		sum2 := recTopDown(dp, coins, idx+1, cur, amount, arr)
+	if dp[idx][amount].count == -1 {
+		count1 := math.MaxInt64
+		if coins[idx] <= amount {
+			res := recTopDown2(dp, coins, idx, amount-coins[idx], append(arr, coins[idx]))
+			if res != math.MaxInt64 {
+				count1 = res + 1
+			}
+		}
+
+		count2 := recTopDown2(dp, coins, idx+1, amount, arr)
 
 		if idx == 2 && amount == 5 {
 			fmt.Println("ho")
 		}
 
-		dp[idx][amount] = min(sum1, sum2)
+		copyArr := make([]int, len(arr))
+		copy(copyArr, arr)
+
+		if count1 < count2 {
+			dp[idx][amount].count, dp[idx][amount].arr = count1, copyArr
+		} else {
+			dp[idx][amount].count, dp[idx][amount].arr = count2, copyArr
+		}
 	}
 
-	return dp[idx][amount]
+	return dp[idx][amount].count
 }
 
 func check(arr []int) bool {
