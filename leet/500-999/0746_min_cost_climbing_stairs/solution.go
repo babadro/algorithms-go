aab2d7f95bcce7ca7da4908fc9a72580294ef099
petrovich.andrey <APetrovich@one.kz>
2021-11-05 08:04:27 +0100

    jumps with fee in progress

--- leet/500-999/0746_min_cost_climbing_stairs/solution.go
+++ leet/500-999/0746_min_cost_climbing_stairs/solution.go
@@ -1,6 +1,6 @@
 package _746_min_cost_climbing_stairs
 
-// passed. tptl
+// passed. tptl. best solution
 func minCostClimbingStairs2(cost []int) int {
 	a, b := 0, 0
 	for i := len(cost) - 1; i >= 0; i-- {
@@ -9,4 +9,51 @@ func minCostClimbingStairs2(cost []int) int {
 	}
 
 	return min(a, b)
+}
+
+// TLE, but correct
+func minCostClimbingStairsBruteForce(cost []int) int {
+	return min(recBruteForce(cost, 0), recBruteForce(cost, 1))
+}
+
+func recBruteForce(cost []int, idx int) int {
+	if idx >= len(cost) {
+		return 0
+	}
+
+	return cost[idx] + min(recBruteForce(cost, idx+1), recBruteForce(cost, idx+2))
+}
+
+// passed
+func minCostClimbingStairsTopDown(cost []int) int {
+	dp := make([]int, len(cost))
+	for i := range dp {
+		dp[i] = -1
+	}
+
+	return min(recTopDown(dp, cost, 0), recTopDown(dp, cost, 1))
+}
+
+func recTopDown(dp, cost []int, idx int) int {
+	if idx >= len(cost) {
+		return 0
+	}
+
+	if dp[idx] == -1 {
+		dp[idx] = cost[idx] + min(recTopDown(dp, cost, idx+1), recTopDown(dp, cost, idx+2))
+	}
+
+	return dp[idx]
+}
+
+func minCostClimbingStairsBottomUp(cost []int) int {
+	dp := make([]int, len(cost)+1)
+
+	dp[0], dp[1], dp[2] = 0, cost[0], cost[0]
+
+	for i := 2; i < len(cost); i++ {
+		dp[i+1] = min(dp[i]+cost[i], dp[i-1]+cost[i-1])
+	}
+
+	return dp[len(cost)]
 }
