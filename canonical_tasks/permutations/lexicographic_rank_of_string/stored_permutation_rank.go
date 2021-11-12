00b54a6c4786cfe4d4bcc8265af65d2b3ff30bc2
petrovich.andrey <APetrovich@one.kz>
2021-11-12 08:06:12 +0100

    sorted permutation rank in progress

---
+++ canonical_tasks/permutations/lexicographic_rank_of_string/stored_permutation_rank.go
@@ -0,0 +1,10 @@
+// https://www.geeksforgeeks.org/lexicographic-rank-of-a-string/
+// https://www.interviewbit.com/problems/sorted-permutation-rank/
+package lexicographic_rank_of_string
+
+const mod = 1_000_003
+
+// todo 1
+func findRank(s string) int {
+
+}
---
+++ canonical_tasks/permutations/lexicographic_rank_of_string_with_duplicates/sorted_permutation_rank.go
@@ -0,0 +1,7 @@
+// https://www.interviewbit.com/problems/sorted-permutation-rank-with-repeats/
+// https://www.geeksforgeeks.org/lexicographic-rank-string-duplicate-characters/
+package lexicographic_rank_of_string_with_duplicates
+
+// todo 1
+func findRank(A string) int {
+}
