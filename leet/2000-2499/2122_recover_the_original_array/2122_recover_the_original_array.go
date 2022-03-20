987caed3fc050e7b69e3447d321897ffb5b290d6
babadro <babadro@gmail.com>
2022-03-20 07:52:53 +0100

    2122 recoverArray

--- leet/2000-2499/2122_recover_the_original_array/2122_recover_the_original_array.go
+++ leet/2000-2499/2122_recover_the_original_array/2122_recover_the_original_array.go
@@ -2,7 +2,7 @@ package _2122_recover_the_original_array
 
 import "sort"
 
-// todo 1 doesn't work. Try C++ solution in comments
+// passed. ntu idea
 // https://leetcode.com/problems/recover-the-original-array/discuss/1647452/Python-Short-solution-explained
 func recoverArray(nums []int) []int {
 	n := len(nums) / 2
@@ -39,7 +39,7 @@ func check(nums, ans []int, m map[int]int, k int) bool {
 		}
 
 		m[num] = m[num] - 1
-		m[num] = m[num+k] - 1
+		m[num+k] = m[num+k] - 1
 
 		ans[idx] = num + k/2
 		idx++
@@ -52,6 +52,77 @@ func copyMap(m map[int]int) map[int]int {
 	res := make(map[int]int, len(m))
 	for k, v := range m {
 		res[k] = v
+	}
+
+	return res
+}
+
+// 4 times faster and pretty. #ntu
+func recoverArray2(nums []int) []int {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)/2; i++ {
+		if k := nums[i+1] - nums[0]; k > 0 && k%2 == 0 {
+			var res, deque []int
+			for _, v := range nums {
+				if len(deque) > 0 && deque[0] == v {
+					deque = deque[1:]
+					res = append(res, v-k/2)
+				} else {
+					deque = append(deque, v+k)
+				}
+			}
+
+			if len(deque) == 0 {
+				return res
+			}
+		}
+	}
+
+	return nil
+}
+
+// 20 times faster. best solution. #ntu
+func recoverArray3(nums []int) []int {
+	n := len(nums)
+	sort.Ints(nums)
+
+	for i := 1; i < n; i++ {
+		res := check3(i, n, nums)
+
+		if len(res) == n/2 {
+			return res
+		}
+	}
+
+	return nil
+}
+
+func check3(i, n int, nums []int) []int {
+	k := nums[i] - nums[0]
+	if k%2 != 0 {
+		return nil
+	}
+
+	used := make([]bool, n)
+	var res []int
+	j := 0
+	for j < n && i < n {
+		if used[j] {
+			j++
+			continue
+		}
+
+		t := nums[j] + k
+		if t == nums[i] {
+			res = append(res, nums[j]+k/2)
+			used[i], used[j] = true, true
+			i++
+			j++
+		} else if t < nums[i] {
+			return nil
+		} else {
+			i++
+		}
 	}
 
 	return res
--- leet/2000-2499/2122_recover_the_original_array/2122_recover_the_original_array_test.go
+++ leet/2000-2499/2122_recover_the_original_array/2122_recover_the_original_array_test.go
@@ -12,10 +12,12 @@ func Test_recoverArray(t *testing.T) {
 		want []int
 	}{
 		{[]int{2, 10, 6, 4, 8, 12}, []int{3, 7, 11}},
+		{[]int{1, 1, 3, 3}, []int{2, 2}},
+		{[]int{5, 435}, []int{220}},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
-			if got := recoverArray(tt.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := recoverArray3(tt.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("recoverArray() = %v, want %v", got, tt.want)
 			}
 		})
