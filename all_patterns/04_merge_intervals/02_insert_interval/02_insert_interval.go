eaef7d8c08fdcd53f0f341fc63f974e5df2df7b5
babadro <babadro@gmail.com>
2022-05-10 07:38:17 +0200

    fix

--- all_patterns/04_merge_intervals/02_insert_interval/02_insert_interval.go
+++ all_patterns/04_merge_intervals/02_insert_interval/02_insert_interval.go
@@ -2,23 +2,26 @@ package _2_insert_interval
 
 import "github.com/babadro/algorithms-go/utils"
 
-// leetcode 57
+// see leetcode 57
 func insert(intervals [][]int, newInterval []int) [][]int {
+	if len(intervals) == 0 {
+		return [][]int{newInterval}
+	}
+
 	res := make([][]int, 0, len(intervals)+1)
 	i := 0
-	for ; intervals[i][1] < newInterval[0]; i++ {
+	for ; i < len(intervals) && intervals[i][1] < newInterval[0]; i++ {
 		res = append(res, intervals[i])
 	}
 
-	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+	for ; i < len(intervals) && newInterval[1] >= intervals[i][0]; i++ {
 		newInterval[0] = utils.Min(newInterval[0], intervals[i][0])
 		newInterval[1] = utils.Max(newInterval[1], intervals[i][1])
-		i++
 	}
 
 	res = append(res, newInterval)
 
-	for i < len(intervals) {
+	for ; i < len(intervals); i++ {
 		res = append(res, intervals[i])
 	}
 
