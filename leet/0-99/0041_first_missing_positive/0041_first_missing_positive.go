b4be8370d1b2fe3708c0ed9ac43779e4ef842152
babadro <babadro@gmail.com>
2022-06-04 07:24:46 +0200

    temp2

--- leet/0-99/0041_first_missing_positive/0041_first_missing_positive.go
+++ leet/0-99/0041_first_missing_positive/0041_first_missing_positive.go
@@ -2,10 +2,10 @@ package _041_first_missing_positive
 
 func firstMissingPositive(nums []int) int {
 	for i := range nums {
-		for nums[i] != i+1 {
+		for nums[i] > 0 && nums[i] < len(nums) && nums[i] != i+1 {
 			tmp := nums[i]
 			nums[i] = nums[nums[i]-1]
-			nums[tmp-1] = tmp
+			nums[tmp-1] = nums[tmp]
 		}
 	}
 
@@ -18,10 +18,10 @@ func firstMissingPositive(nums []int) int {
 	return -1
 }
 
-for i := 0; i < len(nums); i++ {
-for nums[i] != i+1 {
-tmp := nums[i]
-nums[i] = nums[nums[i]-1]
-nums[tmp-1] = tmp
-}
-}
+//for i := 0; i < len(nums); i++ {
+//for nums[i] != i+1 {
+//tmp := nums[i]
+//nums[i] = nums[nums[i]-1]
+//nums[tmp-1] = tmp
+//}
+//}
