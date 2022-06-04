0485702c37620eba55ff4bd0ac821fddef778a9f
babadro <babadro@gmail.com>
2022-06-04 07:52:34 +0200

    first k missing positive numbers

--- all_patterns/05_cyclic_sort/8_find_the_first_k_missing_positive_numbers/8_find_the_first_k_missing_positive_numbers.go
+++ all_patterns/05_cyclic_sort/8_find_the_first_k_missing_positive_numbers/8_find_the_first_k_missing_positive_numbers.go
@@ -1,3 +1,7 @@
 package __find_the_first_k_missing_positive_numbers
 
-// see very similar prlem leetcode 41
+// see very similar problem leetcode 41
+
+// todo solve task according conditions:
+// Given an unsorted array containing numbers and a number ‘k’,
+// find the first ‘k’ missing positive numbers in the array.
