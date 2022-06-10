9f24ed66f815880725c9eceae0d26a810e2b8711
babadro <babadro@gmail.com>
2022-06-10 06:50:15 +0200

    findRightInterval sort solution

--- leet/100-499/0436_find_right_interval/0436_find_right_interval.go
+++ leet/100-499/0436_find_right_interval/0436_find_right_interval.go
@@ -2,10 +2,44 @@ package _436_find_right_interval
 
 import (
 	"container/heap"
+	"sort"
 )
 
-// tptl. passed.
-// todo 2 it's slow. find faster solution
+// tptl. passed. fast
+func findRightInterval2(intervals [][]int) []int {
+	starts, ends := make([]int, len(intervals)), make([]int, len(intervals))
+	for i := range intervals {
+		starts[i], ends[i] = i, i
+	}
+
+	sort.Slice(starts, func(i, j int) bool {
+		return intervals[starts[i]][0] < intervals[starts[j]][0]
+	})
+
+	sort.Slice(ends, func(i, j int) bool {
+		return intervals[ends[i]][1] < intervals[ends[j]][1]
+	})
+
+	res := make([]int, len(intervals))
+
+	j := len(ends) - 1
+	for i := len(ends) - 1; i >= 0; i-- {
+		topEnd := ends[i]
+		topStart := -1
+		for ; j >= 0 && intervals[starts[j]][0] >= intervals[topEnd][1]; j-- {
+			topStart = starts[j]
+		}
+
+		res[topEnd] = topStart
+		if topStart != -1 {
+			j++
+		}
+	}
+
+	return res
+}
+
+// two heaps solution. passed but slow
 func findRightInterval(intervals [][]int) []int {
 	startH, endH := startMaxHeap{intervals: intervals}, endMaxHeap{intervals: intervals}
 	for i := range intervals {
