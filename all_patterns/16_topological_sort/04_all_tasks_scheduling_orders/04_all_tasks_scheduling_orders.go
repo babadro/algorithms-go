fe44a1dfb9237d310693ab3bbfa9ab2d07775cd5
babadro <babadro@gmail.com>
2022-07-02 11:23:24 +0200

    all task scheduling orders in progress

--- all_patterns/16_topological_sort/04_all_tasks_scheduling_orders/04_all_tasks_scheduling_orders.go
+++ all_patterns/16_topological_sort/04_all_tasks_scheduling_orders/04_all_tasks_scheduling_orders.go
@@ -1,3 +1,38 @@
 package _04_all_tasks_scheduling_orders
 
 // todo 1
+func allOrders(tasks int, prerequisites [][]int) [][]int {
+	if tasks <= 0 {
+		return nil
+	}
+
+	inDegree := make([]int, tasks)
+	graph := make([][]int, tasks)
+
+	for i := 0; i < len(prerequisites); i++ {
+		parent := prerequisites[i][0]
+		child := prerequisites[i][1]
+		graph[parent] = append(graph[parent], child)
+		inDegree[child]++
+	}
+
+	var sources []int
+	for v, degree := range inDegree {
+		if degree == 0 {
+			sources = append(sources, v)
+		}
+	}
+
+	var sortedOrder []int
+	return getAllTopologicalSorts(graph, inDegree, sources, &sortedOrder)
+}
+
+func getAllTopologicalSorts(graph [][]int, inDegree []int, sources []int, sortedOrder *[]int) [][]int {
+	if len(sources) > 0 {
+		for _, vertex := range sources {
+			*sortedOrder = append(*sortedOrder, vertex)
+			sourcesForNextCall := make([]int, len(sources))
+			copy(sourcesForNextCall, sources)
+		}
+	}
+}
