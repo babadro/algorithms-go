e02898aa1f29129fbba998dd5931597494cfd3d0
babadro <babadro@gmail.com>
2022-07-04 07:29:24 +0200

    max product

--- leet/100-499/0152_maximum_product_subarray/maxProduct.go
+++ leet/100-499/0152_maximum_product_subarray/maxProduct.go
@@ -1,7 +1,41 @@
+// TODO 2 look for java solution in https://leetcode.com/problems/maximum-product-subarray/discuss/183483/JavaC%2B%2BPython-it-can-be-more-simple
 package _152_maximum_product_subarray
 
-// caterpillar 100% 77%
+// tptl passed. best solution. not mine
 func maxProduct(nums []int) int {
+	res, curMin, curMax := nums[0], nums[0], nums[0]
+	for _, v := range nums[1:] {
+		if v < 0 {
+			curMin, curMax = curMax, curMin
+		}
+
+		curMin = min(curMin*v, v)
+		curMax = max(curMax*v, v)
+
+		res = max(res, curMax)
+	}
+
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// caterpillar 100% 77%
+func maxProduct2(nums []int) int {
 	front, maxProduct := 0, nums[0]
 	for front < len(nums) {
 		if nums[front] == 0 {
@@ -28,32 +62,4 @@ func maxProduct(nums []int) int {
 	}
 
 	return maxProduct
-}
-
-// TODO 2 look for java solution in https://leetcode.com/problems/maximum-product-subarray/discuss/183483/JavaC%2B%2BPython-it-can-be-more-simple
-
-// TODO 2 understand
-// https://leetcode.com/problems/maximum-product-subarray/discuss/302805/Simple-code-in-Go-0ms
-func maxProduct2(nums []int) int {
-	tmp := nums[0]
-	max := nums[0]
-	min := nums[0]
-	for i := 1; i < len(nums); i++ {
-		if nums[i] < 0 {
-			max, min = min, max
-		}
-		max *= nums[i]
-		if max < nums[i] {
-			max = nums[i]
-		}
-		min *= nums[i]
-		if min > nums[i] {
-			min = nums[i]
-		}
-		if max > tmp {
-			tmp = max
-		}
-	}
-
-	return tmp
 }
