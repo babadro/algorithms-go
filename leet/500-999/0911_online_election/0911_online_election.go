6702cd7af5e69995c8244ebdf45562a8cece84fa
babadro <babadro@gmail.com>
2022-07-31 07:52:26 +0200

    slow votes solution

---
+++ leet/500-999/0911_online_election/0911_online_election.go
@@ -0,0 +1,58 @@
+package _911_online_election
+
+type TopVotedCandidate struct {
+	votes map[int][]int
+}
+
+func Constructor(persons []int, times []int) TopVotedCandidate {
+	votes := make(map[int][]int)
+	for i := range persons {
+		person := persons[i]
+		votes[person] = append(votes[person], times[i])
+	}
+
+	return TopVotedCandidate{votes: votes}
+}
+
+func (this *TopVotedCandidate) Q(t int) int {
+	maxVotes := 0
+	var winners [][2]int
+	for person, votes := range this.votes {
+		lastIDx := binarySearch(votes, t)
+		votesCount := lastIDx + 1
+		if votesCount == 0 {
+			continue
+		}
+
+		if votesCount > maxVotes {
+			maxVotes = votesCount
+			winners = winners[:0]
+			winners = append(winners, [2]int{person, votes[lastIDx]})
+		} else if votesCount == maxVotes {
+			winners = append(winners, [2]int{person, votes[lastIDx]})
+		}
+	}
+
+	winner, lastVoteTime := -1, -1
+	for _, w := range winners {
+		if w[1] > lastVoteTime {
+			winner, lastVoteTime = w[0], w[1]
+		}
+	}
+
+	return winner
+}
+
+func binarySearch(votes []int, t int) int {
+	l, r := 0, len(votes)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if votes[m] > t {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+
+	return r
+}
---
+++ leet/500-999/0911_online_election/0911_online_election_test.go
@@ -0,0 +1,45 @@
+package _911_online_election
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_binarySearch(t *testing.T) {
+	tests := []struct {
+		votes []int
+		t     int
+		want  int
+	}{
+		{[]int{0}, 0, 0},
+		{[]int{0, 1}, 0, 0},
+		{[]int{0, 1, 1, 1, 2}, 1, 3},
+		{[]int{0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2}, 1, 9},
+		{[]int{1, 2}, 0, -1},
+		{[]int{5}, 4, -1},
+		{[]int{1}, 2, 0},
+		{[]int{1, 2}, 3, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.votes), func(t *testing.T) {
+			if got := binarySearch(tt.votes, tt.t); got != tt.want {
+				t.Errorf("binarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConstructor(t *testing.T) {
+	v := Constructor([]int{0, 1, 1, 0, 0, 1, 0}, []int{0, 5, 10, 15, 20, 25, 30})
+
+	input := []int{3, 12, 25, 15, 24, 8}
+	output := []int{0, 1, 1, 0, 0, 1}
+
+	for i := range input {
+		res := v.Q(input[i])
+
+		require.Equal(t, output[i], res)
+	}
+}
