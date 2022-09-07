71d362bd363cbfeccb25d74b68dd7b96665bec81
babadro <babadro@gmail.com>
2022-09-07 07:15:11 +0200

    find k closest elements

--- leet/500-999/0658_find_k_closest_elements/0658_find_k_closest_elements.go
+++ leet/500-999/0658_find_k_closest_elements/0658_find_k_closest_elements.go
@@ -2,7 +2,23 @@ package _658_find_k_closest_elements
 
 import "math"
 
-// passed
+// tptl. passed. beautiful, simple and short.
+// Fast when k ~= len(arr), slow, when k significantly less, than len(arr)
+func findClosestElements2(arr []int, k, x int) []int {
+	left, right := 0, len(arr)-1
+	for right-left+1 > k {
+		if abs(arr[left]-x) > abs(arr[right]-x) {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return arr[left : right+1]
+}
+
+// passed, but long. Should be fast on big arr and small k, but slow when len(arr) ~= k
+// O(log(n)) + O(k), where n = len(arr)
 func findClosestElements(arr []int, k int, x int) []int {
 	if len(arr) == 1 {
 		return arr
@@ -10,40 +26,37 @@ func findClosestElements(arr []int, k int, x int) []int {
 
 	idx := binarySearch(arr, x)
 	if arr[idx] > x {
-		diff1 := math.MaxInt64
-		if idx-1 >= 0 {
-			diff1 = abs(x - arr[idx-1])
-		}
-
-		diff2 := abs(x - arr[idx])
-
-		if diff1 < diff2 || diff1 == diff2 {
-			idx = idx - 1
-		}
+		idx = findClosest(arr, x, idx-1, idx)
 	}
 
-	k--
 	l, r := idx, idx
-	for k > 0 {
-		diff1, diff2 := math.MaxInt64, math.MaxInt64
-		if l-1 >= 0 {
-			diff1 = abs(x - arr[l-1])
-		}
-
-		if r+1 < len(arr) {
-			diff2 = abs(x - arr[r+1])
-		}
-
-		if diff1 < diff2 || diff1 == diff2 {
+	for k = k - 1; k > 0; k-- {
+		closestIDx := findClosest(arr, x, l-1, r+1)
+		if closestIDx == l-1 {
 			l--
 		} else {
 			r++
 		}
-
-		k--
 	}
 
 	return arr[l : r+1]
+}
+
+func findClosest(arr []int, target, l, r int) int {
+	diff1, diff2 := math.MaxInt64, math.MaxInt64
+	if l >= 0 {
+		diff1 = abs(target - arr[l])
+	}
+
+	if r < len(arr) {
+		diff2 = abs(target - arr[r])
+	}
+
+	if diff1 < diff2 || diff1 == diff2 {
+		return l
+	}
+
+	return r
 }
 
 func binarySearch(arr []int, target int) int {
