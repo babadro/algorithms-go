1a794eff7c9e24f3f1b4c65e64e275ab88a8b0a0
babadro <babadro@gmail.com>
2022-09-10 07:52:42 +0200

    wiggle subsequence

---
+++ leet/100-499/0376_wiggle_subsequence/0376_wiggle_subsequence.go
@@ -0,0 +1,79 @@
+package _376_wiggle_subsequence
+
+type key struct {
+	prev, cur int
+	bigger    bool
+}
+
+func wiggleMaxLengthTopDown(nums []int) int {
+	dp := make(map[key]int)
+
+	c1 := recTopDown(dp, nums, -1, 0, true)
+	c2 := recTopDown(dp, nums, -1, 0, false)
+
+	return max(c1, c2)
+}
+
+func recTopDown(dp map[key]int, nums []int, prev, cur int, asc bool) int {
+	if cur == len(nums) {
+		return 0
+	}
+
+	k := key{prev, cur, asc}
+	res, ok := dp[k]
+	if !ok {
+		var c1, c2 int
+		if match(nums, prev, cur, asc) {
+			c1 = 1 + recTopDown(dp, nums, cur, cur+1, !asc)
+		}
+
+		c2 = recTopDown(dp, nums, prev, cur+1, asc)
+
+		res = max(c1, c2)
+		dp[k] = res
+	}
+
+	return res
+}
+
+func wiggleMaxLengthBruteForce(nums []int) int {
+	c1 := recBruteForce(nums, -1, 0, true)
+	c2 := recBruteForce(nums, -1, 0, false)
+
+	return max(c1, c2)
+}
+
+func recBruteForce(nums []int, prev, cur int, asc bool) int {
+	if cur == len(nums) {
+		return 0
+	}
+
+	var c1, c2 int
+	if match(nums, prev, cur, asc) {
+		c1 = 1 + recBruteForce(nums, cur, cur+1, !asc)
+	}
+
+	c2 = recBruteForce(nums, prev, cur+1, asc)
+
+	return max(c1, c2)
+}
+
+func match(nums []int, prev, cur int, asc bool) bool {
+	if prev == -1 {
+		return true
+	}
+
+	if asc {
+		return nums[prev] < nums[cur]
+	}
+
+	return nums[prev] > nums[cur]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
