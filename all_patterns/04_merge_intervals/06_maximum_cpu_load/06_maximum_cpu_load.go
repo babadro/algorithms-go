88865c731d721e3b995860c6cc8f67bcb1f98ae8
babadro <babadro@gmail.com>
2022-09-10 08:42:13 +0200

    maximum cpu load

---
+++ all_patterns/04_merge_intervals/06_maximum_cpu_load/06_maximum_cpu_load.go
@@ -0,0 +1,52 @@
+package _6_maximum_cpu_load
+
+import (
+	"container/heap"
+	"sort"
+
+	"github.com/babadro/algorithms-go/utils"
+)
+
+// no leetcode example
+func findMaxCPULoad(jobs [][3]int) int {
+	sort.Slice(jobs, func(i, j int) bool {
+		return jobs[i][0] < jobs[j][0]
+	})
+
+	var h minHeap
+	var maxLoad, curLoad int
+	for _, job := range jobs {
+		now := job[0]
+		for h.Len() > 0 && h[0][1] <= now {
+			curLoad -= h[0][2]
+			heap.Pop(&h)
+		}
+
+		heap.Push(&h, job)
+		curLoad += job[2]
+
+		maxLoad = utils.Max(maxLoad, curLoad)
+	}
+
+	return maxLoad
+}
+
+type minHeap [][3]int
+
+func (h minHeap) Len() int { return len(h) }
+func (h minHeap) Less(i, j int) bool {
+	return h[i][1] < h[j][1]
+}
+func (h minHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *minHeap) Push(v interface{}) {
+	*h = append(*h, v.([3]int))
+}
+func (h *minHeap) Pop() interface{} {
+	last := len(*h) - 1
+	res := (*h)[last]
+	*h = (*h)[:last]
+
+	return res
+}
---
+++ all_patterns/04_merge_intervals/06_maximum_cpu_load/06_maximum_cpu_load_test.go
@@ -0,0 +1,21 @@
+package _6_maximum_cpu_load
+
+import "testing"
+
+func Test_findMaxCPULoad(t *testing.T) {
+	tests := []struct {
+		jobs [][3]int
+		want int
+	}{
+		{[][3]int{{6, 7, 10}, {2, 4, 11}, {8, 12, 15}}, 15},
+		{[][3]int{{1, 4, 2}, {2, 4, 1}, {3, 6, 5}}, 8},
+		{[][3]int{{1, 4, 3}, {2, 5, 4}, {7, 9, 6}}, 7},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := findMaxCPULoad(tt.jobs); got != tt.want {
+				t.Errorf("findMaxCPULoad() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
