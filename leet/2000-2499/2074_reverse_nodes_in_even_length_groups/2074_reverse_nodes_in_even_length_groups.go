4fd81d5414f72727d61472d29fad39440d2948a6
babadro <babadro@gmail.com>
2022-09-29 07:15:41 +0200

    linked list in progress

--- leet/2000-2499/2074_reverse_nodes_in_even_length_groups/2074_reverse_nodes_in_even_length_groups.go
+++ leet/2000-2499/2074_reverse_nodes_in_even_length_groups/2074_reverse_nodes_in_even_length_groups.go
@@ -2,28 +2,39 @@ package _2074_reverse_nodes_in_even_length_groups
 
 import "github.com/babadro/algorithms-go/03_StacksAndQueues/04_LinkedList/single"
 
+// todo 1 doesn't work
 func reverseEvenLengthGroups(head *single.ListNode) *single.ListNode {
 	if head == nil {
 		return nil
 	}
 
-	cur, prev := head, head
+	prev, cur := (*single.ListNode)(nil), head
 	for groupSize := 1; cur != nil; groupSize++ {
-		if groupSize%2 == 1 {
-			for i := 0; i < groupSize && cur != nil; i++ {
-				prev = cur
-				cur = cur.Next
-			}
+		tmpPrev, tmpCur, actualSize := prev, cur, 0
+		for ; actualSize < groupSize && cur != nil; actualSize++ {
+			prev = cur
+			cur = cur.Next
+		}
 
+		if actualSize%2 == 1 {
 			continue
 		}
 
-		left := prev
-		var right *single.ListNode
-		for i := 0; i < groupSize && cur != nil; i++ {
-			right = cur.Next
+		prev, cur = tmpPrev, tmpCur
+
+		left, start := prev, cur
+		for i := 0; i < actualSize; i++ {
+			next := cur.Next
 			cur.Next = prev
-			cur = right
+			prev = cur
+			cur = next
 		}
+
+		end, right := prev, cur
+		start, end = end, start
+		left.Next = start
+		end.Next = right
 	}
+
+	return head
 }
---
+++ leet/2000-2499/2074_reverse_nodes_in_even_length_groups/2074_reverse_nodes_in_even_length_groups_test.go
@@ -0,0 +1,34 @@
+package _2074_reverse_nodes_in_even_length_groups
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/babadro/algorithms-go/03_StacksAndQueues/04_LinkedList/single"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_reverseEvenLengthGroups(t *testing.T) {
+	tests := []struct {
+		input, want []int
+	}{
+		//{[]int{1}, []int{1}},
+		//{[]int{1, 2}, []int{1, 2}},
+		//{[]int{1, 2, 3}, []int{1, 3, 2}},
+		//{[]int{1, 2, 3, 4}, []int{1, 3, 2, 4}},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 3, 2, 5, 4}},
+		//{[]int{1, 2, 3, 4, 5, 6}, []int{1, 3, 2, 4, 5, 6}},
+		//{[]int{1, 2, 3, 4, 5, 6, 7}, []int{1, 3, 2, 4, 5, 6, 7}},
+		//{[]int{1, 2, 3, 4, 5, 6, 7, 8}, []int{1, 3, 2, 4, 5, 6, 8, 7}},
+		//{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9}, []int{1, 3, 2, 4, 5, 6, 9, 8, 7}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.input), func(t *testing.T) {
+			head := single.ArrToLinkedList(tt.input)
+			got := reverseEvenLengthGroups(head)
+			gotArr := single.LinkedListToArr(got)
+
+			require.Equal(t, tt.want, gotArr)
+		})
+	}
+}
