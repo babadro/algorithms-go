a43ba69784eeba673c0fcf17e2a94ddfdc271322
babadro <babadro@gmail.com>
2022-10-05 06:52:36 +0200

    free time

--- all_patterns/04_merge_intervals/07_employee_free_time/07_employee_free_time.go
+++ all_patterns/04_merge_intervals/07_employee_free_time/07_employee_free_time.go
@@ -1,14 +1,14 @@
 package _7_employee_free_time
 
 import (
+	"container/heap"
 	"math"
-	"sort"
 
 	"github.com/babadro/algorithms-go/utils"
 )
 
 // leetcode premium https://leetcode.com/problems/employee-free-time/
-// todo  doesn't work
+// todo  check solution with more test cases
 func findEmployeeFreeTime(schedule [][]int) [][]int {
 	minTime, maxTime := math.MaxInt64, math.MinInt64
 	for _, emp := range schedule {
@@ -16,7 +16,7 @@ func findEmployeeFreeTime(schedule [][]int) [][]int {
 		maxTime = utils.Max(maxTime, emp[len(emp)-1])
 	}
 
-	var freeIntervals [][]int
+	var freeIntervals minHeap
 	for _, emp := range schedule {
 		if minTime < emp[0] {
 			freeIntervals = append(freeIntervals, []int{minTime, emp[0]})
@@ -31,20 +31,27 @@ func findEmployeeFreeTime(schedule [][]int) [][]int {
 		}
 	}
 
-	sort.Slice(freeIntervals, func(i, j int) bool {
-		return freeIntervals[i][0] < freeIntervals[j][0]
-	})
+	heap.Init(&freeIntervals)
 
 	counter := 0
 	empCount := len(schedule)
 	var res [][]int
 	var cur []int
-	for _, intv := range freeIntervals {
+	for len(freeIntervals) > 0 {
+		intv := freeIntervals[0]
+		_ = heap.Pop(&freeIntervals)
+
 		if counter == 0 || cur[1] <= intv[0] {
 			cur = intv
 			counter = 1
 		} else {
 			cur[0] = intv[0]
+			if cur[1] < intv[1] {
+				heap.Push(&freeIntervals, []int{cur[1], intv[1]})
+			} else if cur[1] > intv[1] {
+				heap.Push(&freeIntervals, []int{intv[1], cur[1]})
+			}
+
 			cur[1] = utils.Min(cur[1], intv[1])
 			counter++
 		}
@@ -54,6 +61,20 @@ func findEmployeeFreeTime(schedule [][]int) [][]int {
 			counter = 0
 		}
 	}
+
+	return res
+}
+
+type minHeap [][]int
+
+func (h minHeap) Len() int            { return len(h) }
+func (h minHeap) Less(i, j int) bool  { return h[i][0] < h[j][0] }
+func (h minHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *minHeap) Push(v interface{}) { *h = append(*h, v.([]int)) }
+func (h *minHeap) Pop() interface{} {
+	last := len(*h) - 1
+	res := (*h)[last]
+	*h = (*h)[:last]
 
 	return res
 }
