9a7f2aa5b9daf7d03a95c6dce5cc5194b3dccc3e
babadro <babadro@gmail.com>
2022-11-10 07:52:17 +0100

    characterReplacement tiny refac

--- leet/100-499/424_longest_repeating_character_replacement/424_longest_repeating_character_replacement.go
+++ leet/100-499/424_longest_repeating_character_replacement/424_longest_repeating_character_replacement.go
@@ -5,14 +5,12 @@ func characterReplacement(s string, k int) int {
 	freq := [26]int{}
 	left, mostFreq, res := 0, 0, 0
 	for right := range s {
-		rightChar := s[right]
-		freq[rightChar-'A']++
-
-		mostFreq = max(mostFreq, freq[rightChar-'A'])
+		rightChar := s[right] - 'A'
+		freq[rightChar]++
+		mostFreq = max(mostFreq, freq[rightChar])
 
 		if right-left+1-mostFreq > k {
-			leftChar := s[left]
-			freq[leftChar-'A']--
+			freq[s[left]-'A']--
 			left++
 		}
 
