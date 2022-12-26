4fd24b7da7197f2c505372624f62b89a6fe982b0
babadro <babadro@gmail.com>
2022-12-26 11:01:09 +0100

    sort colors rename vars

--- leet/0-99/0075_sort_colors/sort_colors.go
+++ leet/0-99/0075_sort_colors/sort_colors.go
@@ -2,17 +2,17 @@ package _075_sort_colors
 
 // tptl. passed. medium
 func sortColors(nums []int) {
-	low, high := 0, len(nums)-1
-	for i := 0; i <= high; {
+	firstOne, firstTwo := 0, len(nums)-1
+	for i := 0; i <= firstTwo; {
 		if nums[i] == 0 {
-			nums[i], nums[low] = nums[low], nums[i]
+			nums[i], nums[firstOne] = nums[firstOne], nums[i]
 			i++
-			low++
+			firstOne++
 		} else if nums[i] == 1 {
 			i++
 		} else { // nums[i] == 2
-			nums[i], nums[high] = nums[high], nums[i]
-			high--
+			nums[i], nums[firstTwo] = nums[firstTwo], nums[i]
+			firstTwo--
 		}
 	}
 }
