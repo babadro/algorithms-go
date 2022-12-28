681fb8c3f109146ab03f51a9426e1435aa5b3aab
babadro <babadro@gmail.com>
2022-12-28 09:58:11 +0100

    4sum with map

--- leet/100-499/0454_4sum_ii/0454_4sum_ii.go
+++ leet/100-499/0454_4sum_ii/0454_4sum_ii.go
@@ -2,9 +2,28 @@ package _454_4sum_ii
 
 import "sort"
 
+// tptl. passed. not two pointer
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	m := make(map[int]int)
+	for _, num1 := range nums1 {
+		for _, num2 := range nums2 {
+			m[num1+num2]++
+		}
+	}
+
+	res := 0
+	for _, num3 := range nums3 {
+		for _, num4 := range nums4 {
+			res += m[-(num3 + num4)]
+		}
+	}
+
+	return res
+}
+
 // tptl. passed. very slow, but implements two pointers approach
 // todo2 implement approach with map[sum]count where sum = nums1[i] + nums2[j] for all i and j
-func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+func fourSumCount2(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
 	sort.Ints(nums3)
 	sort.Ints(nums4)
 
