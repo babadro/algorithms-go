ad2a6731870b058d42dc6d13ea3b93efe6ca0fe0
babadro <babadro@gmail.com>
2023-03-02 06:17:36 +0100

    comment find corrupt nums

--- all_patterns/05_cyclic_sort/6_find_the_corrupt_pair/6_find_the_corrupt_pair.go
+++ all_patterns/05_cyclic_sort/6_find_the_corrupt_pair/6_find_the_corrupt_pair.go
@@ -1,5 +1,9 @@
 package __find_the_corrupt_pair
 
+// Given a non-empty unsorted array taken from a range of 1 to n.
+// Due to some data error, one of the numbers is duplicated,
+// which results in another number missing.
+// Create a function that returns the corrupt pair (missing, duplicated).
 func findCorruptNums(nums []int) (int, int) {
 	for i := range nums {
 		for {
