973289956166e93fd45436de0978ebdcece7d869
babadro <babadro@gmail.com>
2023-04-15 10:48:52 +0200

    btree in progress

--- base/b_tree/b_tree.go
+++ base/b_tree/b_tree.go
@@ -1,52 +1,103 @@
 package b_tree
 
-// todo base: insertion, deletion, unit tests
-type BTreeNode struct {
-	keys []int
-	t    int
-	C    []BTreeNode
-	n    int
-	leaf bool
+type Node struct {
+	Keys  []int
+	Child []Node
+	Leaf  bool
+	T     int
+	n     int
 }
 
-func New(t int, leaf bool) BTreeNode {
-	return BTreeNode{
-		keys: make([]int, 2*t-1),
-		t:    t,
-		C:    make([]BTreeNode, 2*t),
-		leaf: false,
-		n:    0,
-	}
-}
-
-func (b *BTreeNode) Traverse(f func(n *BTreeNode)) {
-	i := 0
-	for ; i < b.n; i++ {
-		if !b.leaf {
-			b.C[i].Traverse(f)
-		}
-
-		f(b)
-	}
-
-	if !b.leaf {
-		b.C[i].Traverse(f)
+func NewNode(t int) Node {
+	return Node{
+		Keys:  make([]int, 0),
+		Child: make([]Node, 0),
+		Leaf:  false,
+		T:     t,
+		n:     0,
 	}
 }
 
-func (b *BTreeNode) Search(k int) *BTreeNode {
-	i := 0
-	for i < b.n && k > b.keys[i] {
-		i++
-	}
+func NewBTree(t int) Node {
+	root := NewNode(t)
+	root.Leaf = true
 
-	if b.keys[i] == k {
-		return b
-	}
+	return root
+}
 
-	if b.leaf {
+func (x *Node) Search(key int) *Node {
+	if x == nil {
 		return nil
 	}
 
-	return b.C[i].Search(k)
+	i := 0
+	for i < x.n && key < x.Keys[i] {
+		i++
+	}
+
+	if key == x.Keys[i] {
+		return x
+	}
+
+	if x.Leaf {
+		return nil
+	}
+
+	return x.Child[i].Search(key)
+}
+
+func (x *Node) insert(k int) {
+	i := x.n - 1
+
+	if x.Leaf {
+		for ; i >= 0 && k < x.Keys[i]; i-- {
+			x.Keys[i+1] = x.Keys[i]
+		}
+
+		x.Keys[i+1] = k
+		x.n++
+
+		return
+	}
+
+	for i >= 0 && k < x.Keys[i] {
+		i--
+	}
+
+	i++
+	tmp := x.Child[i]
+	if tmp.n == 2*tmp.T-1 {
+		// split todo
+	}
+
+	x.Child[i].insert(k)
+}
+
+func (x *Node) split(pos int) {
+	t := x.T
+	y, z := &x.Child[pos], NewNode(t)
+	z.Leaf = y.Leaf
+	z.n = t - 1
+	for j := 0; j < t-1; j++ {
+		z.Keys[j] = y.Keys[j+t]
+	}
+
+	if !y.Leaf {
+		for j := 0; j < t; j++ {
+			z.Child[j] = y.Child[j+t]
+		}
+	}
+
+	y.n = t - 1
+	for j := x.n; j >= pos+1; j-- {
+		x.Child[j+1] = x.Child[j]
+	}
+	x.Child[pos+1] = z
+
+	for j := x.n - 1; j >= pos; j-- {
+		x.Keys[j+1] = x.Keys[j]
+	}
+
+	x.Keys[pos] = y.Keys[t-1]
+	x.n = x.n + 1
 }
