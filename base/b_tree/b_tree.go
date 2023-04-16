2a197688dd9ae921f10395575a7e779a93431306
babadro <babadro@gmail.com>
2023-04-16 10:16:09 +0200

    split btree node

--- base/b_tree/b_tree.go
+++ base/b_tree/b_tree.go
@@ -1,20 +1,20 @@
 package b_tree
 
 type Node struct {
-	Keys  []int
-	Child []Node
-	Leaf  bool
-	T     int
-	n     int
+	Keys   []int
+	Child  []Node
+	Leaf   bool
+	T      int
+	keyCnt int
 }
 
 func NewNode(t int) Node {
 	return Node{
-		Keys:  make([]int, 0),
-		Child: make([]Node, 0),
-		Leaf:  false,
-		T:     t,
-		n:     0,
+		Keys:   make([]int, 2*t-1),
+		Child:  make([]Node, 2*t),
+		Leaf:   false,
+		T:      t,
+		keyCnt: 0,
 	}
 }
 
@@ -31,7 +31,7 @@ func (x *Node) Search(key int) *Node {
 	}
 
 	i := 0
-	for i < x.n && key < x.Keys[i] {
+	for i < x.keyCnt && key < x.Keys[i] {
 		i++
 	}
 
@@ -47,7 +47,7 @@ func (x *Node) Search(key int) *Node {
 }
 
 func (x *Node) insert(k int) {
-	i := x.n - 1
+	i := x.keyCnt - 1
 
 	if x.Leaf {
 		for ; i >= 0 && k < x.Keys[i]; i-- {
@@ -55,7 +55,7 @@ func (x *Node) insert(k int) {
 		}
 
 		x.Keys[i+1] = k
-		x.n++
+		x.keyCnt++
 
 		return
 	}
@@ -65,9 +65,11 @@ func (x *Node) insert(k int) {
 	}
 
 	i++
-	tmp := x.Child[i]
-	if tmp.n == 2*tmp.T-1 {
-		// split todo
+	if x.Child[i].keyCnt == 2*x.T-1 {
+		x.split(i)
+		if k > x.Keys[i] {
+			i++
+		}
 	}
 
 	x.Child[i].insert(k)
@@ -77,7 +79,7 @@ func (x *Node) split(pos int) {
 	t := x.T
 	y, z := &x.Child[pos], NewNode(t)
 	z.Leaf = y.Leaf
-	z.n = t - 1
+	z.keyCnt = t - 1
 	for j := 0; j < t-1; j++ {
 		z.Keys[j] = y.Keys[j+t]
 	}
@@ -87,17 +89,17 @@ func (x *Node) split(pos int) {
 			z.Child[j] = y.Child[j+t]
 		}
 	}
+	y.keyCnt = t - 1
 
-	y.n = t - 1
-	for j := x.n; j >= pos+1; j-- {
+	for j := x.keyCnt; j >= pos+1; j-- {
 		x.Child[j+1] = x.Child[j]
 	}
 	x.Child[pos+1] = z
 
-	for j := x.n - 1; j >= pos; j-- {
+	for j := x.keyCnt - 1; j >= pos; j-- {
 		x.Keys[j+1] = x.Keys[j]
 	}
 
 	x.Keys[pos] = y.Keys[t-1]
-	x.n = x.n + 1
+	x.keyCnt++
 }
