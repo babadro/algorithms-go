791bd4cfb20983388a94691d1a4ad505f148b87f
Andrei Petrovich <babadro@gmail.com>
2023-05-24 21:04:54 +0200

    tree height after subtree removal: light refac

--- leet/2000-2499/2458_height_of_binary_tree_after_subtree_removal_queries/solution.go
+++ leet/2000-2499/2458_height_of_binary_tree_after_subtree_removal_queries/solution.go
@@ -10,10 +10,10 @@ func treeQueries2(root *binaryTree.Node, queries []int) []int {
 	seen = [100002]int{} // the height seen so far when visiting ith node
 
 	curMax = 0
-	dfs(root, 0)
+	dfs(root, 0, false)
 
 	curMax = 0
-	dfs(root, 0)
+	dfs(root, 0, true)
 
 	r := make([]int, len(queries))
 	for i, q := range queries {
@@ -23,7 +23,7 @@ func treeQueries2(root *binaryTree.Node, queries []int) []int {
 	return r
 }
 
-func dfs(p *binaryTree.Node, h int) {
+func dfs(p *binaryTree.Node, h int, revert bool) {
 	if seen[p.Val] < curMax {
 		seen[p.Val] = curMax
 	}
@@ -32,13 +32,16 @@ func dfs(p *binaryTree.Node, h int) {
 		curMax = h
 	}
 
-	if p.Left != nil {
-		dfs(p.Left, h+1)
+	first, second := p.Left, p.Right
+	if revert {
+		first, second = p.Right, p.Left
 	}
 
-	if p.Right != nil {
-		dfs(p.Right, h+1)
+	if first != nil {
+		dfs(first, h+1, revert)
 	}
 
-	p.Left, p.Right = p.Right, p.Left
+	if second != nil {
+		dfs(second, h+1, revert)
+	}
 }
