acdc8f499338d54a999ff9428fd9aec2168d72ac
Andrei Petrovich <babadro@gmail.com>
2023-05-28 06:29:03 +0200

    random pick weight

---
+++ leet/500-999/0528_random_pick_with_weight/0528_random_pick_with_weight.go
@@ -0,0 +1,30 @@
+package _0528_random_pick_with_weight
+
+import (
+	"math/rand"
+	"sort"
+)
+
+type Solution struct {
+	intervalEnds []int
+	num          int
+}
+
+func Constructor(w []int) Solution {
+	num := 0
+	intervals := make([]int, len(w))
+	for i, weight := range w {
+		num += weight
+		intervals[i] = num
+	}
+
+	return Solution{intervalEnds: intervals, num: num}
+}
+
+func (this *Solution) PickIndex() int {
+	randomNum := rand.Intn(this.num)
+
+	return sort.Search(len(this.intervalEnds), func(i int) bool {
+		return randomNum < this.intervalEnds[i]
+	})
+}
