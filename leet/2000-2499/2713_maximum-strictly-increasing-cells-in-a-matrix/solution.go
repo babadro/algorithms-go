d5ba574db67af9172af6d1903b46ca35b810eaaa
Andrei Petrovich <babadro@gmail.com>
2023-07-25 06:09:19 +0200

    maxIncreasingCells

--- leet/2000-2499/2713_maximum-strictly-increasing-cells-in-a-matrix/solution.go
+++ leet/2000-2499/2713_maximum-strictly-increasing-cells-in-a-matrix/solution.go
@@ -3,39 +3,43 @@ package _2713_maximum_strictly_increasing_cells_in_a_matrix
 import "sort"
 
 type Pos struct {
-	i, j int
+	y, x int
 }
 
+// #bnsrg
 func maxIncreasingCells(mat [][]int) int {
 	n, m := len(mat), len(mat[0])
 	horizontalMax, verticalMax := make([]int, n), make([]int, m)
-	reverseMat := map[int][]Pos{}
-	uniqueVals := []int{}
-	cache := make([]int, n*m)
+	reversedMap := make(map[int][]Pos)
+	cache := make([]int, m*n)
+	var uniqValues []int
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			val := mat[i][j]
-			if len(reverseMat[val]) == 0 {
-				uniqueVals = append(uniqueVals, val)
+	for y := range mat {
+		for x := range mat[0] {
+			val := mat[y][x]
+			if len(reversedMap[val]) == 0 {
+				uniqValues = append(uniqValues, val)
 			}
-			reverseMat[val] = append(reverseMat[val], Pos{i, j})
+
+			reversedMap[val] = append(reversedMap[val], Pos{y, x})
 		}
 	}
 
-	sort.Ints(uniqueVals)
+	sort.Ints(uniqValues)
+
 	ans := 0
-
-	for _, val := range uniqueVals {
-		for k, pos := range reverseMat[val] {
-			cache[k] = max(horizontalMax[pos.i], verticalMax[pos.j]) + 1
-			ans = max(ans, cache[k])
+	for _, val := range uniqValues {
+		for i, pos := range reversedMap[val] {
+			cache[i] = max(horizontalMax[pos.y], verticalMax[pos.x]) + 1
+			ans = max(ans, cache[i])
 		}
-		for k, pos := range reverseMat[val] {
-			horizontalMax[pos.i] = max(horizontalMax[pos.i], cache[k])
-			verticalMax[pos.j] = max(verticalMax[pos.j], cache[k])
+
+		for i, pos := range reversedMap[val] {
+			horizontalMax[pos.y] = max(horizontalMax[pos.y], cache[i])
+			verticalMax[pos.x] = max(verticalMax[pos.x], cache[i])
 		}
 	}
+
 	return ans
 }
 
@@ -43,5 +47,6 @@ func max(a, b int) int {
 	if a > b {
 		return a
 	}
+
 	return b
 }
