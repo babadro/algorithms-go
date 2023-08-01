f7983b3d4a507c1078edca84ffcfcf795c1358c8
andreipetrovich <babadro@gmail.com>
2023-08-01 21:17:28 +0200

    odd even jumps in progress

---
+++ leet/500-999/0975_odd_even_jump/0975_odd_even_jump_test.go
@@ -0,0 +1,30 @@
+package _975_odd_even_jump
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_oddEvenJumps(t *testing.T) {
+	tests := []struct {
+		arr  []int
+		want int
+	}{
+		{[]int{10, 13, 12, 14, 15}, 2},
+		{[]int{2, 3, 1, 1, 4}, 3},
+		{[]int{5, 1, 3, 4, 2}, 3},
+		{[]int{81, 54, 96, 60, 58}, 2}, // todo doesn't work
+	}
+	for _, tt := range tests {
+		limit := len(tt.arr)
+		if limit > 10 {
+			limit = 10
+		}
+
+		t.Run(fmt.Sprintf("%v", tt.arr[:limit]), func(t *testing.T) {
+			if got := oddEvenJumps(tt.arr); got != tt.want {
+				t.Errorf("oddEvenJumps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
