7ced252fa6d4e7ab515383641acd777b5db52b37
andreipetrovich <babadro@gmail.com>
2023-09-12 21:23:53 +0200

    walk implementation

--- leet/100-499/0489_robot_cleaner/0489_robot_cleaner.go
+++ leet/100-499/0489_robot_cleaner/0489_robot_cleaner.go
@@ -19,26 +19,92 @@ func (robot *Robot) TurnRight() {}
 // Clean the current cell.
 func (robot *Robot) Clean() {}
 
-var coords = [][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+var directions = [][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
 
-func cleanRoom(robot *Robot) {
+var visited map[[2]int]bool
 
+var matrix [][]int
+
+type robotWrapper struct {
+	x, y         int
+	directionIDx int
+	r            *Robot
 }
 
-var idx int
+func (r *robotWrapper) rotate180() {
+	r.directionIDx = (r.directionIDx + 2) % 4
+	r.r.TurnRight()
+	r.r.TurnRight()
+}
 
-func walk(robot *Robot, coords [2]int, visited map[[2]int]bool) {
-	if visited[coords] || !robot.Move() {
-		rotate180()
-		return
+func (r *robotWrapper) turnLeft() {
+	if r.directionIDx == 0 {
+		r.directionIDx = 3
+	} else {
+		r.directionIDx--
 	}
 
-	visited[coords] = true
-
-	robot.Clean()
-
+	r.r.TurnLeft()
 }
 
-func rotate180() {
-	idx %= idx + 2
+func (r *robotWrapper) turnRight() {
+	r.directionIDx = (r.directionIDx + 1) % 4
+
+	r.r.TurnRight()
+}
+
+func (r *robotWrapper) move(checkVisited bool) bool {
+	direction := directions[r.directionIDx]
+	deltaY, deltaX := direction[0], direction[1]
+	newY, newX := r.y+deltaY, r.x+deltaX
+
+	key := [2]int{newY, newX}
+	if (checkVisited && visited[key]) || !r.r.Move() {
+		return false
+	}
+
+	if checkVisited {
+		visited[key] = true
+	}
+
+	r.y, r.x = newY, newX
+
+	return true
+}
+
+func (r *robotWrapper) clean() { r.r.Clean() }
+
+func (r *robotWrapper) walk() {
+	if r.move(true) {
+		r.clean()
+		r.walk()
+	} else {
+		r.rotate180()
+	}
+
+	// robot always returns from walk having opposite direction
+	r.turnRight()
+	// go right
+	r.walk()
+
+	// go left
+	r.walk()
+
+	// after returning take right direction to go back
+	r.turnLeft()
+
+	// go back
+	r.move(false)
+}
+
+func cleanRoom(robot *Robot) {
+	visited = make(map[[2]int]bool)
+
+	r := robotWrapper{r: robot}
+
+	r.walk()
+	r.rotate180()
+	r.move(false)
+	r.rotate180()
+	r.walk()
 }
