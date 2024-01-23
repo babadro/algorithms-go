e090527acba5c93d89f1362c78056f9298daf20f
Andrei <babadro@gmail.com>
2024-01-23 21:11:46 +0100

    1730 shortest path tle

---
+++ leet/1500-1799/1730_shortest_path_to_get_food/1730_shortest_path_to_get_food.go
@@ -0,0 +1,51 @@
+package _1730_shortest_path_to_get_food
+
+import "math"
+
+// #bnsrg medium tle
+func getFood(grid [][]byte) int {
+	res := math.MaxInt64
+
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == '*' {
+				traverse(y, x, 0, grid, make(map[[2]int]int), &res)
+
+				if res == math.MaxInt64 {
+					return -1
+				}
+
+				return res
+			}
+		}
+	}
+
+	return 0
+}
+
+func traverse(y, x, counter int, grid [][]byte, visited map[[2]int]int, res *int) {
+	key := [2]int{y, x}
+
+	if y < 0 || y == len(grid) ||
+		x < 0 || x == len(grid[y]) ||
+		grid[y][x] == 'X' {
+		return
+	}
+
+	if cnt, ok := visited[key]; ok && cnt <= counter+1 {
+		return
+	}
+
+	if grid[y][x] == '#' {
+		*res = min(*res, counter)
+
+		return
+	}
+
+	visited[key] = counter + 1
+
+	traverse(y-1, x, counter+1, grid, visited, res)
+	traverse(y+1, x, counter+1, grid, visited, res)
+	traverse(y, x-1, counter+1, grid, visited, res)
+	traverse(y, x+1, counter+1, grid, visited, res)
+}
---
+++ leet/1500-1799/1730_shortest_path_to_get_food/1730_shortest_path_to_get_food_test.go
@@ -0,0 +1,74 @@
+package _1730_shortest_path_to_get_food
+
+import "testing"
+
+func Test_getFood(t *testing.T) {
+	tests := []struct {
+		grid [][]byte
+		want int
+	}{
+		{
+			grid: [][]byte{
+				[]byte("XXXXXX"),
+				[]byte("X*000X"),
+				[]byte("X00#0X"),
+				[]byte("XXXXXX"),
+			},
+			want: 3,
+		},
+		{
+			grid: [][]byte{
+				[]byte("XXXXX"),
+				[]byte("X*X0X"),
+				[]byte("X0X#X"),
+				[]byte("XXXXX"),
+			},
+			want: -1,
+		},
+		{
+			grid: [][]byte{
+				[]byte("XXXXXXXX"),
+				[]byte("X*0X0#0X"),
+				[]byte("X00X00XX"),
+				[]byte("X0000#0X"),
+				[]byte("XXXXXXXX"),
+			},
+			want: 6,
+		},
+		{
+			grid: [][]byte{
+				{'#', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0'},
+				{'0', '0', '#', '#', '0', '0', '0', '0'},
+				{'0', '0', '#', '0', 'X', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '#', '0'},
+				{'0', '0', '0', '#', '*', '0', '0', '0'},
+				{'0', '0', '0', '0', '0', '0', '0', '0'},
+			},
+			want: 1,
+		},
+		{
+			grid: [][]byte{},
+		},
+		{
+			grid: [][]byte{
+				[]byte("000"),
+				[]byte("0##"),
+				[]byte("00X"),
+				[]byte("000"),
+				[]byte("#0*"),
+			},
+			want: 2,
+		},
+		{
+			grid: tle, want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if got := getFood(tt.grid); got != tt.want {
+				t.Errorf("getFood() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
