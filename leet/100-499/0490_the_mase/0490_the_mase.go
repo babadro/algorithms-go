91925909860cbe8a44d3e5ebdedd0f1e8ee5a026
andreipetrovich <babadro@gmail.com>
2024-01-27 20:10:13 +0100

    490 maze - 76 out 78 test cases passed

--- leet/100-499/0490_the_mase/0490_the_mase.go
+++ leet/100-499/0490_the_mase/0490_the_mase.go
@@ -13,42 +13,44 @@ func hasPath(maze [][]int, start []int, destination []int) bool {
 	y, x := start[0], start[1]
 	visited := make(map[[3]int]bool)
 
-	for i := range directions {
-		rec(maze, destination, y, x, i, &res, visited)
-	}
+	rec(maze, destination, y, x, 0, &res, visited)
 
 	return res
 }
 
-func rec(maze [][]int, destination []int, curY, curX int, dir int, res *bool, visited map[[3]int]bool) {
-	y, x := directions[dir][0], directions[dir][1]
+func rec(maze [][]int, destination []int, y, x int, dir int, res *bool, visited map[[3]int]bool) bool {
+	if y < 0 || y == len(maze) || x < 0 || x == len(maze[y]) ||
+		maze[y][x] == 1 {
+		return false
+	}
 
 	key := [3]int{y, x, dir}
 
 	if visited[key] {
-		return
-	}
-
-	if y == len(maze) || y < 0 || x == len(maze[y]) || x < 0 ||
-		maze[y][x] == 1 {
-		if curY == destination[0] && curX == destination[1] {
-			*res = true
-
-			return
-		}
-
-		for i := range directions {
-			if i == dir {
-				continue
-			}
-
-			rec(maze, destination, curY, curX, i, res, visited)
-		}
-
-		return
+		return true
 	}
 
 	visited[key] = true
 
-	rec(maze, destination, y, x, dir, res, visited)
+	ok := rec(maze, destination, y+directions[dir][0], x+directions[dir][1], dir, res, visited)
+
+	if ok {
+		return true
+	}
+
+	if destination[0] == y && destination[1] == x {
+		*res = true
+
+		return true
+	}
+
+	for i, d := range directions {
+		if i == dir {
+			continue
+		}
+
+		rec(maze, destination, y+d[0], x+d[1], i, res, visited)
+	}
+
+	return true
 }
