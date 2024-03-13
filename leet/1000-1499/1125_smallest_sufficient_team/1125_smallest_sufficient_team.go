9b908db66bce78ee369da8720516f1036d5e194b
Andrei <babadro@gmail.com>
2024-03-13 08:01:01 +0100

    1125 32/38 passed

--- leet/1000-1499/1125_smallest_sufficient_team/1125_smallest_sufficient_team.go
+++ leet/1000-1499/1125_smallest_sufficient_team/1125_smallest_sufficient_team.go
@@ -1,40 +1,75 @@
 package _1125_smallest_sufficient_team
 
 func smallestSufficientTeam(req_skills []string, people [][]string) []int {
-	var res []int
+	var bestRes []int
 
 	reqSkills := make(map[string]bool)
 	for _, skill := range req_skills {
 		reqSkills[skill] = true
 	}
 
-	for {
-		maxSkillSet := 0
-		maxSkillSetIDx := -1
+	rec(reqSkills, -1, nil, &bestRes, people)
 
-		for i, p := range people {
-			skillSet := 0
+	return bestRes
+}
 
-			for _, skill := range p {
-				if reqSkills[skill] {
-					skillSet++
-				}
-			}
-
-			if skillSet > maxSkillSet {
-				maxSkillSet = skillSet
-				maxSkillSetIDx = i
-			}
+func rec(reqSkills map[string]bool, idx int, curr []int, bestRes *[]int, people [][]string) {
+	if idx != -1 {
+		if len(*bestRes) > 0 && len(curr)+1 == len(*bestRes) {
+			return
 		}
 
-		for _, skill := range people[maxSkillSetIDx] {
+		for _, skill := range people[idx] {
 			delete(reqSkills, skill)
 		}
 
-		res = append(res, maxSkillSetIDx)
+		curr = append(curr, idx)
+	}
 
-		if len(reqSkills) == 0 {
-			return res
+	if len(reqSkills) == 0 {
+		*bestRes = make([]int, len(curr))
+		copy(*bestRes, curr)
+
+		if idx != -1 {
+			for _, skill := range people[idx] {
+				reqSkills[skill] = true
+			}
+		}
+
+		return
+	}
+
+	var bestPeople []int
+	maxSkillset := 0
+
+	for i, p := range people {
+		skillCount := 0
+
+		for _, skill := range p {
+			if reqSkills[skill] {
+				skillCount++
+			}
+		}
+
+		if skillCount == 0 {
+			continue
+		}
+
+		if skillCount == maxSkillset {
+			bestPeople = append(bestPeople, i)
+		} else if skillCount > maxSkillset {
+			bestPeople = []int{i}
+			maxSkillset = skillCount
+		}
+	}
+
+	for _, peopleIDx := range bestPeople {
+		rec(reqSkills, peopleIDx, curr, bestRes, people)
+	}
+
+	if idx != -1 {
+		for _, skill := range people[idx] {
+			reqSkills[skill] = true
 		}
 	}
 }
--- leet/1000-1499/1125_smallest_sufficient_team/1125_smallest_sufficient_team_test.go
+++ leet/1000-1499/1125_smallest_sufficient_team/1125_smallest_sufficient_team_test.go
@@ -23,6 +23,16 @@ func Test_smallestSufficientTeam(t *testing.T) {
 			[][]string{{"algorithms", "math", "java"}, {"algorithms", "math", "reactjs"}, {"java", "csharp", "aws"}, {"reactjs", "csharp"}, {"csharp", "math"}, {"aws", "java"}},
 			[]int{1, 2},
 		},
+		{
+			[]string{"1", "2", "3", "4", "5"},
+			[][]string{{}, {}, {}, {}, {"2"}, {"2", "5"}, {"2", "3"}, {"1"}, {"4"}, {"1", "3"}},
+			[]int{5, 8, 9},
+		},
+		{
+			skills,
+			people,
+			[]int{0, 15, 18, 29},
+		},
 	}
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
